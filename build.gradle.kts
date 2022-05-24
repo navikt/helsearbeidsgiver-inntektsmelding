a4a9a881d89f493caca056efe6645fd31fdd3f41
Morten Laukvik <morten.laukvik@nav.no>
2022-05-24 15:24:30 +0200

    Environment konfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,17 +46,14 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:2.0.1")
-    implementation("io.ktor:ktor-server-netty-jvm:2.0.1")
-    implementation("io.ktor:ktor-client-core-jvm:2.0.1")
-    implementation("io.ktor:ktor-client-json-jvm:2.0.1")
-    implementation("io.ktor:ktor-client-serialization-jvm:2.0.1")
-    testImplementation("io.ktor:ktor-server-tests-jvm:2.0.1")
-    implementation("io.ktor:ktor-serialization-gson:2.0.1")
-    implementation("io.ktor:ktor-serialization-jackson:2.0.1")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
     implementation("com.nimbusds:nimbus-jose-jwt:$nimbusJoseJwtVersion")
-    implementation("no.nav.helsearbeidsgiver:helsearbeidsgiver-tokenprovider:$helsearbeidsgiverTokenproviderVersion")
+    implementation("no.nav.helsearbeidsgiver:tokenprovider:$helsearbeidsgiverTokenproviderVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
     testImplementation(kotlin("test"))
-    testImplementation("io.ktor:ktor-client-mock-jvm:2.0.1")
+    testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
 }
 
--- src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/Application.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/Application.kt
@@ -1,10 +1,12 @@
 package no.nav.helsearbeidsgiver.inntektsmelding
 
+import io.ktor.server.application.log
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        log.info("Starting")
         configureRouting()
     }.start(wait = true)
 }

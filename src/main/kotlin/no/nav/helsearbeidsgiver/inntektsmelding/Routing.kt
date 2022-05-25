96d6780d75afb4a39e5db3f7100051a9ffdcb731
Morten Laukvik <morten.laukvik@nav.no>
2022-05-25 08:50:40 +0200

    Helsesjekker

--- src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/Routing.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/Routing.kt
@@ -1,15 +1,28 @@
 package no.nav.helsearbeidsgiver.inntektsmelding
 
+import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    install(ContentNegotiation) {
+        jackson()
+    }
     routing {
         get("/") {
             call.respondText("Hello inntektsmelding")
         }
     }
     routing {
+        get("/is-alive") {
+            call.respondText("I'm alive")
+        }
+    }
+    routing {
+        get("/is-ready") {
+            call.respondText("I'm ready")
+        }
     }
 }

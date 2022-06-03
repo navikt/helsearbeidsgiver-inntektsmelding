fd6b8f5cd5e435ad289f81035b7811ae6dda8220
Morten Laukvik <morten.laukvik@nav.no>
2022-06-03 15:55:50 +0200

    BrregLøser

--- brreg/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/brreg/App.kt
+++ brreg/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/brreg/App.kt
@@ -1,7 +1,6 @@
 package no.nav.helsearbeidsgiver.inntektsmelding.brreg
 
 import no.nav.helse.rapids_rivers.RapidApplication
-import no.nav.helse.rapids_rivers.RapidsConnection
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -11,24 +10,6 @@ internal val logger: Logger = LoggerFactory.getLogger("helsearbeidsgiver-im-brre
 fun main() {
     val env = System.getenv()
     RapidApplication.create(env).apply {
-
+        BrregLøser(this)
     }.start()
-}
-
-
-fun main2() {
-    logger.info("Henter inn environment...")
-    sikkerlogg.info("Henter inn environment...")
-    val app = createApp(setUpEnvironment())
-    app.start().also {
-        sikkerlogg.info("Første melding på Rapid fra brreg modulen...")
-        logger.info("Første melding på Rapid fra brreg modulen...")
-        app.publish("Første melding på Rapid fra brreg modulen")
-    }
-}
-
-fun createApp(environment: Environment): RapidsConnection {
-    val rapidsConnection = RapidApplication.create(environment.raw)
-    BrregLøser(rapidsConnection)
-    return rapidsConnection
 }

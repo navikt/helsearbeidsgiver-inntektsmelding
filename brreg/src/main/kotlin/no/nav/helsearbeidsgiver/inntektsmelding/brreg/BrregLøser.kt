98c3a54d4ff66b48764b6f709c6cee26c68b2ed5
Morten Laukvik <morten.laukvik@nav.no>
2022-06-07 16:15:57 +0200

    Skrur av logging da den logger så mye

--- brreg/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/brreg/BrregLøser.kt
+++ brreg/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/brreg/BrregLøser.kt
@@ -16,14 +16,14 @@ class BrregLøser(rapidsConnection: RapidsConnection) : River.PacketListener {
     private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
     private val log = LoggerFactory.getLogger(this::class.java)
 
-    init {
-        River(rapidsConnection).apply {
-            // validate { it.requireContains("@behov", behov) }
-            //validate { it.requireKey()}
-            validate { it.requireKey("@event_name") }
-            //validate { it.requireContains("@event_name", "inntektsmelding_registrert") }
-        }.register(this)
-    }
+//    init {
+//        River(rapidsConnection).apply {
+//            // validate { it.requireContains("@behov", behov) }
+//            //validate { it.requireKey()}
+//            validate { it.requireKey("@event_name") }
+//            //validate { it.requireContains("@event_name", "inntektsmelding_registrert") }
+//        }.register(this)
+//    }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         log.info("Mottok melding: ${packet.toJson()}")

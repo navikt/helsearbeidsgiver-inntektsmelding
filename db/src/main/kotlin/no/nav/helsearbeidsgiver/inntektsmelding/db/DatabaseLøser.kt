124ff7599ef9cc933cd407073acf62c66761628d
Morten Laukvik <morten.laukvik@nav.no>
2022-06-10 12:23:44 +0200

    La til database

---
+++ db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/DatabaseLøser.kt
@@ -0,0 +1,45 @@
+package no.nav.helsearbeidsgiver.inntektsmelding.db
+
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.MessageProblems
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.River
+import org.slf4j.LoggerFactory
+
+class DatabaseLøser(rapidsConnection: RapidsConnection) : River.PacketListener {
+
+    companion object {
+        internal const val behov = "DatabaseLøser"
+    }
+
+    private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    init {
+        River(rapidsConnection).apply {
+            validate { it.requireValue("@event_name" , "inntektsmelding_inn") }
+            validate { it.requireContains("@behov", behov) }
+            validate { it.rejectKey("@løsning") }
+        }.register(this)
+    }
+
+    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        log.info("Mottok melding: ${packet.toJson()}")
+        // TODO -
+        // Følgende publiser at inntektsmelding er persistert
+        packet.setLøsning(behov, "persistert")
+        context.publish(packet.toJson())
+    }
+
+    private fun JsonMessage.setLøsning(nøkkel: String, data: Any) {
+        this["@løsning"] = mapOf(
+            nøkkel to data
+        )
+    }
+
+    override fun onError(problems: MessageProblems, context: MessageContext) {
+        log.error("Fikk error $problems")
+    }
+
+}

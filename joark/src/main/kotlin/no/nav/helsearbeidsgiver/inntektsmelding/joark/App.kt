a1dbfad01c67ae6114316b260aa5003a50e1e5ac
Morten Laukvik <morten.laukvik@nav.no>
2022-09-15 10:26:41 +0200

    Felles måte å starte apper på

--- joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/App.kt
+++ joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/App.kt
@@ -1,15 +1,23 @@
 package no.nav.helsearbeidsgiver.inntektsmelding.joark
 
 import no.nav.helse.rapids_rivers.RapidApplication
+import no.nav.helse.rapids_rivers.RapidsConnection
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 val sikkerlogg: Logger = LoggerFactory.getLogger("tjenestekall")
-internal val logger: Logger = LoggerFactory.getLogger("helsearbeidsgiver-im-kontroll")
+internal val logger: Logger = LoggerFactory.getLogger("im-joark")
 
 fun main() {
-    val env = System.getenv()
-    RapidApplication.create(env).apply {
-        JournalførInntektsmeldingLøser(this)
-    }.start()
+    val environment = setUpEnvironment()
+    val app = createApp(environment)
+    app.start()
+}
+
+internal fun createApp(environment: Environment): RapidsConnection {
+    logger.info("Starting RapidApplication...")
+    val rapidsConnection = RapidApplication.create(environment.raw)
+    logger.info("Starting JournalførInntektsmeldingLøser...")
+    JournalførInntektsmeldingLøser(rapidsConnection)
+    return rapidsConnection
 }
--- pdl/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/pdl/App.kt
+++ pdl/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/pdl/App.kt
@@ -10,7 +10,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 val sikkerlogg: Logger = LoggerFactory.getLogger("tjenestekall")
-internal val logger: Logger = LoggerFactory.getLogger("helsearbeidsgiver-im-pdl")
+internal val logger: Logger = LoggerFactory.getLogger("im-pdl")
 
 fun main() {
     val environment = setUpEnvironment()

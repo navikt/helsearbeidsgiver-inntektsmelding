c0e6e84cc1c4abfd3e6f4b6d2a7424cbff1ea1a2
Morten Laukvik <morten.laukvik@nav.no>
2022-09-15 12:58:30 +0200

    RedisPoller. Logging

--- api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/RedisPoller.kt
+++ api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/RedisPoller.kt
@@ -7,21 +7,22 @@ import java.util.concurrent.TimeoutException
 class RedisPoller(val redisClient: RedisClient) {
 
     suspend fun getValue(key: String, maxRetries: Int = 10, waitMillis: Long = 500): String {
-        for (x in 0..maxRetries) {
-            val value = getValue(key)
-            if (value.isNullOrEmpty()) {
-                delay(waitMillis)
-            } else {
-                return value
+        logger.info("Poller starter...")
+        val connection = redisClient.connect()
+        logger.info("Fikk connection.")
+        connection.use {
+            for (x in 0..maxRetries) {
+                logger.info("Hent verdi")
+                val value = it.sync().get(key)
+                logger.info("Fikk verdi: $value")
+                if (value.isNullOrEmpty()) {
+                    delay(waitMillis)
+                } else {
+                    return value
+                }
             }
         }
         throw TimeoutException("Klarte ikke hente ut verdier!")
-    }
-
-    private fun getValue(key: String): String? {
-        redisClient.connect().use {
-            return it.sync().get(key)
-        }
     }
 
     fun shutdown() {

8bdd97718d64953f4326e7de77a9254328a21b09
Morten Laukvik <morten.laukvik@nav.no>
2022-09-15 10:08:21 +0200

    PDL

--- pdl/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/pdl/App.kt
+++ pdl/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/pdl/App.kt
@@ -5,6 +5,7 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import no.nav.helse.rapids_rivers.RapidApplication
+import no.nav.helse.rapids_rivers.RapidsConnection
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -13,6 +14,13 @@ internal val logger: Logger = LoggerFactory.getLogger("helsearbeidsgiver-im-pdl"
 
 fun main() {
     val environment = setUpEnvironment()
+    val app = createApp(environment)
+    app.start()
+}
+
+internal fun createApp(environment: Environment): RapidsConnection {
+    logger.info("Starting RapidApplication...")
+    val rapidsConnection = RapidApplication.create(environment.raw)
     val httpClient = HttpClient {
         install(ContentNegotiation) {
             json(
@@ -22,10 +30,7 @@ fun main() {
             )
         }
     }
-    // val oauth2TokenProvider = OAuth2TokenProvider()
-    // val pdlClient = PdlClient(environment.pdlUrl, { oauth2TokenProvider.getToken() }, httpClient)
-
-    RapidApplication.create(environment.raw).apply {
-        PdlLøser(this)
-    }.start()
+    logger.info("Starting BrregLøser...")
+    PdlLøser(rapidsConnection)
+    return rapidsConnection
 }

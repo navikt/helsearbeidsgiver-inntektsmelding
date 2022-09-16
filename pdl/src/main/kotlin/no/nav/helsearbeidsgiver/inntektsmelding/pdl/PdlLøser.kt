f299a1f43f0f26f35120cccf77f8329c39260f28
Morten Laukvik <morten.laukvik@nav.no>
2022-09-16 09:20:55 +0200

    PDL med ekte data

--- pdl/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/pdl/PdlLøser.kt
+++ pdl/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/pdl/PdlLøser.kt
@@ -5,11 +5,12 @@ import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import no.nav.helsearbeidsgiver.pdl.PdlClient
 import org.slf4j.LoggerFactory
 
 class PdlLøser(
-    rapidsConnection: RapidsConnection
-    // private val pdlClient: PdlClient
+    rapidsConnection: RapidsConnection,
+    private val pdlClient: PdlClient
 ) : River.PacketListener {
 
     companion object {
@@ -30,10 +31,13 @@ class PdlLøser(
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        val identitetsnummer = packet["identitetsnummer"].asText()
+        sikkerlogg.info("Henter navn for identitetsnummer $identitetsnummer")
         logger.info("Løser behov $behov med id ${packet["@id"].asText()}")
-        // val navn = pdlClient.personNavn(packet["identitetsnummer"].asText())?.navn?.firstOrNull()
-        // val løsning = "${navn?.fornavn} ${navn?.mellomnavn} ${navn?.etternavn}"
-        val løsning = "Navn Navnesen"
+        val navn = pdlClient.personNavn(identitetsnummer)?.navn?.firstOrNull()
+        val løsning = "${navn?.fornavn} ${navn?.mellomnavn} ${navn?.etternavn}"
+        sikkerlogg.info("Fant navn: $løsning for identitetsnummer: $identitetsnummer")
+        //val løsning = "Navn Navnesen"
         packet.setLøsning(behov, løsning)
         context.publish(packet.toJson())
     }

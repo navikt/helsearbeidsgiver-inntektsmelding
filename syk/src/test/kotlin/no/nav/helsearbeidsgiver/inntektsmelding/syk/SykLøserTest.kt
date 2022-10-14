823ddca214b9ff2372f0ec660a24da9b60aab238
Morten Laukvik <morten.laukvik@nav.no>
2022-10-14 15:14:01 +0200

    Syk: Forbedring av lesbarhet av tester

--- syk/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/syk/SykLøserTest.kt
+++ syk/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/syk/SykLøserTest.kt
@@ -22,17 +22,16 @@ import java.util.UUID
 internal class SykLøserTest {
 
     private val rapid = TestRapid()
-    private var sykLøser: SykLøser
+    private val BEHOV = BehovType.SYK.toString()
     private val BEHOV_PDL = BehovType.FULLT_NAVN.toString()
     private val BEHOV_INNTEKT = BehovType.INNTEKT.toString()
-    private val BEHOV_SYK = BehovType.SYK.toString()
     internal val objectMapper: ObjectMapper = jacksonObjectMapper()
         .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
         .registerModule(JavaTimeModule())
 
     init {
-        sykLøser = SykLøser(rapid)
+        SykLøser(rapid)
     }
 
     @BeforeEach
@@ -41,15 +40,26 @@ internal class SykLøserTest {
     }
 
     @Test
+    fun `skal kreve at løsning ikke er satt`() {
+        sendMessage(
+            mapOf(
+                "@behov" to listOf(BEHOV),
+                "uuid" to "uuid",
+                "identitetsnummer" to "abc",
+                "@løsning" to ""
+            )
+        )
+        assertEquals(0, rapid.inspektør.size)
+    }
+
+    @Test
     fun `skal ikke respondere når det ikke er behov`() {
-        rapid.sendTestMessage(
-            objectMapper.writeValueAsString(
-                mapOf(
-                    "@behov" to listOf(BEHOV_PDL),
-                    "@id" to UUID.randomUUID(),
-                    "uuid" to "uuid",
-                    "identitetsnummer" to "abc"
-                )
+        sendMessage(
+            mapOf(
+                "@behov" to listOf(BEHOV_PDL),
+                "@id" to UUID.randomUUID(),
+                "uuid" to "uuid",
+                "identitetsnummer" to "abc"
             )
         )
         assertEquals(0, rapid.inspektør.size)
@@ -57,13 +67,11 @@ internal class SykLøserTest {
 
     @Test
     fun `skal kreve at identitetsnummer er satt`() {
-        rapid.sendTestMessage(
-            objectMapper.writeValueAsString(
-                mapOf(
-                    "@behov" to listOf(BEHOV_PDL),
-                    "@id" to UUID.randomUUID(),
-                    "uuid" to "uuid"
-                )
+        sendMessage(
+            mapOf(
+                "@behov" to listOf(BEHOV_PDL),
+                "@id" to UUID.randomUUID(),
+                "uuid" to "uuid"
             )
         )
         assertEquals(0, rapid.inspektør.size)
@@ -71,13 +79,11 @@ internal class SykLøserTest {
 
     @Test
     fun `skal kreve at id er satt`() {
-        rapid.sendTestMessage(
-            objectMapper.writeValueAsString(
-                mapOf(
-                    "@behov" to listOf(BEHOV_PDL),
-                    "uuid" to "uuid",
-                    "identitetsnummer" to "abc"
-                )
+        sendMessage(
+            mapOf(
+                "@behov" to listOf(BEHOV_PDL),
+                "uuid" to "uuid",
+                "identitetsnummer" to "abc"
             )
         )
         assertEquals(0, rapid.inspektør.size)
@@ -85,32 +91,46 @@ internal class SykLøserTest {
 
     @Test
     fun `skal håndtere når man får hentet data`() {
-        val melding = mapOf(
-            "@behov" to listOf(BEHOV_PDL, BEHOV_INNTEKT, BEHOV_SYK),
-            "@id" to UUID.randomUUID(),
-            "uuid" to "uuid",
-            "identitetsnummer" to "abc"
+        sendMessage(
+            mapOf(
+                "@behov" to listOf(BEHOV_PDL, BEHOV_INNTEKT, BEHOV),
+                "@id" to UUID.randomUUID(),
+                "uuid" to "uuid",
+                "identitetsnummer" to "abc"
+            )
         )
-        rapid.sendTestMessage(objectMapper.writeValueAsString(melding))
-        val løsning: JsonNode = rapid.inspektør.message(0).path("@løsning")
-        val inntektLøsning = objectMapper.readValue<SykLøsning>(løsning.get(BEHOV_SYK).toString())
-        assertNotNull(inntektLøsning.value)
-        assertNull(inntektLøsning.error)
+        val løsning = hentLøsning()
+        assertNotNull(løsning.value)
+        assertNull(løsning.error)
     }
 
     @Test
     fun `skal håndtere når man ikke får hentet data`() {
-        val melding = mapOf(
-            "@behov" to listOf(BEHOV_PDL, BEHOV_INNTEKT, BEHOV_SYK),
-            "@id" to UUID.randomUUID(),
-            "uuid" to "uuid",
-            "identitetsnummer" to "000000000"
+        sendMessage(
+            mapOf(
+                "@behov" to listOf(BEHOV_PDL, BEHOV_INNTEKT, BEHOV),
+                "@id" to UUID.randomUUID(),
+                "uuid" to "uuid",
+                "identitetsnummer" to "000000000"
+            )
         )
-        rapid.sendTestMessage(objectMapper.writeValueAsString(melding))
+        val løsning = hentLøsning()
+        assertNull(løsning.value)
+        assertNotNull(løsning.error)
+        assertEquals("Klarte ikke hente syk", løsning.error?.melding)
+    }
+
+    fun sendMessage(mapOf: Map<String, Any>) {
+        rapid.reset()
+        rapid.sendTestMessage(
+            objectMapper.writeValueAsString(
+                mapOf
+            )
+        )
+    }
+
+    fun hentLøsning(): SykLøsning {
         val løsning: JsonNode = rapid.inspektør.message(0).path("@løsning")
-        val inntektLøsning = objectMapper.readValue<SykLøsning>(løsning.get(BEHOV_SYK).toString())
-        assertNull(inntektLøsning.value)
-        assertNotNull(inntektLøsning.error)
-        assertEquals("Klarte ikke hente syk", inntektLøsning.error?.melding)
+        return objectMapper.readValue<SykLøsning>(løsning.get(BEHOV).toString())
     }
 }

d360cf910df8c7ae4b040be7566195e5717c3e46
Morten Laukvik <morten.laukvik@nav.no>
2022-10-27 11:19:31 +0200

    Mapping av Rapid format til domene objekt

---
+++ joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/MapInntektsmelding.kt
@@ -0,0 +1,48 @@
+package no.nav.helsearbeidsgiver.inntektsmelding.joark
+
+import com.fasterxml.jackson.databind.JsonNode
+import no.nav.helse.rapids_rivers.asLocalDate
+import java.time.LocalDate
+
+fun mapInntektsmelding(jsonNode: JsonNode): Inntektsmelding {
+    try {
+        return parseInntektsmelding(jsonNode)
+    } catch (ex: Exception) {
+        throw UgyldigFormatException(ex)
+    }
+}
+
+fun parseBehandlingsdager(data: JsonNode): List<LocalDate> {
+    return data.map { it.asLocalDate() }
+}
+
+fun parseEgenmeldinger(data: JsonNode): List<Egenmelding> {
+    return data.map { Egenmelding(it.get("fom").asLocalDate(), it.get("tom").asLocalDate()) }
+}
+
+fun parseNaturalytelser(data: JsonNode): List<Naturalytelse> {
+    return data.map { Naturalytelse(it.get("naturalytelseKode").asText(), it.get("dato").asLocalDate(), it.get("beløp").asDouble()) }
+}
+
+fun parseInntektsmelding(data: JsonNode): Inntektsmelding {
+    return Inntektsmelding(
+        data.get("orgnrUnderenhet").asText(),
+        data.get("identitetsnummer").asText(),
+        data.get("behandlingsdagerFom").asLocalDate(),
+        data.get("behandlingsdagerTom").asLocalDate(),
+        parseBehandlingsdager(data.get("behandlingsdager")),
+        parseEgenmeldinger(data.get("egenmeldinger")),
+        data.get("bruttonInntekt").asDouble(),
+        data.get("bruttoBekreftet").asBoolean(),
+        data.get("utbetalerFull").asBoolean(),
+        data.get("begrunnelseRedusert").asText(),
+        data.get("utbetalerHeleEllerDeler").asBoolean(),
+        data.get("refusjonPrMnd").asDouble(),
+        data.get("opphørerKravet").asBoolean(),
+        data.get("opphørSisteDag").asLocalDate(),
+        parseNaturalytelser(data.get("naturalytelser")),
+        data.get("bekreftOpplysninger").asBoolean()
+    )
+}
+
+class UgyldigFormatException(ex: Exception) : Exception(ex)
--- joark/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/JournalførInntektsmeldingLøserTest.kt
+++ joark/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/JournalførInntektsmeldingLøserTest.kt
@@ -1,3 +1,5 @@
+@file:Suppress("NonAsciiCharacters")
+
 package no.nav.helsearbeidsgiver.inntektsmelding.joark
 
 import com.fasterxml.jackson.databind.DeserializationFeature
@@ -29,6 +31,17 @@ internal class JournalførInntektsmeldingLøserTest {
         løser = JournalførInntektsmeldingLøser(rapid)
     }
 
+    fun sendMessage(mapOf: Map<String, Any>): JournalpostLøsning {
+        rapid.reset()
+        rapid.sendTestMessage(
+            objectMapper.writeValueAsString(
+                mapOf
+            )
+        )
+        val losning: JsonNode = rapid.inspektør.message(0).path("@løsning")
+        return objectMapper.readValue<JournalpostLøsning>(losning.get(BEHOV).toString())
+    }
+
     @Test
     fun `skal journalføre når gyldige data`() {
         val løsning = sendMessage(
@@ -38,7 +51,30 @@ internal class JournalførInntektsmeldingLøserTest {
                 "uuid" to "uuid",
                 "inntektsmelding" to mapOf(
                     "identitetsnummer" to "123",
-                    "orgnrUnderenhet" to "abc"
+                    "orgnrUnderenhet" to "abc",
+                    "behandlingsdagerFom" to "2022-10-01",
+                    "behandlingsdagerTom" to "2022-10-11",
+                    "behandlingsdager" to listOf("2022-10-27", "2022-10-26"),
+                    "egenmeldinger" to listOf(
+                        mapOf("fom" to "2022-09-01", "tom" to "2022-09-05"),
+                        mapOf("fom" to "2022-09-06", "tom" to "2022-09-15")
+                    ),
+                    "bruttonInntekt" to "25300",
+                    "bruttoBekreftet" to "true",
+                    "utbetalerFull" to "true",
+                    "begrunnelseRedusert" to "BeskjedGittForSent",
+                    "utbetalerHeleEllerDeler" to "false",
+                    "refusjonPrMnd" to "19500",
+                    "opphørerKravet" to "false",
+                    "opphørSisteDag" to "2022-08-08",
+                    "naturalytelser" to listOf(
+                        mapOf(
+                            "naturalytelseKode" to "abc",
+                            "dato" to "2022-08-08",
+                            "beløp" to "123"
+                        )
+                    ),
+                    "bekreftOpplysninger" to "true"
                 )
             )
         )
@@ -58,16 +94,5 @@ internal class JournalførInntektsmeldingLøserTest {
             )
         )
         assertNotNull(løsning.error)
-    }
-
-    fun sendMessage(mapOf: Map<String, Any>): JournalpostLøsning {
-        rapid.reset()
-        rapid.sendTestMessage(
-            objectMapper.writeValueAsString(
-                mapOf
-            )
-        )
-        val løsning: JsonNode = rapid.inspektør.message(0).path("@løsning")
-        return objectMapper.readValue<JournalpostLøsning>(løsning.get(BEHOV).toString())
     }
 }

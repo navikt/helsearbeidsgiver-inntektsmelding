65d9bceaea24bbbd753187b6e87e50eff64d9abe
Mikael Bjerga <mikael.bjerga@nav.no>
2022-11-01 14:52:22 +0100

    Angre å gjøre interface sealed

--- api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/validation/CustomValidatorConstraints.kt
+++ api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/validation/CustomValidatorConstraints.kt
@@ -2,7 +2,7 @@ package no.nav.helsearbeidsgiver.inntektsmelding.api.validation
 
 import org.valiktor.Constraint
 
-sealed interface CustomConstraint : Constraint {
+interface CustomConstraint : Constraint {
     override val messageBundle: String
         get() = "messages"
 }

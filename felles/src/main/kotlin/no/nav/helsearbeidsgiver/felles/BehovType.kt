57fd403ddbf2cfc612e6aeb046fa8fb35be70d31
Mikael Bjerga <mikael.bjerga@nav.no>
2022-11-03 13:25:25 +0100

    Fjern ubrukte annotasjoner for json-alias for behovtype

--- felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/BehovType.kt
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/BehovType.kt
@@ -1,7 +1,5 @@
 package no.nav.helsearbeidsgiver.felles
 
-import com.fasterxml.jackson.annotation.JsonAlias
-
 enum class BehovType {
     FULLT_NAVN,
     VIRKSOMHET,
@@ -12,35 +10,3 @@ enum class BehovType {
     JOURNALFOER,
     ARBEIDSGIVERE
 }
-
-@Target(AnnotationTarget.PROPERTY)
-@JsonAlias("FULLT_NAVN")
-annotation class JsonAliasFulltNavn
-
-@Target(AnnotationTarget.PROPERTY)
-@JsonAlias("VIRKSOMHET")
-annotation class JsonAliasVirksomhet
-
-@Target(AnnotationTarget.PROPERTY)
-@JsonAlias("INNTEKT")
-annotation class JsonAliasInntekt
-
-@Target(AnnotationTarget.PROPERTY)
-@JsonAlias("ARBEIDSFORHOLD")
-annotation class JsonAliasArbeidsforhold
-
-@Target(AnnotationTarget.PROPERTY)
-@JsonAlias("EGENMELDING")
-annotation class JsonAliasEgenmelding
-
-@Target(AnnotationTarget.PROPERTY)
-@JsonAlias("SYK")
-annotation class JsonAliasSyk
-
-@Target(AnnotationTarget.PROPERTY)
-@JsonAlias("JOURNALFOER")
-annotation class JsonAliasJournalfoer
-
-@Target(AnnotationTarget.PROPERTY)
-@JsonAlias("ARBEIDSGIVERE")
-annotation class JsonAliasArbeidsgivere

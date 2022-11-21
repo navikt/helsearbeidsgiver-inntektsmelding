eb9536bb5b5a94ba5c33b8aef8843e7f31e16b58
Morten Laukvik <morten.laukvik@nav.no>
2022-11-21 13:53:14 +0100

    Ny dokarkiv client og bedre mapping av felter

--- joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/MapOpprettJournalpostRequest.kt
+++ joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/MapOpprettJournalpostRequest.kt
@@ -9,7 +9,7 @@ import no.nav.helsearbeidsgiver.dokarkiv.Journalposttype
 import no.nav.helsearbeidsgiver.dokarkiv.OpprettJournalpostRequest
 import no.nav.helsearbeidsgiver.inntektsmelding.joark.dokument.PdfDokument
 import no.nav.helsearbeidsgiver.inntektsmelding.joark.dokument.mapXmlDokument
-import java.time.LocalDateTime
+import java.time.LocalDate
 import java.util.Base64
 
 /**
@@ -19,9 +19,10 @@ import java.util.Base64
 fun mapOpprettJournalpostRequest(uuid: String, inntektsmelding: Inntektsmelding, arbeidsgiver: String): OpprettJournalpostRequest {
     return OpprettJournalpostRequest(
         tema = "FOR",
-        // behandlingsTema = "ab0326", TODO Skal vi ha med behandlingstema?
+        behandlingsTema = "ab0326",
         tittel = "Inntektsmelding",
         journalposttype = Journalposttype.INNGAAENDE,
+        journalfoerendeEnhet = null,
         kanal = "NAV_NO",
         bruker = Bruker(inntektsmelding.identitetsnummer, IdType.FNR), //  Bruker.id er fnr til personen kravet gjelder for
         eksternReferanseId = "ARI-$uuid", // TODO Hva skal vi bruke som ekstern referanse?
@@ -32,22 +33,24 @@ fun mapOpprettJournalpostRequest(uuid: String, inntektsmelding: Inntektsmelding,
         ),
         dokumenter = listOf(
             Dokument(
+                tittel = "Inntektsmelding",
+                brevkode = "4936",
                 dokumentVarianter = listOf(
-                    DokumentVariant(
-                        filtype = "PDFA",
-                        variantFormat = "ARKIV",
-                        fysiskDokument = Base64.getEncoder().encodeToString(PdfDokument().export(inntektsmelding))
-                    ),
                     DokumentVariant(
                         filtype = "XML",
                         fysiskDokument = Base64.getEncoder().encodeToString(mapXmlDokument(inntektsmelding).toByteArray()),
-                        variantFormat = "ORIGINAL"
+                        variantFormat = "ORIGINAL",
+                        filnavn = "ari-$uuid.xml"
+                    ),
+                    DokumentVariant(
+                        filtype = "PDFA",
+                        variantFormat = "ARKIV",
+                        fysiskDokument = Base64.getEncoder().encodeToString(PdfDokument().export(inntektsmelding)),
+                        filnavn = "ari-$uuid.pdf"
                     )
-                ),
-                brevkode = "4936",
-                tittel = "journalfoeringsTittel"
+                )
             )
         ),
-        datoMottatt = LocalDateTime.now()
+        datoMottatt = LocalDate.now()
     )
 }

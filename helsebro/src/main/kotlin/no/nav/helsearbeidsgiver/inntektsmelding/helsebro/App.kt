cd22d2a836d7fd0caead148291d4397bef731498
Mikael Bjerga <mikael.bjerga@nav.no>
2022-11-24 13:09:39 +0100

    Fiks pakkenavn og avhengigheter
    Co-authored-by: Gustav Berggren <gustav.berggren@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- helsebro/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/helsebro/App.kt
+++ helsebro/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/helsebro/App.kt
@@ -1,5 +1,11 @@
-package no.nav.helsearbeidsgiver.helsebro
+package no.nav.helsearbeidsgiver.inntektsmelding.helsebro
 
+import io.ktor.server.application.call
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 import org.slf4j.LoggerFactory
 
 private val logger = LoggerFactory.getLogger("helsebro-main")

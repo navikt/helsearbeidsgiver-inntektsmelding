79259b2f70f41119cb6f4ed88da198e9a7030065
Morten Laukvik <morten.laukvik@nav.no>
2022-11-25 15:05:48 +0100

    Oppdater journalføring i XML med nyeste felter

--- joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/MapXmlDokument.kt
+++ joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/MapXmlDokument.kt
@@ -6,38 +6,26 @@ fun mapXmlDokument(inntektsmeldingDokument: InntektsmeldingDokument): String {
 <melding xmlns="http://seres.no/xsd/NAV/Inntektsmelding_M/20180924">
     <Skjemainnhold>
         <ytelse>Sykepenger</ytelse>
-        <aarsakTilInnsending>Ny</aarsakTilInnsending>
+        <aarsakTilInnsending>${inntektsmeldingDokument.årsakInnsending}</aarsakTilInnsending>
         <arbeidsgiver>
             <virksomhetsnummer>${inntektsmeldingDokument.orgnrUnderenhet}</virksomhetsnummer>
             <kontaktinformasjon>
-                <kontaktinformasjonNavn>Fru Corporate</kontaktinformasjonNavn>
-                <telefonnummer>01010101</telefonnummer>
+                <kontaktinformasjonNavn>Ukjent kontaktperson</kontaktinformasjonNavn>
+                <telefonnummer>Ukjent telefon</telefonnummer>
             </kontaktinformasjon>
         </arbeidsgiver>
         <arbeidstakerFnr>${inntektsmeldingDokument.identitetsnummer}</arbeidstakerFnr>
         <naerRelasjon>false</naerRelasjon>
         <arbeidsforhold>
-            <foersteFravaersdag>2018-10-26</foersteFravaersdag>
+            <foersteFravaersdag>${inntektsmeldingDokument.bestemmendeFraværsdag}</foersteFravaersdag>
             <beregnetInntekt>
                 <beloep>${inntektsmeldingDokument.beregnetInntekt}</beloep>
             </beregnetInntekt>
         </arbeidsforhold>
-        <sykepengerIArbeidsgiverperioden>
-            <arbeidsgiverperiodeListe>
-                <arbeidsgiverperiode>
-                    <fom>2018-01-01</fom>
-                    <tom>2018-01-10</tom>
-                </arbeidsgiverperiode>
-            </arbeidsgiverperiodeListe>
-            <bruttoUtbetalt>5000</bruttoUtbetalt>
-        </sykepengerIArbeidsgiverperioden>
-        <opphoerAvNaturalytelseListe>
-            <opphoerAvNaturalytelse>
-                <naturalytelseType>elektroniskKommunikasjon</naturalytelseType>
-                <fom>2018-03-02</fom>
-                <beloepPrMnd>100</beloepPrMnd>
-            </opphoerAvNaturalytelse>
-        </opphoerAvNaturalytelseListe>
+        <refusjon>
+            <refusjonsbeloepPrMnd>${inntektsmeldingDokument.refusjon.refusjonPrMnd}</refusjonsbeloepPrMnd>
+            <refusjonsopphoersdato>${inntektsmeldingDokument.refusjon.refusjonOpphører}</refusjonsopphoersdato>
+        </refusjon>
         <avsendersystem>
             <systemnavn>NAV_NO</systemnavn>
             <systemversjon>1.0</systemversjon>

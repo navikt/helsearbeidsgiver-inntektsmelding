705fc53cb9a1fd28713b82e1cb4f94ccee5608d9
Morten Laukvik <morten.laukvik@nav.no>
2022-11-28 12:31:56 +0100

    Bruk begrunnelseForReduksjonEllerIkkeUtbetalt i journalføring

--- joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/MapXmlDokument.kt
+++ joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/MapXmlDokument.kt
@@ -24,21 +24,38 @@ fun mapXmlDokument(inntektsmeldingDokument: InntektsmeldingDokument): String {
                 <beloep>${inntektsmeldingDokument.beregnetInntekt}</beloep>
             </beregnetInntekt>
         </arbeidsforhold>
+        <sykepengerIArbeidsgiverperioden>
+            <arbeidsgiverperiodeListe>
+            {${inntektsmeldingDokument.arbeidsgiverperioder.map {
+        """
+                        <arbeidsgiverperiode>
+                            <fom>${it.fom}</fom>
+                            <tom>${it.tom}</tom>
+                        </arbeidsgiverperiode>
+        """.trimIndent()
+    }}}
+            </arbeidsgiverperiodeListe>
+            <bruttoUtbetalt>0</bruttoUtbetalt>
+            <begrunnelseForReduksjonEllerIkkeUtbetalt>${inntektsmeldingDokument.beregnetInntektEndringÅrsak}</begrunnelseForReduksjonEllerIkkeUtbetalt>
+        </sykepengerIArbeidsgiverperioden>
         <refusjon>
             <refusjonsbeloepPrMnd>${inntektsmeldingDokument.refusjon.refusjonPrMnd}</refusjonsbeloepPrMnd>
             <refusjonsopphoersdato>${inntektsmeldingDokument.refusjon.refusjonOpphører}</refusjonsopphoersdato>
         </refusjon>
+        <gjenopptakelseNaturalytelseListe>
+        </gjenopptakelseNaturalytelseListe>
         <opphoerAvNaturalytelseListe>
             {${inntektsmeldingDokument.naturalytelser?.map {
         """
-                <opphoerAvNaturalytelse>
-                    <naturalytelseType>${it.naturalytelse}</naturalytelseType>
-                    <fom>${it.dato}</fom>
-                    <beloepPrMnd>${it.beløp}</beloepPrMnd>
-                </opphoerAvNaturalytelse>
+            <opphoerAvNaturalytelse>
+                <naturalytelseType>${it.naturalytelse}</naturalytelseType>
+                <fom>${it.dato}</fom>
+                <beloepPrMnd>${it.beløp}</beloepPrMnd>
+            </opphoerAvNaturalytelse>
         """.trimIndent()
     }}}
         </opphoerAvNaturalytelseListe>
+        <innsendingstidspunkt>${inntektsmeldingDokument.tidspunkt}</innsendingstidspunkt>
         <avsendersystem>
             <systemnavn>NAV_NO</systemnavn>
             <systemversjon>1.0</systemversjon>

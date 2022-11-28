704b690f1810442b1e76351cb3bfb4e4832d6cf3
Morten Laukvik <morten.laukvik@nav.no>
2022-11-28 08:56:25 +0100

    Naturalytelser i XML journalføring

--- joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/MapXmlDokument.kt
+++ joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/MapXmlDokument.kt
@@ -1,6 +1,8 @@
 package no.nav.helsearbeidsgiver.inntektsmelding.joark.dokument
 
 fun mapXmlDokument(inntektsmeldingDokument: InntektsmeldingDokument): String {
+    val kontaktNavn = "Ukjent kontaktperson" // TODO
+    val kontaktTelefon = "" // TODO
     return """
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <melding xmlns="http://seres.no/xsd/NAV/Inntektsmelding_M/20180924">
@@ -10,8 +12,8 @@ fun mapXmlDokument(inntektsmeldingDokument: InntektsmeldingDokument): String {
         <arbeidsgiver>
             <virksomhetsnummer>${inntektsmeldingDokument.orgnrUnderenhet}</virksomhetsnummer>
             <kontaktinformasjon>
-                <kontaktinformasjonNavn>Ukjent kontaktperson</kontaktinformasjonNavn>
-                <telefonnummer>Ukjent telefon</telefonnummer>
+                <kontaktinformasjonNavn>$kontaktNavn</kontaktinformasjonNavn>
+                <telefonnummer>$kontaktTelefon</telefonnummer>
             </kontaktinformasjon>
         </arbeidsgiver>
         <arbeidstakerFnr>${inntektsmeldingDokument.identitetsnummer}</arbeidstakerFnr>
@@ -26,6 +28,17 @@ fun mapXmlDokument(inntektsmeldingDokument: InntektsmeldingDokument): String {
             <refusjonsbeloepPrMnd>${inntektsmeldingDokument.refusjon.refusjonPrMnd}</refusjonsbeloepPrMnd>
             <refusjonsopphoersdato>${inntektsmeldingDokument.refusjon.refusjonOpphører}</refusjonsopphoersdato>
         </refusjon>
+        <opphoerAvNaturalytelseListe>
+            {${inntektsmeldingDokument.naturalytelser?.map {
+        """
+                <opphoerAvNaturalytelse>
+                    <naturalytelseType>${it.naturalytelse}</naturalytelseType>
+                    <fom>${it.dato}</fom>
+                    <beloepPrMnd>${it.beløp}</beloepPrMnd>
+                </opphoerAvNaturalytelse>
+        """.trimIndent()
+    }}}
+        </opphoerAvNaturalytelseListe>
         <avsendersystem>
             <systemnavn>NAV_NO</systemnavn>
             <systemversjon>1.0</systemversjon>

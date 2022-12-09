4bc72d176377c0f26730783831aee7619bc831cd
Morten Byhring <morten.byhring@nav.no>
2022-12-09 09:42:36 +0100

    Inntekt: filtrer bort inntekter fra andre arbeidsgivere

--- inntekt/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/inntekt/MapInntekt.kt
+++ inntekt/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/inntekt/MapInntekt.kt
@@ -5,11 +5,16 @@ import no.nav.helsearbeidsgiver.felles.MottattHistoriskInntekt
 import no.nav.helsearbeidsgiver.inntekt.InntektskomponentResponse
 import java.math.BigDecimal
 
-fun mapInntekt(response: InntektskomponentResponse): Inntekt {
+fun mapInntekt(response: InntektskomponentResponse, orgnr: String): Inntekt {
     val list = mutableListOf<MottattHistoriskInntekt>()
     var sum = BigDecimal(0.0)
+    if (orgnr.isNullOrEmpty()) {
+        return Inntekt(sum.toDouble(), list)
+    }
     response.arbeidsInntektMaaned?.forEach { maaned ->
-        maaned.arbeidsInntektInformasjon?.inntektListe?.map { inntekt ->
+        maaned.arbeidsInntektInformasjon?.inntektListe?.filter {
+            it.virksomhet?.identifikator.equals(orgnr)
+        }?.toList()?.map { inntekt ->
             val belop = inntekt.beloep ?: 0.0
             list.add(MottattHistoriskInntekt(maaned.aarMaaned, belop))
             sum = sum.add(BigDecimal.valueOf(belop))

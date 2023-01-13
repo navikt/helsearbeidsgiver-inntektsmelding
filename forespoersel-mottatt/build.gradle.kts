3f903d460d25bea8dac1b1b7d82158f7137b0b2a
Mikael Bjerga <mikael.bjerga@nav.no>
2023-01-13 09:47:36 +0100

    Legg til postgres-driver

--- forespoersel-mottatt/build.gradle.kts
+++ forespoersel-mottatt/build.gradle.kts
@@ -2,6 +2,7 @@ val exposedVersion: String by project
 val flywayVersion: String by project
 val hikariVersion: String by project
 val ktorVersion: String by project
+val postgresqlVersion: String by project
 
 dependencies {
     implementation("com.zaxxer:HikariCP:$hikariVersion")
@@ -9,4 +10,6 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+
+    runtimeOnly("org.postgresql:postgresql:$postgresqlVersion")
 }

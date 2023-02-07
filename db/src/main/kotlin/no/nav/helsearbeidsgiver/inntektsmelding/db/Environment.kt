6436c5495d674f27bc1a4d7615d60a6b6c988774
Morten Laukvik <morten.laukvik@nav.no>
2023-02-07 12:49:58 +0100

    Bytt databasenavn til inntektsmelding

--- db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/Environment.kt
+++ db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/Environment.kt
@@ -4,7 +4,7 @@ import no.nav.helsearbeidsgiver.felles.fromEnv
 
 object Environment {
     object Database {
-        private const val prefix = "NAIS_DATABASE_IM_DB_INNTEKTSMELDING_DB"
+        private const val prefix = "NAIS_DATABASE_IM_DB_INNTEKTSMELDING"
         val host = "${prefix}_HOST".fromEnv()
         val port = "${prefix}_PORT".fromEnv()
         val name = "${prefix}_DATABASE".fromEnv()

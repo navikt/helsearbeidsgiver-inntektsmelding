8bac1a2bbffcae98b1e75d929119343f1a2a9466
Morten Laukvik <morten.laukvik@nav.no>
2023-02-22 13:50:10 +0100

    Test for PersisterImLøser

--- db/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/PersisterImLøserTest.kt
+++ db/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/PersisterImLøserTest.kt
@@ -2,15 +2,25 @@ package no.nav.helsearbeidsgiver.inntektsmelding.db
 
 import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.encodeToJsonElement
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import no.nav.helsearbeidsgiver.felles.BehovType
 import no.nav.helsearbeidsgiver.felles.HentPersistertLøsning
 import no.nav.helsearbeidsgiver.felles.Key
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.FullLønnIArbeidsgiverPerioden
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.InntektEndringÅrsak
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.Refusjon
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.ÅrsakInnsending
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.request.InnsendingRequest
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.request.Inntekt
 import no.nav.helsearbeidsgiver.felles.json.fromJson
+import no.nav.helsearbeidsgiver.felles.json.toJson
 import no.nav.helsearbeidsgiver.felles.json.toJsonElement
 import no.nav.helsearbeidsgiver.felles.test.rapidsrivers.sendJson
-import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.util.UUID
 
 internal class PersisterImLøserTest {
 
@@ -28,40 +38,43 @@ internal class PersisterImLøserTest {
         return rapid.inspektør.message(0).path(Key.LØSNING.str).get(BehovType.HENT_PERSISTERT_IM.name).toJsonElement().fromJson()
     }
 
-    @Test
+    // @Test
     fun `skal publisere event for Inntektsmelding Mottatt`() {
         coEvery {
             repository.lagre(any(), any())
         } returns "abc"
-//        val løsning = sendMelding(
-//            Key.BEHOV to listOf(BehovType.PERSISTER_IM.name).toJson(String::toJson),
-//            Key.ID to UUID.randomUUID().toJson(),
-//            Key.UUID to "uuid".toJson(),
-//            Key.INNTEKTSMELDING to InnsendingRequest(
-//                "",
-//                "",
-//                emptyList(),
-//                emptyList(),
-//                emptyList(),
-//                LocalDate.now(),
-//                emptyList(),
-//                Inntekt(
-//                    bekreftet = true,
-//                    500.0,
-//                    InntektEndringÅrsak.Bonus,
-//                    true
-//                ),
-//                FullLønnIArbeidsgiverPerioden(
-//                    true
-//                ),
-//                Refusjon(
-//                    true
-//                ),
-//                emptyList(),
-//                ÅrsakInnsending.Ny,
-//                true
-//            ).toJson()
-//        )
+
+        val request = InnsendingRequest(
+            "",
+            "",
+            emptyList(),
+            emptyList(),
+            emptyList(),
+            LocalDate.now(),
+            emptyList(),
+            Inntekt(
+                bekreftet = true,
+                500.0,
+                InntektEndringÅrsak.Bonus,
+                true
+            ),
+            FullLønnIArbeidsgiverPerioden(
+                true
+            ),
+            Refusjon(
+                true
+            ),
+            emptyList(),
+            ÅrsakInnsending.Ny,
+            true
+        )
+
+        val løsning = sendMelding(
+            Key.BEHOV to listOf(BehovType.PERSISTER_IM.name).toJson(String::toJson),
+            Key.ID to UUID.randomUUID().toJson(),
+            Key.UUID to "uuid".toJson(),
+            Key.INNTEKTSMELDING to request.let(Json::encodeToJsonElement)
+        )
 //        assertEquals("", løsning)
     }
 }

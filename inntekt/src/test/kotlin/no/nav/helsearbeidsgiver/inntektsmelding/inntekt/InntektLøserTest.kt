db8af122e88597a6cfd31edeb36d4aead63f9f81
Morten Byhring <morten.byhring@nav.no>
2023-03-02 12:25:23 +0100

    refaktorer InntektløserTest

--- inntekt/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/inntekt/InntektLøserTest.kt
+++ inntekt/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/inntekt/InntektLøserTest.kt
@@ -71,16 +71,7 @@ internal class InntektLøserTest {
             Key.UUID to "uuid".toJson(),
             Key.IDENTITETSNUMMER to "abc".toJson(),
             Key.ORGNRUNDERENHET to "123456789".toJson(),
-            Key.SESSION to mapOf(
-                BehovType.HENT_TRENGER_IM to HentTrengerImLøsning(
-                    value = mockTrengerInntekt().copy(
-                        fnr = "fnr",
-                        orgnr = "orgnr",
-                        sykmeldingsperioder = sykmeldingsperioder(LocalDate.of(2022, 5, 1), LocalDate.of(2022, 5, 16)),
-                        forespurtData = emptyList()
-                    )
-                )
-            )
+            Key.SESSION to sessionData()
                 .toJson(
                     MapSerializer(
                         BehovType.serializer(),
@@ -111,16 +102,7 @@ internal class InntektLøserTest {
             Key.UUID to "uuid".toJson(),
             Key.IDENTITETSNUMMER to "abc".toJson(),
             Key.ORGNRUNDERENHET to "123456789".toJson(),
-            Key.SESSION to mapOf(
-                BehovType.HENT_TRENGER_IM to HentTrengerImLøsning(
-                    value = mockTrengerInntekt().copy(
-                        fnr = "fnr",
-                        orgnr = "orgnr",
-                        sykmeldingsperioder = sykmeldingsperioder(LocalDate.of(2022, 5, 1), LocalDate.of(2022, 5, 16)),
-                        forespurtData = emptyList()
-                    )
-                )
-            )
+            Key.SESSION to sessionData()
                 .toJson(
                     MapSerializer(
                         BehovType.serializer(),
@@ -175,21 +157,11 @@ internal class InntektLøserTest {
         assertEquals(p1.fom, bestemmende)
     }
 
-//    @Test
-//    fun `Bruk første dato i siste sammenhengende sykmeldingsperiode`() {
-//        val aar = 2023
-//        val p1 = Periode(31.januar(aar), 2.februar(aar)) // 3 dager, så opphold 10 dager, så 2 dager -> mindre enn 16 dager arb.giver-periode, teller ikke!
-//        val p2 = Periode(12.februar(aar), 14.februar(aar))
-//        val p3 = Periode(26.februar(aar), 28.februar(aar)) // p3 + p4 er en sammenhengende periode større enn arb.giver-periode - 26/2 skal brukes som start!
-//        val p4 = Periode(1.mars(aar), 20.mars(aar))
-//        val sykmeldinger = listOf(p1, p2, p3, p4)
-//    }
-
     @Test
     fun `skal finne riktig inntekt basert på sykmeldingsperioden som kommer i BEHOV`() {
         // TODO: Denne testen er litt grisete og tester mest egen mocke-logikk, men beholder foreløpig
         val svar = List(12) {
-            lagInntektMaaned(YearMonth.now().minusMonths(it.toLong()), 1.0, "orgnr")
+            lagInntektMaaned(YearMonth.of(2022, 12).minusMonths(it.toLong()), 1.0, "orgnr")
         }
         val til = slot<LocalDate>()
         val fra = slot<LocalDate>()
@@ -212,24 +184,13 @@ internal class InntektLøserTest {
             InntektskomponentResponse(filtered)
         }
 
-        val sykmeldingsperiode = sykmeldingsperioder(LocalDate.of(2022, 12, 1), LocalDate.of(2022, 12, 30))
-
         rapid.sendJson(
             Key.BEHOV to listOf(BEHOV_PDL, BEHOV_INNTEKT).toJson(String.serializer()),
             Key.ID to UUID.randomUUID().toJson(),
             Key.UUID to "uuid".toJson(),
             Key.IDENTITETSNUMMER to "fnr".toJson(),
             Key.ORGNRUNDERENHET to "orgnr".toJson(),
-            Key.SESSION to mapOf(
-                BehovType.HENT_TRENGER_IM to HentTrengerImLøsning(
-                    value = mockTrengerInntekt().copy(
-                        fnr = "fnr",
-                        orgnr = "orgnr",
-                        sykmeldingsperioder = sykmeldingsperiode,
-                        forespurtData = emptyList()
-                    )
-                )
-            )
+            Key.SESSION to sessionData()
                 .toJson(
                     MapSerializer(
                         BehovType.serializer(),
@@ -242,12 +203,24 @@ internal class InntektLøserTest {
         val inntektLøsning = løsning.get(BEHOV_INNTEKT)?.toJsonElement()?.fromJson(InntektLøsning.serializer())
         assertNull(inntektLøsning?.error)
         assertEquals(3, inntektLøsning?.value!!.historisk.size)
-        assertEquals(YearMonth.of(2022, 11), inntektLøsning.value!!.historisk[0].maanedsnavn)
-        assertEquals(YearMonth.of(2022, 10), inntektLøsning.value!!.historisk[1].maanedsnavn)
-        assertEquals(YearMonth.of(2022, 9), inntektLøsning.value!!.historisk[2].maanedsnavn)
+        assertEquals(YearMonth.of(2022, 4), inntektLøsning.value!!.historisk[0].maanedsnavn)
+        assertEquals(YearMonth.of(2022, 3), inntektLøsning.value!!.historisk[1].maanedsnavn)
+        assertEquals(YearMonth.of(2022, 2), inntektLøsning.value!!.historisk[2].maanedsnavn)
     }
 
+    private fun sessionData() = mapOf(
+        BehovType.HENT_TRENGER_IM to HentTrengerImLøsning(
+            value = mockTrengerInntekt().copy(
+                fnr = "fnr",
+                orgnr = "orgnr",
+                sykmeldingsperioder = sykmeldingsperioder(LocalDate.of(2022, 5, 1), LocalDate.of(2022, 5, 16)),
+                forespurtData = emptyList()
+            )
+        )
+    )
+
     private fun sykmeldingsperioder(fom: LocalDate, tom: LocalDate) = listOf(Periode(fom, tom))
+
     private fun lagInntektMaaned(mnd: YearMonth, beloep: Double, orgnr: String): ArbeidsinntektMaaned {
         return ArbeidsinntektMaaned(
             aarMaaned = mnd,

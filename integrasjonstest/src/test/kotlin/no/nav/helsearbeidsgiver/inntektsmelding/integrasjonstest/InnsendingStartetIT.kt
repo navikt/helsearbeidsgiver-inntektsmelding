f8c4141e5d719e5744c9ddf72befbf05906b0c40
Alexander Petrov <alexander.petrov@nav.no>
2023-03-17 12:16:35 +0100

    skript på IT

---
+++ integrasjonstest/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/integrasjonstest/InnsendingStartetIT.kt
@@ -0,0 +1,13 @@
+package no.nav.helsearbeidsgiver.inntektsmelding.integrasjonstest
+
+class InnsendingStartetIT : EndToEndTest() {
+
+    fun `Test at innsnending er mottatt`() {
+
+        //  1) vi sender en InntektsMeldingStartet event
+        //  2) vi kjører Prossessoren ,med avhengigheter breg og arbeidsforhold, postgres, redis
+        //  3) InntektMeldingMottatt
+        //
+
+    }
+}

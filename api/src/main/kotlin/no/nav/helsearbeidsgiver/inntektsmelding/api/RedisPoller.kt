36be8252e83df30b4969f9fa12770f52aba7b0eb
Alexander Petrov <alexander.petrov@nav.no>
2023-03-20 21:15:12 +0100

    fiks redis url for api

--- api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/RedisPoller.kt
+++ api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/RedisPoller.kt
@@ -10,7 +10,9 @@ import no.nav.helsearbeidsgiver.felles.log.loggerSikker
 
 // TODO Bruke kotlin.Result istedenfor exceptions?
 class RedisPoller {
-    private val redisClient = RedisClient.create(Env.Redis.url)
+    private val redisClient = RedisClient.create(
+        "redis://${Env.Redis.url}:6379/0"
+    )
     private val loggerSikker = loggerSikker()
 
     suspend fun hent(key: String, maxRetries: Int = 10, waitMillis: Long = 500): JsonElement {

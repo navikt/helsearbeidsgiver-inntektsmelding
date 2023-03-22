2ec895f1a6abc9cc713d36efc919f45180c07cb5
Morten Laukvik <morten.laukvik@nav.no>
2023-03-22 14:33:20 +0100

    Config

---
+++ integrasjonstest/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/integrasjonstest/mapHikariConfig.kt
@@ -0,0 +1,24 @@
+package no.nav.helsearbeidsgiver.inntektsmelding.integrasjonstest
+
+import com.zaxxer.hikari.HikariConfig
+import org.testcontainers.containers.PostgreSQLContainer
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
+
+fun mapHikariConfig(postgreSQLContainer: PostgreSQLContainer<Nothing>): HikariConfig {
+    return HikariConfig().apply {
+        jdbcUrl = postgreSQLContainer.jdbcUrl
+        username = postgreSQLContainer.username
+        password = postgreSQLContainer.password
+        maximumPoolSize = 1
+        connectionTimeout = 30.seconds.toMillis()
+        initializationFailTimeout = 1.minutes.toMillis()
+        idleTimeout = 1.minutes.toMillis()
+        maxLifetime = idleTimeout * 5
+    }
+}
+
+fun Duration.toMillis(): Long =
+    toJavaDuration().toMillis()

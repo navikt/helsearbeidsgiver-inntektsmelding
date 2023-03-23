bb59ef7ae09aa39d530ab99eec2dc1c07da8ff3c
Morten Byhring <morten.byhring@nav.no>
2023-03-23 12:18:15 +0100

    kafka config for lokal kjøring

--- felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/app/LocalApp.kt
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/app/LocalApp.kt
@@ -4,7 +4,7 @@ open class LocalApp {
 
     open fun setupEnvironment(memberId: String, httpPort: Int): Map<String, String> {
         val env = mapOf(
-            "KAFKA_BOOTSTRAP_SERVERS" to "localhost:9092",
+            "KAFKA_BOOTSTRAP_SERVERS" to "PLAINTEXT://localhost:9092",
             "KAFKA_CONSUMER_GROUP_ID" to memberId,
             "KAFKA_RAPID_TOPIC" to "helsearbeidsgiver.rapid",
             "LOGINSERVICE_IDPORTEN_AUDIENCE" to "aud-localhost",

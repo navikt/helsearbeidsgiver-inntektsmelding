e05e4c1e6cbf65eab85455257984e49e846a1bd8
Morten Laukvik <morten.laukvik@nav.no>
2023-03-28 09:42:51 +0200

    DB: Ikke close connection ved migrering

--- db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/Database.kt
+++ db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/Database.kt
@@ -5,7 +5,6 @@ import com.zaxxer.hikari.HikariDataSource
 import org.flywaydb.core.Flyway
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
-import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
 import org.jetbrains.exposed.sql.Database as ExposedDatabase
 
@@ -25,19 +24,8 @@ class Database(
                     .load()
                     .migrate()
             }
-            .close()
     }
 }
-
-private fun dbConfig(): HikariConfig =
-    HikariConfig().apply {
-        jdbcUrl = this.jdbcUrl
-        maximumPoolSize = 1
-        connectionTimeout = 30.seconds.toMillis()
-        initializationFailTimeout = 1.minutes.toMillis()
-        idleTimeout = 1.minutes.toMillis()
-        maxLifetime = idleTimeout * 5
-    }
 
 private fun migrationConfig(conf: HikariConfig): HikariConfig =
     HikariConfig().apply {

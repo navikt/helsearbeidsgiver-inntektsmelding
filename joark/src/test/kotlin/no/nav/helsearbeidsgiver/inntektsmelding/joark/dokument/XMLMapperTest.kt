bb2e6249944a12681c84613fc4b619f44f2c909e
Alexander Petrov <alexander.petrov@nav.no>
2023-03-28 16:30:13 +0200

    jaxb og xml,mapper variant

--- joark/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/XMLMapperTest.kt
+++ joark/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/XMLMapperTest.kt
@@ -4,9 +4,12 @@ import io.kotest.equals.Equality
 import io.kotest.equals.byReflectionUsingFields
 import no.nav.helsearbeidsgiver.inntektsmelding.joark.mappers.InntektDokumentTilSkjemainnholdMapper
 import no.seres.xsd.nav.inntektsmelding_m._20181211.InntektsmeldingM
+import no.seres.xsd.nav.inntektsmelding_m._20181211.ObjectFactory
 import org.apache.commons.lang3.builder.EqualsBuilder
 import org.junit.jupiter.api.Test
 import org.mapstruct.factory.Mappers
+import java.io.StringWriter
+import javax.xml.bind.JAXBContext
 import kotlin.reflect.full.memberProperties
 
 class XMLMapperTest {
@@ -16,14 +19,22 @@ class XMLMapperTest {
         val mockInntektsmeldingDokument = MockInntektsmeldingDokument()
         val mapper = Mappers.getMapper(InntektDokumentTilSkjemainnholdMapper::class.java)
         val inntektM = mapper.InntektDokumentTilInntekstmeldingM(mockInntektsmeldingDokument)
-
-        val strIm = xmlMapper().writeValueAsString(inntektM)
+        val ctx = JAXBContext.newInstance(ObjectFactory::class.java)
+        val writer = StringWriter()
+        ctx.createMarshaller().marshal(ObjectFactory().createMelding(inntektM),writer)
+        println(writer.toString())
+        val strIm = xmlMapper().writeValueAsString(Melding(inntektM))
         println(strIm)
         val reverseImM = xmlMapper().readValue(strIm, InntektsmeldingM::class.java)
-       // Assertions.assertEquals(inntektM, reverseImM)
+        EqualsBuilder().setExcludeFields("serialVersionUID").reflectionAppend(
+            inntektM.skjemainnhold.sykepengerIArbeidsgiverperioden.arbeidsgiverperiodeListe,
+            reverseImM.skjemainnhold.sykepengerIArbeidsgiverperioden.arbeidsgiverperiodeListe
+        )
         EqualsBuilder().setTestRecursive(true).reflectionAppend(inntektM, reverseImM)
-            val result = Equality.byReflectionUsingFields<InntektsmeldingM>(*InntektsmeldingM::class.memberProperties.toTypedArray()).verify(inntektM,reverseImM)
+        val result = Equality.byReflectionUsingFields<InntektsmeldingM>(*InntektsmeldingM::class.memberProperties.toTypedArray()).verify(
+            inntektM,
+            reverseImM
+        )
         println(result)
-
     }
 }

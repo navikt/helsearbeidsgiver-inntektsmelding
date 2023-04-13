2ae30ce5f9f346f9956d7b60814d64746ddac96e
Morten Byhring <morten.byhring@nav.no>
2023-04-13 19:44:44 +0200

    lint

--- innsending/src/test/kotlin/no.nav.helsearbeidsgiver.inntektsmelding.innsending/KvitteringServiceTest.kt
+++ innsending/src/test/kotlin/no.nav.helsearbeidsgiver.inntektsmelding.innsending/KvitteringServiceTest.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-@Disabled     // Må starte lokal redis for å kjøre denne
+@Disabled // Må starte lokal redis for å kjøre denne
 class KvitteringServiceTest {
     private val redisStore = RedisStore("redis://localhost:6379/0")
 

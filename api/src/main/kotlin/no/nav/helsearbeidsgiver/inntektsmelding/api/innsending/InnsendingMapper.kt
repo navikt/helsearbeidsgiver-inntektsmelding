30979da45027dddda40d6852d0c92abdecd0331d
Alexander Petrov <alexander.petrov@nav.no>
2023-04-14 15:54:11 +0200

    InnsendingMapper

--- api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/innsending/InnsendingMapper.kt
+++ api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/innsending/InnsendingMapper.kt
@@ -7,6 +7,9 @@ import no.nav.helsearbeidsgiver.felles.Løsning
 import no.nav.helsearbeidsgiver.felles.NavnLøsning
 import no.nav.helsearbeidsgiver.felles.Resultat
 import no.nav.helsearbeidsgiver.felles.VirksomhetLøsning
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.InnsendingRequest
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Inntekt
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.InntektsmeldingDokument
 import no.nav.helsearbeidsgiver.inntektsmelding.api.mapper.ResultatMapper
 import no.nav.helsearbeidsgiver.inntektsmelding.api.validation.FeilmeldingConstraint
 import org.valiktor.ConstraintViolation
@@ -27,4 +30,24 @@ class InnsendingMapper(val uuid: String, resultat: Resultat) : ResultatMapper<In
     override fun getResultatResponse(): InnsendingResponse {
         return InnsendingResponse(uuid)
     }
+}
+
+fun mapInnsending(inntektsmeldingDokument: InntektsmeldingDokument): InnsendingRequest {
+    return InnsendingRequest(
+        inntektsmeldingDokument.orgnrUnderenhet,
+        inntektsmeldingDokument.identitetsnummer,
+        inntektsmeldingDokument.behandlingsdager,
+        inntektsmeldingDokument.egenmeldingsperioder,
+        inntektsmeldingDokument.arbeidsgiverperioder,
+        inntektsmeldingDokument.bestemmendeFraværsdag,
+        inntektsmeldingDokument.fraværsperioder,
+        // @TODO det er noe informasjon som vi mangler på InntektsMeldingDokument
+        Inntekt(true, inntektsmeldingDokument.beregnetInntekt, null, false),
+        inntektsmeldingDokument.fullLønnIArbeidsgiverPerioden,
+        inntektsmeldingDokument.refusjon,
+        inntektsmeldingDokument.naturalytelser,
+        inntektsmeldingDokument.årsakInnsending,
+        // @TODO  hva e value for dette
+        false
+    )
 }

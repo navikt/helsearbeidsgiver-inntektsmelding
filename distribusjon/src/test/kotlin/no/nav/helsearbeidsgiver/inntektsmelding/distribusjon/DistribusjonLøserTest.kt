12cf3759889b50d9bf1d9faaa45eb5758b3ddc62
Morten Laukvik <morten.laukvik@nav.no>
2023-04-19 11:48:59 +0200

    Bedre asserts i test

--- distribusjon/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/distribusjon/DistribusjonLøserTest.kt
+++ distribusjon/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/distribusjon/DistribusjonLøserTest.kt
@@ -10,6 +10,7 @@ import no.nav.helsearbeidsgiver.felles.json.customObjectMapper
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 import java.util.concurrent.CompletableFuture
 
@@ -38,10 +39,11 @@ internal class DistribusjonLøserTest {
         )
         sendMelding(mld)
         val melding = rapid.inspektør.message(0)
-        assertNotNull(melding)
+        assertNotNull(melding, "Skal publisere event at inntektsmelding er distribuert")
         assertEquals(EventName.INNTEKTSMELDING_DISTRIBUERT.name, melding.get(Key.EVENT_NAME.str).asText())
         assertEquals(JOURNALPOST_ID, melding.get(Key.JOURNALPOST_ID.str).asText())
         assertNotNull(melding.get(Key.INNTEKTSMELDING_DOKUMENT.str))
+        assertNull(melding.get(Key.FAIL.str), "Skal ikke inneholde feil")
     }
 
     @Test
@@ -56,7 +58,10 @@ internal class DistribusjonLøserTest {
             Key.INNTEKTSMELDING_DOKUMENT.str to "dummy"
         )
         sendMelding(mld)
-        assertEquals(1, rapid.inspektør.size)
+        val melding = rapid.inspektør.message(0)
+        assertNotNull(melding,"Skal publisere event at inntektsmelding IKKE ble distribuert")
+        assertEquals(EventName.INNTEKTSMELDING_JOURNALFOERT.name, melding.get(Key.EVENT_NAME.str).asText())
+        assertNotNull(melding.get(Key.FAIL.str).asText(), "Skal inneholde feil")
     }
 
     @Test
@@ -71,7 +76,10 @@ internal class DistribusjonLøserTest {
             Key.INNTEKTSMELDING_DOKUMENT.str to "dummy"
         )
         sendMelding(mld)
-        assertEquals(1, rapid.inspektør.size)
+        val melding = rapid.inspektør.message(0)
+        assertNotNull(melding,"Skal publisere event at inntektsmelding IKKE ble distribuert")
+        assertEquals(EventName.INNTEKTSMELDING_JOURNALFOERT.name, melding.get(Key.EVENT_NAME.str).asText())
+        assertNotNull(melding.get(Key.FAIL.str).asText(), "Skal inneholde feil")
     }
 
     private fun sendMelding(melding: Map<String, Any>) {

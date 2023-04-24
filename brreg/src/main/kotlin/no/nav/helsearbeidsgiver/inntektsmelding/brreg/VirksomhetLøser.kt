9bd93028a2a4c7498036149cf24ab3f7baea7fa9
Alexander Petrov <alexander.petrov@nav.no>
2023-04-24 16:54:56 +0200

    ha virksomhetløser å inheret fra Løser

--- brreg/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/brreg/VirksomhetLøser.kt
+++ brreg/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/brreg/VirksomhetLøser.kt
@@ -4,8 +4,6 @@ package no.nav.helsearbeidsgiver.inntektsmelding.brreg
 
 import kotlinx.coroutines.runBlocking
 import no.nav.helse.rapids_rivers.JsonMessage
-import no.nav.helse.rapids_rivers.MessageContext
-import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 import no.nav.helsearbeidsgiver.brreg.BrregClient
@@ -16,25 +14,13 @@ import no.nav.helsearbeidsgiver.felles.Key
 import no.nav.helsearbeidsgiver.felles.VirksomhetLøsning
 import no.nav.helsearbeidsgiver.felles.createFail
 import no.nav.helsearbeidsgiver.felles.publishFail
+import no.nav.helsearbeidsgiver.felles.rapidsrivers.Løser
 import org.slf4j.LoggerFactory
 
-class VirksomhetLøser(rapidsConnection: RapidsConnection, private val brregClient: BrregClient, private val isPreProd: Boolean) : River.PacketListener {
+class VirksomhetLøser(rapidsConnection: RapidsConnection, private val brregClient: BrregClient, private val isPreProd: Boolean) : Løser(rapidsConnection) {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
     private val BEHOV = BehovType.VIRKSOMHET
-
-    init {
-        River(rapidsConnection).apply {
-            validate {
-                it.demandAll(Key.BEHOV.str, BEHOV)
-                it.requireKey(Key.ID.str)
-                it.requireKey(Key.ORGNRUNDERENHET.str)
-                it.rejectKey(Key.LØSNING.str)
-                it.interestedIn(Key.UUID.str)
-                it.interestedIn(Key.FORESPOERSEL_ID.str)
-            }
-        }.register(this)
-    }
 
     fun hentVirksomhet(orgnr: String): String {
         if (isPreProd) {
@@ -49,27 +35,34 @@ class VirksomhetLøser(rapidsConnection: RapidsConnection, private val brregClie
         return runBlocking { brregClient.hentVirksomhetNavn(orgnr) } ?: throw FantIkkeVirksomhetException(orgnr)
     }
 
-    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+    override fun accept(): River.PacketValidation {
+        return River.PacketValidation {
+            it.demandAll(Key.BEHOV.str, BEHOV)
+            it.requireKey(Key.ORGNRUNDERENHET.str)
+        }
+    }
+
+    override fun onBehov(packet: JsonMessage) {
         logger.info("Løser behov $BEHOV med id ${packet[Key.ID.str].asText()}")
         val orgnr = packet[Key.ORGNRUNDERENHET.str].asText()
         try {
             val navn = hentVirksomhet(orgnr)
             logger.info("Fant $navn for $orgnr")
-            publiserLøsning(VirksomhetLøsning(navn), packet, context)
-            publishDatagram(navn, packet, context)
+            publiserLøsning(VirksomhetLøsning(navn), packet)
+            publishDatagram(navn, packet)
         } catch (ex: FantIkkeVirksomhetException) {
             logger.error("Fant ikke virksomhet for $orgnr")
-            publiserLøsning(VirksomhetLøsning(error = Feilmelding("Ugyldig virksomhet $orgnr")), packet, context)
-            publishFail(packet.createFail("Ugyldig virksomhet $orgnr", behoveType = BehovType.VIRKSOMHET), packet, context)
+            publiserLøsning(VirksomhetLøsning(error = Feilmelding("Ugyldig virksomhet $orgnr")), packet)
+            publishFail(packet.createFail("Ugyldig virksomhet $orgnr", behoveType = BehovType.VIRKSOMHET))
         } catch (ex: Exception) {
             logger.error("Det oppstod en feil ved henting for $orgnr")
             sikkerlogg.error("Det oppstod en feil ved henting for orgnr $orgnr: ", ex)
-            publiserLøsning(VirksomhetLøsning(error = Feilmelding("Klarte ikke hente virksomhet")), packet, context)
-            publishFail(packet.createFail("Klarte ikke hente virksomhet", behoveType = BehovType.VIRKSOMHET), packet, context)
+            publiserLøsning(VirksomhetLøsning(error = Feilmelding("Klarte ikke hente virksomhet")), packet)
+            publishFail(packet.createFail("Klarte ikke hente virksomhet", behoveType = BehovType.VIRKSOMHET))
         }
     }
 
-    fun publishDatagram(navn: String, jsonMessage: JsonMessage, context: MessageContext) {
+    fun publishDatagram(navn: String, jsonMessage: JsonMessage) {
         val message = JsonMessage.newMessage(
             mapOf(
                 Key.EVENT_NAME.str to jsonMessage[Key.EVENT_NAME.str].asText(),
@@ -78,12 +71,12 @@ class VirksomhetLøser(rapidsConnection: RapidsConnection, private val brregClie
                 DataFelt.VIRKSOMHET.str to navn
             )
         )
-        context.publish(message.toJson())
+        super.publishData(message)
     }
 
-    fun publiserLøsning(virksomhetLøsning: VirksomhetLøsning, packet: JsonMessage, context: MessageContext) {
+    fun publiserLøsning(virksomhetLøsning: VirksomhetLøsning, packet: JsonMessage) {
         packet.setLøsning(BEHOV, virksomhetLøsning)
-        context.publish(packet.toJson())
+        super.publishBehov(packet)
     }
 
     private fun JsonMessage.setLøsning(nøkkel: BehovType, data: Any) {
@@ -91,6 +84,4 @@ class VirksomhetLøser(rapidsConnection: RapidsConnection, private val brregClie
             nøkkel.name to data
         )
     }
-
-    override fun onError(problems: MessageProblems, context: MessageContext) {}
 }

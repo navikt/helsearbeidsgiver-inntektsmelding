3cb1aad54d282b9a749a43f997818324a3c69043
Alexander Petrov <alexander.petrov@nav.no>
2023-04-26 12:11:10 +0200

    som more automation

---
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/rapidsrivers/DataFields.kt
@@ -0,0 +1,6 @@
+package no.nav.helsearbeidsgiver.felles.rapidsrivers
+
+public class DataFields {
+    val IN : List<String> = mutableListOf()
+    val OUT : List<String> = mutableListOf()
+}
--- felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/rapidsrivers/DelegatingEventListener.kt
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/rapidsrivers/DelegatingEventListener.kt
@@ -4,7 +4,7 @@ import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 
-abstract class DelegatingEventListener(val mainListener: River.PacketListener, rapidsConnection: RapidsConnection) : EventListener(rapidsConnection) {
+abstract class DelegatingEventListener(open val mainListener: River.PacketListener, rapidsConnection: RapidsConnection) : EventListener(rapidsConnection) {
 
     override fun onEvent(packet: JsonMessage) {
         mainListener.onPacket(packet, rapidsConnection)

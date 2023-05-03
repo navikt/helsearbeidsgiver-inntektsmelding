461a1a891edf26f210de7ae51a2d8ab1125960b7
Alexander Petrov <alexander.petrov@nav.no>
2023-05-03 17:03:13 +0200

    Failhaandtering (#102)
    * infør TransaksjonOrigin og ta borte ubrukte løsning fra Journalføringen
    
    * fjern ubrukt kode
    
    * lint format
    
    * fiks integrasjonstestene

--- db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/LagreJournalpostIdLøser.kt
+++ db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/LagreJournalpostIdLøser.kt
@@ -67,7 +67,7 @@ class LagreJournalpostIdLøser(
         logger.info("Fant sakId $sakId for forespørselId $forespoerselId")
         val jsonMessage = JsonMessage.newMessage(
             mapOf(
-                Key.EVENT_NAME.str to EventName.INNTEKTSMELDING_JOURNALFOERT,
+                Key.EVENT_NAME.str to EventName.INNTEKTSMELDING_JOURNALFOERT.name,
                 Key.JOURNALPOST_ID.str to journalpostId,
                 Key.OPPGAVE_ID.str to oppgaveId!!, // TODO Lag bedre feilhåndtering dersom oppgaveId ikke ble funnet i db
                 Key.SAK_ID.str to sakId!!, // TODO Lag bedre feilhåndtering dersom oppgaveId ikke ble funnet i db
@@ -76,16 +76,6 @@ class LagreJournalpostIdLøser(
             )
         )
         publishEvent(jsonMessage)
-    }
-
-    fun publishLagret(uuid: String) {
-        val message = JsonMessage.newMessage(
-            mapOf(
-                Key.DATA.str to "Lagret",
-                Key.UUID.str to uuid
-            )
-        )
-        this.publishData(message)
     }
 
     fun publiserFeil(feilmelding: Feilmelding, packet: JsonMessage) {

5976c30eea9154ba14e2f68de8ad896b63769ef0
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2023-05-10 15:54:42 +0200

    Legg til type wrappers for orgnr og fnr (#105)
    * Legg til type wrappers for orgnr og fnr
    
    * Støtt testpersoner

---
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/TypeWrappers.kt
@@ -0,0 +1,33 @@
+package no.nav.helsearbeidsgiver.felles
+
+import kotlinx.serialization.Serializable
+
+private val orgnrRgx = Regex("\\d{9}")
+
+private val fnrRgx = Regex(
+    "(?:0[1-9]|[12]\\d|3[01])" + // to første siffer er gyldig dag
+        "(?:[048][1-9]|[159][012])" + // to neste siffer er gyldig måned, med støtte for testpersoner (+40 for NAV, +80 for TestNorge)
+        "\\d{7}" // resten er tall (siste del kan forbedres)
+)
+
+@Serializable
+@JvmInline
+value class Orgnr(val verdi: String) {
+    init {
+        require(verdi.matches(orgnrRgx))
+    }
+
+    override fun toString(): String =
+        verdi
+}
+
+@Serializable
+@JvmInline
+value class Fnr(val verdi: String) {
+    init {
+        require(verdi.matches(fnrRgx))
+    }
+
+    override fun toString(): String =
+        verdi
+}

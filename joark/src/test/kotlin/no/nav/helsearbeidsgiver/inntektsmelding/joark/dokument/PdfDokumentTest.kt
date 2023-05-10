921608c1dd9bec49512f4746c8fe4885e9b22bf6
Morten Laukvik <morten.laukvik@nav.no>
2023-05-10 09:31:59 +0200

    Få med alle varianter av inntekt endring årsak

--- joark/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/PdfDokumentTest.kt
+++ joark/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/PdfDokumentTest.kt
@@ -1,10 +1,21 @@
 package no.nav.helsearbeidsgiver.inntektsmelding.joark.dokument
 
 import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.BegrunnelseIngenEllerRedusertUtbetalingKode
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Bonus
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Ferie
 import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.FullLonnIArbeidsgiverPerioden
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Inntekt
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.InntektEndringAarsak
 import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.InntektsmeldingDokument
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.NyStilling
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.NyStillingsprosent
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Periode
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Permisjon
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Permittering
 import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Refusjon
 import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.RefusjonEndring
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Tariffendring
+import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.VarigLonnsendring
 import org.junit.jupiter.api.Test
 import java.io.File
 import java.io.FileOutputStream
@@ -89,6 +100,29 @@ internal class PdfDokumentTest {
                 )
             )
         )
+    }
+
+    @Test
+    fun `med inntekt endring årsak - alle varianter`() {
+        val perioder = listOf(Periode(dag, dag.plusDays(12)))
+        val map = HashMap<String, InntektEndringAarsak>()
+        map.put("tariffendring", Tariffendring(dag, dag.plusDays(2)))
+        map.put("ferie", Ferie(perioder))
+        map.put("variglonnsendring", VarigLonnsendring(dag))
+        map.put("nystilling", NyStilling(dag))
+        map.put("nystillingsprosent", NyStillingsprosent(dag))
+        map.put("bonus", Bonus())
+        map.put("permisjon", Permisjon(perioder))
+        map.put("permittering", Permittering(perioder))
+
+        map.forEach {
+            writePDF(
+                "med_inntekt_endring_${it.key}",
+                im.copy(
+                    inntekt = Inntekt(true, 123.0.toBigDecimal(), it.value, true)
+                )
+            )
+        }
     }
 
     fun writePDF(title: String, im: InntektsmeldingDokument) {

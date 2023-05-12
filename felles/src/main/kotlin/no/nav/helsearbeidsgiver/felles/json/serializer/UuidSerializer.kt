c91115e9c8887e154ab3130311b84fc4faf2f6ef
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2023-05-12 15:25:44 +0200

    Gjør opphav til serialiseringsfeil mer tydelig (#112)

--- felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/json/serializer/UuidSerializer.kt
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/json/serializer/UuidSerializer.kt
@@ -1,6 +1,7 @@
 package no.nav.helsearbeidsgiver.felles.json.serializer
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
@@ -10,5 +11,8 @@ import java.util.UUID
 object UuidSerializer : KSerializer<UUID> {
     override val descriptor = PrimitiveSerialDescriptor("java.util.UUID", PrimitiveKind.STRING)
     override fun serialize(encoder: Encoder, value: UUID) = value.toString().let(encoder::encodeString)
-    override fun deserialize(decoder: Decoder): UUID = decoder.decodeString().let(UUID::fromString)
+    override fun deserialize(decoder: Decoder): UUID =
+        decoder.decodeString()
+            .runCatching(UUID::fromString)
+            .getOrElse { throw SerializationException(it) }
 }

c1dce7ca45fd718d9e320a8b87abe17f3f7b7786
Alexander Petrov <alexander.petrov@nav.no>
2023-05-12 14:25:56 +0200

    fiks wildcard import

--- integrasjonstest/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/integrasjonstest/NotifikasjonTrengerInntektMeldingIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/integrasjonstest/NotifikasjonTrengerInntektMeldingIT.kt
@@ -20,7 +20,7 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
-import java.util.*
+import java.util.UUID
 
 class NotifikasjonTrengerInntektMeldingIT : EndToEndTest() {
 

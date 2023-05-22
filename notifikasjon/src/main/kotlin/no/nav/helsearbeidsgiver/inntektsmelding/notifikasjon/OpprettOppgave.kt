6da87e710309e03a06d87983d3a870922beed1be
Gustav Berggren <83211434+b162214@users.noreply.github.com>
2023-05-22 16:05:14 +0200

    Fiks manglende forespørselid ved oppgave notifikasjon (#125)
    * Bruk forespøreselId istedenfor randomgenerert uuid i linken

--- notifikasjon/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/OpprettOppgave.kt
+++ notifikasjon/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/OpprettOppgave.kt
@@ -4,12 +4,14 @@ import kotlinx.coroutines.runBlocking
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import no.nav.helse.rapids_rivers.isMissingOrNull
 import no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon.ArbeidsgiverNotifikasjonKlient
 import no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon.opprettNyOppgave
 import no.nav.helsearbeidsgiver.felles.BehovType
 import no.nav.helsearbeidsgiver.felles.DataFelt
 import no.nav.helsearbeidsgiver.felles.EventName
 import no.nav.helsearbeidsgiver.felles.Key
+import no.nav.helsearbeidsgiver.felles.createFail
 import no.nav.helsearbeidsgiver.felles.json.customObjectMapper
 import no.nav.helsearbeidsgiver.felles.rapidsrivers.EventListener
 import no.nav.helsearbeidsgiver.felles.rapidsrivers.Løser
@@ -24,10 +26,16 @@ class ForespørselLagretListener(rapidsConnection: RapidsConnection) : EventList
     }
     override fun onEvent(packet: JsonMessage) {
         val uuid: String = UUID.randomUUID().toString()
+        val forespørselId = packet[Key.FORESPOERSEL_ID.str]
+        if (forespørselId.isMissingOrNull() || forespørselId.asText().isEmpty()) {
+            publishFail(packet.createFail("Mangler forespørselId"))
+            return
+        }
         publishBehov(
             JsonMessage.newMessage(
                 mapOf(
                     Key.BEHOV.str to BehovType.OPPRETT_OPPGAVE,
+                    Key.FORESPOERSEL_ID.str to forespørselId,
                     Key.UUID.str to uuid,
                     Key.ORGNRUNDERENHET.str to packet[Key.ORGNRUNDERENHET.str]
                 )
@@ -47,18 +55,18 @@ class OpprettOppgaveLøser(
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     fun opprettOppgave(
-        uuid: String,
+        forespørselId: String,
         orgnr: String
     ): String { // ktlint-disable trailing-comma-on-declaration-site
         return runBlocking {
             arbeidsgiverNotifikasjonKlient.opprettNyOppgave(
-                uuid,
-                "$linkUrl/im-dialog/$uuid",
+                forespørselId,
+                "$linkUrl/im-dialog/$forespørselId",
                 "Send inn inntektsmelding",
                 orgnr,
                 "Inntektsmelding",
                 null,
-                uuid,
+                forespørselId,
                 "Nav trenger inntektsmelding",
                 "En av dine ansatte har sendt søknad for sykepenger og vi trenger inntektsmelding for å behandle " +
                     "søknaden. Logg inn på Min side – arbeidsgiver på nav.no"
@@ -75,18 +83,24 @@ class OpprettOppgaveLøser(
 
     override fun onBehov(packet: JsonMessage) {
         sikkerLogger.info("OpprettOppgaveLøser mottok pakke: ${packet.toJson()}")
+        val forespørselId = packet[Key.FORESPOERSEL_ID.str]
+        if (forespørselId.isMissingOrNull() || forespørselId.asText().isEmpty()) {
+            publishFail(packet.createFail("Mangler forespørselId"))
+            return
+        }
         val uuid = packet[Key.UUID.str].asText()
         val orgnr = packet[Key.ORGNRUNDERENHET.str].asText()
-        val oppgaveId = opprettOppgave(uuid, orgnr)
+        val oppgaveId = opprettOppgave(forespørselId.asText(), orgnr)
         val message = JsonMessage.newMessage(
             mapOf(
                 Key.BEHOV.str to BehovType.PERSISTER_OPPGAVE_ID.name,
+                Key.FORESPOERSEL_ID.str to forespørselId,
                 Key.UUID.str to uuid,
                 Key.ORGNRUNDERENHET.str to orgnr,
                 DataFelt.OPPGAVE_ID.str to oppgaveId
             )
         )
         publishBehov(message)
-        sikkerLogger.info("OpprettOppgaveLøser publiserte uuid $uuid med json: $message")
+        sikkerLogger.info("OpprettOppgaveLøser publiserte med json: $message")
     }
 }

6b2134fef55df7605ef7e9c2ef5d61b162e3db74
Gustav Berggren <gustav.berggren@nav.no>
2023-05-22 09:32:52 +0200

    Bruk forespøreselId istedenfor randomgenerert uuid i linken

--- notifikasjon/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/OpprettOppgave.kt
+++ notifikasjon/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/OpprettOppgave.kt
@@ -21,6 +21,7 @@ class ForespørselLagretListener(rapidsConnection: RapidsConnection) : EventList
 
     override fun accept(): River.PacketValidation = River.PacketValidation {
         it.requireKey(Key.ORGNRUNDERENHET.str)
+        it.requireKey(Key.FORESPOERSEL_ID.str)
     }
     override fun onEvent(packet: JsonMessage) {
         val uuid: String = UUID.randomUUID().toString()
@@ -28,6 +29,7 @@ class ForespørselLagretListener(rapidsConnection: RapidsConnection) : EventList
             JsonMessage.newMessage(
                 mapOf(
                     Key.BEHOV.str to BehovType.OPPRETT_OPPGAVE,
+                    Key.FORESPOERSEL_ID.str to packet[Key.FORESPOERSEL_ID.str],
                     Key.UUID.str to uuid,
                     Key.ORGNRUNDERENHET.str to packet[Key.ORGNRUNDERENHET.str]
                 )
@@ -47,18 +49,18 @@ class OpprettOppgaveLøser(
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     fun opprettOppgave(
-        uuid: String,
+        forespørselId: String,
         orgnr: String
     ): String { // ktlint-disable trailing-comma-on-declaration-site
         return runBlocking {
             arbeidsgiverNotifikasjonKlient.opprettNyOppgave(
-                uuid,
-                "$linkUrl/im-dialog/$uuid",
+                forespørselId,
+                "$linkUrl/im-dialog/$forespørselId",
                 "Send inn inntektsmelding",
                 orgnr,
                 "Inntektsmelding",
                 null,
-                uuid,
+                forespørselId,
                 "Nav trenger inntektsmelding",
                 "En av dine ansatte har sendt søknad for sykepenger og vi trenger inntektsmelding for å behandle " +
                     "søknaden. Logg inn på Min side – arbeidsgiver på nav.no"
@@ -71,22 +73,25 @@ class OpprettOppgaveLøser(
             it.demandValue(Key.EVENT_NAME.str, EVENT.name)
             it.demandValue(Key.BEHOV.str, BehovType.OPPRETT_OPPGAVE.name)
             it.interestedIn(Key.ORGNRUNDERENHET.str)
+            it.requireKey(Key.FORESPOERSEL_ID.str)
         }
 
     override fun onBehov(packet: JsonMessage) {
         sikkerLogger.info("OpprettOppgaveLøser mottok pakke: ${packet.toJson()}")
+        val forespørselId = packet[Key.FORESPOERSEL_ID.str].asText()
         val uuid = packet[Key.UUID.str].asText()
         val orgnr = packet[Key.ORGNRUNDERENHET.str].asText()
-        val oppgaveId = opprettOppgave(uuid, orgnr)
+        val oppgaveId = opprettOppgave(forespørselId, orgnr)
         val message = JsonMessage.newMessage(
             mapOf(
                 Key.BEHOV.str to BehovType.PERSISTER_OPPGAVE_ID.name,
+                Key.FORESPOERSEL_ID.str to forespørselId,
                 Key.UUID.str to uuid,
                 Key.ORGNRUNDERENHET.str to orgnr,
                 DataFelt.OPPGAVE_ID.str to oppgaveId
             )
         )
         publishBehov(message)
-        sikkerLogger.info("OpprettOppgaveLøser publiserte uuid $uuid med json: $message")
+        sikkerLogger.info("OpprettOppgaveLøser publiserte uuid $forespørselId med json: $message")
     }
 }

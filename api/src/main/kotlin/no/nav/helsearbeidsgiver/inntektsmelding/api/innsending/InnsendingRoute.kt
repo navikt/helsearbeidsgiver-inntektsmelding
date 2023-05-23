69ec2cd1bea5e244df47a79dfd423aad145d20f6
Alexander Petrov <alexander.petrov@nav.no>
2023-05-23 13:45:22 +0200

    debug

--- api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/innsending/InnsendingRoute.kt
+++ api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/innsending/InnsendingRoute.kt
@@ -1,9 +1,11 @@
 package no.nav.helsearbeidsgiver.inntektsmelding.api.innsending
 
 import com.fasterxml.jackson.databind.JsonMappingException
+import io.ktor.http.*
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.call
 import io.ktor.server.request.receiveText
+import io.ktor.server.response.*
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.InnsendingRequest
@@ -22,6 +24,7 @@ import no.nav.helsearbeidsgiver.inntektsmelding.api.utils.respondBadRequest
 import no.nav.helsearbeidsgiver.inntektsmelding.api.utils.respondInternalServerError
 import no.nav.helsearbeidsgiver.inntektsmelding.api.validation.ValidationResponse
 import no.nav.helsearbeidsgiver.inntektsmelding.api.validation.validationResponseMapper
+import no.nav.helsearbeidsgiver.utils.json.toJson
 import org.valiktor.ConstraintViolationException
 
 fun RouteExtra.InnsendingRoute() {
@@ -52,11 +55,16 @@ fun RouteExtra.InnsendingRoute() {
                 transaksjonId = producer.publish(forespoerselId, request)
                 logger.info("Publiserte til rapid med forespørselId: $forespoerselId og transaksjonId=$transaksjonId")
 
-                val resultat = redis.getResultat(transaksjonId, 10, 500)
+                val resultat = redis.getString(transaksjonId, 10, 500)  //.getResultat(transaksjonId, 10, 500)
                 sikkerLogger.info("Fikk resultat: $resultat")
 
-                val mapper = InnsendingMapper(forespoerselId, resultat)
-                respond(mapper.getStatus(), mapper.getResponse(), InnsendingResponse.serializer())
+            //    val mapper = InnsendingMapper(forespoerselId, resultat)
+                call.respond(
+                    status = HttpStatusCode.OK,
+                    message = resultat
+                )
+
+              //  respond(HttpStatusCode.OK, resultat) // mapper.getStatus(), mapper.getResponse(), InnsendingResponse.serializer())
             } catch (e: ConstraintViolationException) {
                 logger.info("Fikk valideringsfeil for forespørselId: $forespoerselId")
                 respondBadRequest(validationResponseMapper(e.constraintViolations), ValidationResponse.serializer())

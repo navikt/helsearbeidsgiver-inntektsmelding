862827397ab8d13bd10392d59b2354c139cdcffd
Mikael Bjerga <mikael.bjerga@nav.no>
2023-06-01 13:59:15 +0200

    Fjern eksludering av transitiv avhengighet fra tokenprovider

--- notifikasjon/build.gradle.kts
+++ notifikasjon/build.gradle.kts
@@ -1,8 +1,5 @@
-val ktorVersion: String by project
 val arbeidsgiverNotifikasjonKlientVersion: String by project
 
 dependencies {
-    // Nødvending for no.nav.helsearbeidsgiver.tokenprovider.DefaultOAuth2HttpClient, burde ikke være det
-    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
     implementation("no.nav.helsearbeidsgiver:arbeidsgiver-notifikasjon-klient:$arbeidsgiverNotifikasjonKlientVersion")
 }

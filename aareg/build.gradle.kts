d9d1c86910f76cd27d44397a3496805db385e8eb
Mikael Bjerga <mikael.bjerga@nav.no>
2023-06-01 12:07:27 +0200

    Legg til nødvendige transitive avhengigheter

--- aareg/build.gradle.kts
+++ aareg/build.gradle.kts
@@ -1,6 +1,8 @@
-
+val ktorVersion: String by project
 val aaregClientVersion: String by project
 
 dependencies {
+    // Nødvending for no.nav.helsearbeidsgiver.tokenprovider.DefaultOAuth2HttpClient, burde ikke være det
+    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
     implementation("no.nav.helsearbeidsgiver:aareg-client:$aaregClientVersion")
 }

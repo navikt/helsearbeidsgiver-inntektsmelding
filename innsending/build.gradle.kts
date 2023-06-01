d25b3880a902ace17d54fe4475848dfd362e7cc1
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2023-06-01 11:08:00 +0200

    Fjern ubrukte ktor-avhengigheter (#136)
    * Fjern ubrukte ktor-avhengigheter
    
    * Fjern unødvendig avhengighet

--- innsending/build.gradle.kts
+++ innsending/build.gradle.kts
@@ -1,12 +1,5 @@
-val ktorVersion: String by project
 val lettuceVersion: String by project
 
 dependencies {
     implementation("io.lettuce:lettuce-core:$lettuceVersion")
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("io.ktor:ktor-client-json:$ktorVersion")
-    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 }

489e9cf659ead9614699a382eb1a4f6eb59346b2
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2023-06-01 14:15:22 +0200

    Fjern eksludering av transitiv avhengighet fra tokenprovider (#140)

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -25,8 +25,8 @@ dependencies {
     implementation("no.nav.security:token-validation-ktor-v2:$tokenSupportVersion")
     implementation("org.valiktor:valiktor-core:$valiktorVersion")
 
-    testImplementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
     testImplementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    testImplementation("io.ktor:ktor-client-core:$ktorVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
     testImplementation("no.nav.security:mock-oauth2-server:$mockOauth2ServerVersion")
     testImplementation(project(":felles-test"))

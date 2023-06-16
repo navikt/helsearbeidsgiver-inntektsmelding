6ee7204314882bae02d62555d7be60ed65b5c048
Alexander Petrov <alexander.petrov@nav.no>
2023-06-16 11:32:27 +0200

    Endre navn på key interface (#161)

--- felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/rapidsrivers/RiverUtils.kt
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/rapidsrivers/RiverUtils.kt
@@ -5,48 +5,48 @@ import kotlinx.serialization.json.JsonElement
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helsearbeidsgiver.felles.BehovType
-import no.nav.helsearbeidsgiver.felles.WrappedKey
+import no.nav.helsearbeidsgiver.felles.IKey
 import no.nav.helsearbeidsgiver.felles.json.toJsonElement
 import no.nav.helsearbeidsgiver.felles.json.toJsonNode
 import no.nav.helsearbeidsgiver.utils.json.parseJson
 import no.nav.helsearbeidsgiver.utils.pipe.mapFirst
 
-fun JsonMessage.demandAll(key: WrappedKey, values: List<BehovType>) {
+fun JsonMessage.demandAll(key: IKey, values: List<BehovType>) {
     demandAll(key.str, values.map(BehovType::name))
 }
 
-fun JsonMessage.demandValues(vararg keyAndValuePairs: Pair<WrappedKey, String>) {
+fun JsonMessage.demandValues(vararg keyAndValuePairs: Pair<IKey, String>) {
     keyAndValuePairs.forEach { (key, value) ->
         demandValue(key.str, value)
     }
 }
 
-fun JsonMessage.demand(vararg keyAndParserPairs: Pair<WrappedKey, (JsonElement) -> Any>) {
-    val keyStringAndParserPairs = keyAndParserPairs.map { it.mapFirst(WrappedKey::str) }
+fun JsonMessage.demand(vararg keyAndParserPairs: Pair<IKey, (JsonElement) -> Any>) {
+    val keyStringAndParserPairs = keyAndParserPairs.map { it.mapFirst(IKey::str) }
     validate(JsonMessage::demand, keyStringAndParserPairs)
 }
 
-fun JsonMessage.rejectKeys(vararg keys: WrappedKey) {
-    val keysAsStr = keys.map(WrappedKey::str).toTypedArray()
+fun JsonMessage.rejectKeys(vararg keys: IKey) {
+    val keysAsStr = keys.map(IKey::str).toTypedArray()
     rejectKey(*keysAsStr)
 }
 
-fun JsonMessage.requireKeys(vararg keys: WrappedKey) {
-    val keysAsStr = keys.map(WrappedKey::str).toTypedArray()
+fun JsonMessage.requireKeys(vararg keys: IKey) {
+    val keysAsStr = keys.map(IKey::str).toTypedArray()
     requireKey(*keysAsStr)
 }
 
-fun JsonMessage.require(vararg keyAndParserPairs: Pair<WrappedKey, (JsonElement) -> Any>) {
-    val keyStringAndParserPairs = keyAndParserPairs.map { it.mapFirst(WrappedKey::str) }
+fun JsonMessage.require(vararg keyAndParserPairs: Pair<IKey, (JsonElement) -> Any>) {
+    val keyStringAndParserPairs = keyAndParserPairs.map { it.mapFirst(IKey::str) }
     validate(JsonMessage::require, keyStringAndParserPairs)
 }
 
-fun JsonMessage.interestedIn(vararg keys: WrappedKey) {
-    val keysAsStr = keys.map(WrappedKey::str).toTypedArray()
+fun JsonMessage.interestedIn(vararg keys: IKey) {
+    val keysAsStr = keys.map(IKey::str).toTypedArray()
     interestedIn(*keysAsStr)
 }
 
-fun MessageContext.publish(vararg messageFields: Pair<WrappedKey, JsonElement>): JsonElement =
+fun MessageContext.publish(vararg messageFields: Pair<IKey, JsonElement>): JsonElement =
     messageFields
         .associate { (key, value) -> key.str to value.toJsonNode() }
         .let(JsonMessage::newMessage)

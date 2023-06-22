0e73386fbccc62cef53e5195011b184ccced28ce
Mikael Bjerga <mikael.bjerga@nav.no>
2023-06-22 16:08:40 +0200

    Flytt test til modul hvor testet kode ligger

--- felles/src/test/kotlin/no/nav/helsearbeidsgiver/felles/ForespurtDataTest.kt
+++ felles/src/test/kotlin/no/nav/helsearbeidsgiver/felles/ForespurtDataTest.kt
@@ -1,11 +1,10 @@
-package no.nav.helsearbeidsgiver.inntektsmelding.helsebro.domene
+package no.nav.helsearbeidsgiver.felles
 
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.data.row
 import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
-import no.nav.helsearbeidsgiver.felles.ForespurtData
 import no.nav.helsearbeidsgiver.felles.test.mock.mockForespurtDataListe
 import no.nav.helsearbeidsgiver.felles.test.mock.mockForespurtDataMedFastsattInntektListe
 import no.nav.helsearbeidsgiver.felles.test.resource.readResource

ccea837bda1edb361e80373bb6c18a7c05b12aeb
ADEO\P141319 <alexander.petrov@nav.no>
2023-06-26 22:38:12 +0200

    log

--- akkumulator/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/trenger/TrengerService.kt
+++ akkumulator/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/trenger/TrengerService.kt
@@ -89,6 +89,7 @@ class TrengerService(private val rapidsConnection: RapidsConnection, override va
 
     override fun dispatchBehov(message: JsonMessage, transaction: Transaction) {
         val uuid = message[Key.UUID.str].asText()
+        sikkerLogger().info("Dispatcher for $uuid with trans state $transaction")
         if (transaction == Transaction.NEW) {
             sikkerLogger().info("Dispatcher HENT_TRENGER_IM for $uuid")
             sikkerLogger().info("${this.javaClass.simpleName} Dispatcher HENT_TRENGER_IM for $uuid")

e3b33ef66ccb87d5a7f9e4bd82a5bea9a7c3586b
Mikael Bjerga <mikael.bjerga@nav.no>
2023-06-29 17:12:40 +0200

    Integrasjonstest: Bruk kotlinx-serialisering i test av innsending

--- integrasjonstest/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/integrasjonstest/InnsendingIT.kt
+++ integrasjonstest/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/integrasjonstest/InnsendingIT.kt
@@ -1,5 +1,7 @@
 package no.nav.helsearbeidsgiver.inntektsmelding.integrasjonstest
 
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import no.nav.helsearbeidsgiver.dokarkiv.OpprettJournalpostResponse
 import no.nav.helsearbeidsgiver.felles.BehovType
@@ -8,164 +10,164 @@ import no.nav.helsearbeidsgiver.felles.EventName
 import no.nav.helsearbeidsgiver.felles.Key
 import no.nav.helsearbeidsgiver.felles.OppgaveFerdigLøsning
 import no.nav.helsearbeidsgiver.felles.SakFerdigLøsning
-import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.BegrunnelseIngenEllerRedusertUtbetalingKode
-import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.FullLonnIArbeidsgiverPerioden
-import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.InnsendingRequest
-import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Inntekt
-import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Naturalytelse
-import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.NaturalytelseKode
-import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Refusjon
-import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.ÅrsakInnsending
-import no.nav.helsearbeidsgiver.felles.json.toJsonElement
+import no.nav.helsearbeidsgiver.felles.json.toJson
+import no.nav.helsearbeidsgiver.felles.test.json.fromJsonMapOnlyDatafelter
+import no.nav.helsearbeidsgiver.felles.test.json.fromJsonMapOnlyKeys
+import no.nav.helsearbeidsgiver.inntektsmelding.integrasjonstest.mock.mockInnsendingRequest
 import no.nav.helsearbeidsgiver.inntektsmelding.integrasjonstest.utils.EndToEndTest
+import no.nav.helsearbeidsgiver.inntektsmelding.integrasjonstest.utils.Jackson
+import no.nav.helsearbeidsgiver.inntektsmelding.integrasjonstest.utils.fromJsonToString
+import no.nav.helsearbeidsgiver.inntektsmelding.integrasjonstest.utils.lesLoesning
 import no.nav.helsearbeidsgiver.utils.json.fromJson
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.DisplayName
+import no.nav.helsearbeidsgiver.utils.json.serializer.UuidSerializer
+import no.nav.helsearbeidsgiver.utils.json.toJson
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.UUID
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@DisplayName("Innsending av skjema fra frontend")
 class InnsendingIT : EndToEndTest() {
-
-    private val FNR = "fnr-123"
-    private val ORGNR = "orgnr-456"
-    private val SAK_ID = "sak_id_123"
-    private val OPPGAVE_ID = "oppgave_id_456"
-    private val FORESPØRSEL_ID = UUID.randomUUID().toString()
-    private val CLIENT_ID = UUID.randomUUID().toString()
-    private val REQUEST = mockRequest()
-    private val JOURNALPOST_ID = "jp-789"
-
-    private fun setup() {
-        forespoerselRepository.lagreForespørsel(FORESPØRSEL_ID, ORGNR)
-        forespoerselRepository.oppdaterSakId(SAK_ID, FORESPØRSEL_ID)
-        forespoerselRepository.oppdaterOppgaveId(FORESPØRSEL_ID, OPPGAVE_ID)
-
-        // Mocking
-        coEvery {
-            arbeidsgiverNotifikasjonKlient.opprettNySak(any(), any(), any(), any(), any(), any(), any())
-        } answers {
-            SAK_ID
-        }
-        coEvery {
-            arbeidsgiverNotifikasjonKlient.opprettNyOppgave(any(), any(), any(), any(), any(), any(), any(), any(), any())
-        } answers {
-            OPPGAVE_ID
-        }
-        coEvery {
-            dokarkivClient.opprettJournalpost(any(), any(), any())
-        } answers {
-            OpprettJournalpostResponse(JOURNALPOST_ID, journalpostFerdigstilt = true, "FERDIGSTILT", "", emptyList())
-        }
-    }
 
     @Test
     fun `skal ta imot forespørsel ny inntektsmelding, deretter opprette sak og oppgave`() {
-        setup()
-        publish(
-            mapOf(
-                Key.EVENT_NAME.str to EventName.INSENDING_STARTED.name,
-                Key.OPPRETTET.str to LocalDateTime.now(),
-                Key.CLIENT_ID to CLIENT_ID,
-                Key.FORESPOERSEL_ID.str to FORESPØRSEL_ID,
-                DataFelt.ORGNRUNDERENHET.str to REQUEST.orgnrUnderenhet,
-                Key.IDENTITETSNUMMER.str to REQUEST.identitetsnummer,
-                DataFelt.INNTEKTSMELDING.str to REQUEST
-            )
+        forespoerselRepository.lagreForespørsel(Mock.forespoerselId.toString(), Mock.innsendingRequest.orgnrUnderenhet)
+        forespoerselRepository.oppdaterSakId(Mock.SAK_ID, Mock.forespoerselId.toString())
+        forespoerselRepository.oppdaterOppgaveId(Mock.forespoerselId.toString(), Mock.OPPGAVE_ID)
+
+        coEvery { arbeidsgiverNotifikasjonKlient.opprettNySak(any(), any(), any(), any(), any(), any(), any()) } returns Mock.SAK_ID
+
+        coEvery { arbeidsgiverNotifikasjonKlient.opprettNyOppgave(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Mock.OPPGAVE_ID
+
+        coEvery { dokarkivClient.opprettJournalpost(any(), any(), any()) } returns OpprettJournalpostResponse(
+            journalpostId = Mock.JOURNALPOST_ID,
+            journalpostFerdigstilt = true,
+            journalStatus = "FERDIGSTILT",
+            melding = "",
+            dokumenter = emptyList()
         )
+
+        publishMessage(
+            Key.EVENT_NAME to EventName.INSENDING_STARTED.toJson(),
+            Key.OPPRETTET to LocalDateTime.now().toJson(),
+            Key.CLIENT_ID to UUID.randomUUID().toJson(),
+            Key.FORESPOERSEL_ID to Mock.forespoerselId.toJson(),
+            DataFelt.ORGNRUNDERENHET to Mock.innsendingRequest.orgnrUnderenhet.toJson(),
+            Key.IDENTITETSNUMMER to Mock.innsendingRequest.identitetsnummer.toJson(),
+            DataFelt.INNTEKTSMELDING to Mock.innsendingRequest.let(Jackson::toJson)
+        )
+
         Thread.sleep(10000)
 
-        assertNotNull(messages)
+        messages.filter(EventName.INSENDING_STARTED)
+            .filter(DataFelt.INNTEKTSMELDING_DOKUMENT)
+            .first()
+            .fromJsonMapOnlyDatafelter()
+            .also {
+                // Ble lagret i databasen
+                it[DataFelt.INNTEKTSMELDING_DOKUMENT].shouldNotBeNull()
+            }
 
-        with(filter(EventName.INSENDING_STARTED, datafelt = DataFelt.INNTEKTSMELDING_DOKUMENT).first()) {
-            // Ble lagret i databasen
-            assertNotNull(get(DataFelt.INNTEKTSMELDING_DOKUMENT.str))
-        }
+        messages.filter(EventName.INNTEKTSMELDING_MOTTATT)
+            .filter(BehovType.JOURNALFOER, loesningPaakrevd = false)
+            .first()
+            .fromJsonMapOnlyKeys()
+            .also {
+                // Journalført i dokarkiv
+                it[Key.FORESPOERSEL_ID]?.fromJson(UuidSerializer) shouldBe Mock.forespoerselId
+            }
 
-        with(filter(EventName.INNTEKTSMELDING_MOTTATT, BehovType.JOURNALFOER, løsning = false).first()) {
-            // Journalført i dokarkiv
-            assertEquals(FORESPØRSEL_ID, get(Key.FORESPOERSEL_ID.str).asText())
-        }
-        with(filter(EventName.INNTEKTSMELDING_MOTTATT, BehovType.LAGRE_JOURNALPOST_ID, løsning = false).first()) {
-            // Journalført i dokarkiv
-            assertEquals(FORESPØRSEL_ID, get(Key.FORESPOERSEL_ID.str).asText())
-            assertEquals(JOURNALPOST_ID, get(Key.JOURNALPOST_ID.str).asText())
-        }
+        messages.filter(EventName.INNTEKTSMELDING_MOTTATT)
+            .filter(BehovType.LAGRE_JOURNALPOST_ID, loesningPaakrevd = false)
+            .first()
+            .fromJsonMapOnlyKeys()
+            .also {
+                // Journalført i dokarkiv
+                it[Key.FORESPOERSEL_ID]?.fromJson(UuidSerializer) shouldBe Mock.forespoerselId
+                it[Key.JOURNALPOST_ID]?.fromJsonToString() shouldBe Mock.JOURNALPOST_ID
+            }
 
-        with(filter(EventName.INNTEKTSMELDING_MOTTATT, null, løsning = false).first()) {
-            // EVENT: Mottatt inntektsmelding
-            assertEquals(FORESPØRSEL_ID, get(Key.FORESPOERSEL_ID.str).asText())
-        }
+        messages.filter(EventName.INNTEKTSMELDING_MOTTATT)
+            .first()
+            .fromJsonMapOnlyKeys()
+            .also {
+                // EVENT: Mottatt inntektsmelding
+                it[Key.FORESPOERSEL_ID]?.fromJson(UuidSerializer) shouldBe Mock.forespoerselId
+            }
 
-        with(filter(EventName.INNTEKTSMELDING_JOURNALFOERT, null, løsning = false).first()) {
-            // EVENT: Journalføring
-            assertEquals(JOURNALPOST_ID, get(Key.JOURNALPOST_ID.str).asText())
-            assertEquals(FORESPØRSEL_ID, get(Key.FORESPOERSEL_ID.str).asText())
-            assertEquals(OPPGAVE_ID, get(DataFelt.OPPGAVE_ID.str).asText())
-        }
+        messages.filter(EventName.INNTEKTSMELDING_JOURNALFOERT)
+            .first()
+            .also {
+                // EVENT: Journalføring
+                val oppgaveId = it.fromJsonMapOnlyDatafelter()
+                    .get(DataFelt.OPPGAVE_ID)
+                    ?.fromJsonToString()
 
-        with(filter(EventName.INNTEKTSMELDING_JOURNALFOERT, BehovType.DISTRIBUER_IM, løsning = false).first()) {
-            // Be om å distribuere
-            assertEquals(JOURNALPOST_ID, get(Key.JOURNALPOST_ID.str).asText())
-        }
+                oppgaveId shouldBe Mock.OPPGAVE_ID
 
-        with(filter(EventName.INNTEKTSMELDING_JOURNALFOERT, BehovType.ENDRE_SAK_STATUS, løsning = true).first()) {
-            // Endre status for arbeidsgivernotifikasjon sak
-            assertEquals(SAK_ID, get(DataFelt.SAK_ID.str).asText())
-            val løsning: SakFerdigLøsning =
-                get(Key.LØSNING.str).get(BehovType.ENDRE_SAK_STATUS.name).toJsonElement().fromJson(SakFerdigLøsning.serializer())
-            assertEquals(SAK_ID, løsning.value)
-        }
+                val msgKeyValues = it.fromJsonMapOnlyKeys()
 
-        with(filter(EventName.INNTEKTSMELDING_JOURNALFOERT, BehovType.ENDRE_OPPGAVE_STATUS, løsning = true).first()) {
-            // Endre status for arbeidsgivernotifikasjon oppgave
-            assertEquals(OPPGAVE_ID, get(DataFelt.OPPGAVE_ID.str).asText())
-            val løsning: OppgaveFerdigLøsning =
-                get(Key.LØSNING.str).get(BehovType.ENDRE_OPPGAVE_STATUS.name).toJsonElement().fromJson(OppgaveFerdigLøsning.serializer())
-            assertEquals(OPPGAVE_ID, løsning.value)
-        }
+                msgKeyValues[Key.JOURNALPOST_ID]?.fromJsonToString() shouldBe Mock.JOURNALPOST_ID
+                msgKeyValues[Key.FORESPOERSEL_ID]?.fromJson(UuidSerializer) shouldBe Mock.forespoerselId
+            }
 
-        with(filter(EventName.INNTEKTSMELDING_DISTRIBUERT).first()) {
-            // Verifiser at inntektsmelding er distribuert på ekstern kafka
-            assertEquals(JOURNALPOST_ID, get(Key.JOURNALPOST_ID.str).asText())
-            assertNotNull(get(DataFelt.INNTEKTSMELDING_DOKUMENT.str))
-        }
+        messages.filter(EventName.INNTEKTSMELDING_JOURNALFOERT)
+            .filter(BehovType.DISTRIBUER_IM, loesningPaakrevd = false)
+            .first()
+            .fromJsonMapOnlyKeys()
+            .also {
+                // Be om å distribuere
+                it[Key.JOURNALPOST_ID]?.fromJsonToString() shouldBe Mock.JOURNALPOST_ID
+            }
+
+        messages.filter(EventName.INNTEKTSMELDING_JOURNALFOERT)
+            .filter(BehovType.ENDRE_SAK_STATUS, loesningPaakrevd = true)
+            .first()
+            .also {
+                // Endre status for arbeidsgivernotifikasjon sak
+                val sakId = it.fromJsonMapOnlyDatafelter()[DataFelt.SAK_ID]?.fromJsonToString()
+
+                sakId shouldBe Mock.SAK_ID
+
+                val loesning = it.fromJsonMapOnlyKeys()
+                    .lesLoesning(BehovType.ENDRE_SAK_STATUS, SakFerdigLøsning.serializer())
+
+                loesning?.value shouldBe Mock.SAK_ID
+            }
+
+        messages.filter(EventName.INNTEKTSMELDING_JOURNALFOERT)
+            .filter(BehovType.ENDRE_OPPGAVE_STATUS, loesningPaakrevd = true)
+            .first()
+            .also {
+                // Endre status for arbeidsgivernotifikasjon oppgave
+                val oppgaveId = it.fromJsonMapOnlyDatafelter()[DataFelt.OPPGAVE_ID]?.fromJsonToString()
+
+                oppgaveId shouldBe Mock.OPPGAVE_ID
+
+                val loesning = it.fromJsonMapOnlyKeys()
+                    .lesLoesning(BehovType.ENDRE_OPPGAVE_STATUS, OppgaveFerdigLøsning.serializer())
+
+                loesning?.value shouldBe Mock.OPPGAVE_ID
+            }
+
+        messages.filter(EventName.INNTEKTSMELDING_DISTRIBUERT)
+            .first()
+            .also { msg ->
+                // Verifiser at inntektsmelding er distribuert på ekstern kafka
+                val journalpostId = msg.fromJsonMapOnlyKeys()[Key.JOURNALPOST_ID]?.fromJsonToString()
+
+                journalpostId shouldBe Mock.JOURNALPOST_ID
+
+                msg.fromJsonMapOnlyDatafelter()[DataFelt.INNTEKTSMELDING_DOKUMENT].shouldNotBeNull()
+            }
     }
 
-    private fun mockRequest(): InnsendingRequest {
-        return InnsendingRequest(
-            ORGNR,
-            FNR,
-            listOf(LocalDate.now().plusDays(5)),
-            listOf(
-                no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.Periode(
-                    LocalDate.now(),
-                    LocalDate.now().plusDays(2)
-                )
-            ),
-            emptyList(),
-            LocalDate.now(),
-            emptyList(),
-            Inntekt(true, 32100.0.toBigDecimal(), endringÅrsak = null, false),
-            FullLonnIArbeidsgiverPerioden(
-                true,
-                BegrunnelseIngenEllerRedusertUtbetalingKode.ARBEID_OPPHOERT
-            ),
-            Refusjon(true, 200.0.toBigDecimal(), LocalDate.now()),
-            listOf(
-                Naturalytelse(
-                    NaturalytelseKode.KOSTDOEGN,
-                    LocalDate.now(),
-                    300.0.toBigDecimal()
-                )
-            ),
-            ÅrsakInnsending.ENDRING,
-            true
-        )
+    private object Mock {
+        const val SAK_ID = "sak-id-skoleboller"
+        const val OPPGAVE_ID = "oppgave-id-skoleboller"
+        const val JOURNALPOST_ID = "jorunalpost-id-skoleboller"
+
+        val forespoerselId = UUID.randomUUID()
+        val innsendingRequest = mockInnsendingRequest()
     }
 }

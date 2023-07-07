a0773eabc8782da56ad13296f8b2ebf7847ec4bd
Mikael Bjerga <mikael.bjerga@nav.no>
2023-07-07 14:02:33 +0200

    Publiser meldinger til akkumulator og service uavhengig av hverandre

--- helsebro/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/helsebro/ForespoerselSvarLøser.kt
+++ helsebro/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/helsebro/ForespoerselSvarLøser.kt
@@ -77,44 +77,40 @@ class ForespoerselSvarLøser(rapid: RapidsConnection) : River.PacketListener {
         val loesning = forespoerselSvar.toHentTrengerImLøsning()
         val resultat = loesning.value
 
-        when {
-            initiateEvent != EventName.TRENGER_REQUESTED -> {
-                context.publish(
-                    Key.EVENT_NAME to initiateEvent.toJson(),
-                    Key.BEHOV to listOf(BehovType.HENT_TRENGER_IM).toJson(BehovType.serializer()),
-                    Key.LØSNING to mapOf(
-                        BehovType.HENT_TRENGER_IM to loesning
-                    ).toJson(),
-                    Key.BOOMERANG to forespoerselSvar.boomerang
-                )
-                logger.info("Publiserte løsning for [${BehovType.HENT_TRENGER_IM}].")
-            }
+        if (initiateEvent != EventName.TRENGER_REQUESTED) {
+            context.publish(
+                Key.EVENT_NAME to initiateEvent.toJson(),
+                Key.BEHOV to listOf(BehovType.HENT_TRENGER_IM).toJson(BehovType.serializer()),
+                Key.LØSNING to mapOf(
+                    BehovType.HENT_TRENGER_IM to loesning
+                ).toJson(),
+                Key.BOOMERANG to forespoerselSvar.boomerang
+            )
+            logger.info("Publiserte løsning for [${BehovType.HENT_TRENGER_IM}].")
+        }
 
-            resultat != null -> {
-                context.publish(
-                    Key.EVENT_NAME to initiateEvent.toJson(),
-                    Key.DATA to "".toJson(),
-                    Key.UUID to transactionID,
-                    DataFelt.FORESPOERSEL_SVAR to resultat.toJson(TrengerInntekt.serializer())
-                )
+        if (resultat != null) {
+            context.publish(
+                Key.EVENT_NAME to initiateEvent.toJson(),
+                Key.DATA to "".toJson(),
+                Key.UUID to transactionID,
+                DataFelt.FORESPOERSEL_SVAR to resultat.toJson(TrengerInntekt.serializer())
+            )
 
-                logger.info("Recieve answer from helsebro for " + forespoerselSvar.forespoerselId + " current time" + System.currentTimeMillis())
-                logger.info("Publiserte data for [${BehovType.HENT_TRENGER_IM}].")
-            }
+            logger.info("Recieve answer from helsebro for " + forespoerselSvar.forespoerselId + " current time" + System.currentTimeMillis())
+            logger.info("Publiserte data for [${BehovType.HENT_TRENGER_IM}].")
+        } else {
+            val feil = loesning.error?.melding ?: FEIL_TOMT_SVAR
 
-            else -> {
-                val feil = loesning.error?.melding ?: FEIL_TOMT_SVAR
-
-                context.publish(
-                    Fail(
-                        eventName = initiateEvent,
-                        behov = BehovType.HENT_TRENGER_IM,
-                        feilmelding = feil,
-                        forespørselId = forespoerselSvar.forespoerselId.toString(),
-                        uuid = transactionID.toJsonNode().asText()
-                    ).toJsonMessage().toJson()
-                )
-            }
+            context.publish(
+                Fail(
+                    eventName = initiateEvent,
+                    behov = BehovType.HENT_TRENGER_IM,
+                    feilmelding = feil,
+                    forespørselId = forespoerselSvar.forespoerselId.toString(),
+                    uuid = transactionID.toJsonNode().asText()
+                ).toJsonMessage().toJson()
+            )
         }
     }
 }

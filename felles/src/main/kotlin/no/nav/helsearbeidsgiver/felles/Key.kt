e3e74834b057f696ad0aaeb6f3351d5b1f6e776d
Mikael Bjerga <mikael.bjerga@nav.no>
2023-07-11 18:23:17 +0200

    Bruk forbedret måte å lese alle enum-verdier

--- felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/Key.kt
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/Key.kt
@@ -45,7 +45,7 @@ enum class Key(override val str: String) : IKey {
 
     companion object {
         internal fun fromJson(json: String): Key =
-            Key.values().firstOrNull {
+            Key.entries.firstOrNull {
                 json == it.str
             }
                 ?: throw IllegalArgumentException("Fant ingen Key med verdi som matchet '$json'.")
@@ -76,7 +76,7 @@ enum class DataFelt(override val str: String) : IKey {
 
     companion object {
         internal fun fromJson(json: String): DataFelt =
-            DataFelt.values().firstOrNull {
+            DataFelt.entries.firstOrNull {
                 json == it.str
             }
                 ?: throw IllegalArgumentException("Fant ingen DataFelt med verdi som matchet '$json'.")

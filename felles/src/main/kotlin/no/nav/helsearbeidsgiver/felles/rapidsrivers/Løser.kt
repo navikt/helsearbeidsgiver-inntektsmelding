5437d78088280a53ee064fcf4e8b544357a565d9
Alexander Petrov <alexander.petrov@nav.no>
2023-08-24 11:18:48 +0200

    fiks feil merge

--- felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/rapidsrivers/Løser.kt
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/rapidsrivers/Løser.kt
@@ -46,13 +46,6 @@ abstract class Løser(val rapidsConnection: RapidsConnection) : River.PacketList
         }
         rapidsConnection.publish(message.toJson())
     }
-    fun publishBehov(behov: Behov) {
-        behov.toJsonMessage().also { rapidsConnection.publish(it.toJson()) }
-            .also {
-                logger.info("Publiserte behov ${behov.behov} for eventname ${behov.event.name} and uuid ${behov.uuid()}'.")
-                sikkerLogger.info("Publiserte data:\n${it.toPretty()}")
-            }
-    }
 
     fun publishBehov(behov: Behov) {
         behov.toJsonMessage().also {
@@ -60,7 +53,7 @@ abstract class Løser(val rapidsConnection: RapidsConnection) : River.PacketList
         }
             .also {
                 logger.info("Publiserte behov ${behov.behov} for eventname ${behov.event.name} and uuid ${behov.uuid()}'.")
-                sikkerLogger.info("Publiserte data:\n${it.toPretty()}")
+                sikkerLogger.info("Publiserte behov:\n${it.toPretty()}")
             }
     }
 

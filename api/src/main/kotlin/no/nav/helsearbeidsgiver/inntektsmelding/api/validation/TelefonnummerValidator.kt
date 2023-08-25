ac5ba0df68bd306fea8f78f585e518965fca899f
Morten Byhring <morten.byhring@nav.no>
2023-08-25 11:44:33 +0200

    valider telefonnummer

---
+++ api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/validation/TelefonnummerValidator.kt
@@ -0,0 +1,28 @@
+package no.nav.helsearbeidsgiver.inntektsmelding.api.validation
+
+class TelefonnummerValidator(input: String?) {
+
+    init {
+        require(input != null)
+        require(isEightDigits(input) || startsWithPlus(input) || isTwelveDigits(input)) { "Ikke et gyldig telefonnummer" }
+    }
+
+    private fun startsWithPlus(input: String): Boolean {
+        return """\+\d{10}""".toRegex().matches(input)
+    }
+
+    private fun isEightDigits(input: String) = """\d{8}""".toRegex().matches(input)
+
+    private fun isTwelveDigits(input: String) = """00\d{10}""".toRegex().matches(input)
+
+    companion object {
+        fun isValid(telefonnummer: String?): Boolean {
+            return try {
+                TelefonnummerValidator(telefonnummer)
+                true
+            } catch (t: Throwable) {
+                false
+            }
+        }
+    }
+}

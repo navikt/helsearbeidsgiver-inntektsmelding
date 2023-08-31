e99e49b043136b9face2bf508f9c474a86b0bb8f
mortenbyhring <117746154+mortenbyhring@users.noreply.github.com>
2023-08-31 13:11:49 +0200

    idporten upgrade (#263)
    Fjerne gammel loginservice, bruk idporten med sidecar:
    * idporten / tokensupport upgrade til 3.1.4
    * tokenProvider versjon oppgradert til 0.3.0
    * ikke bruk config map for loginservice
    * Bytt navn til idporten istedenfor loginservice. (Så vi ikke dukker opp i skammeliste på slack)
    ---------
    
    Co-authored-by: Gustav Berggren <gustav.berggren@nav.no>

--- api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/Env.kt
+++ api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/Env.kt
@@ -4,8 +4,8 @@ import no.nav.helsearbeidsgiver.felles.fromEnv
 
 object Env {
     object Auth {
-        val discoveryUrl: String = "LOGINSERVICE_IDPORTEN_DISCOVERY_URL".fromEnv()
-        val acceptedAudience: List<String> = "LOGINSERVICE_IDPORTEN_AUDIENCE".fromEnv().let(::listOf)
+        val discoveryUrl: String = "IDPORTEN_WELL_KNOWN_URL".fromEnv()
+        val acceptedAudience: List<String> = "IDPORTEN_AUDIENCE".fromEnv().let(::listOf)
     }
 
     object Redis {

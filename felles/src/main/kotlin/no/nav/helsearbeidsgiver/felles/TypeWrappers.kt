8be25d87a1e18c32143590ef860f94d9f22610c9
Mikael Bjerga <mikael.bjerga@nav.no>
2023-09-11 14:45:01 +0200

    Oppdater kommentar

--- felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/TypeWrappers.kt
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/TypeWrappers.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 private val orgnrRgx = Regex("\\d{9}")
 
 private val fnrRgx = Regex(
-    "(?:[04][1-9]|[1256]\\d|[37][01])" + // to første siffer er gyldig dag
+    "(?:[04][1-9]|[1256]\\d|[37][01])" + // to første siffer er gyldig dag (+40 for D-nummer)
         "(?:[048][1-9]|[159][012])" + // to neste siffer er gyldig måned, med støtte for testpersoner (+40 for NAV, +80 for TestNorge)
         "\\d{7}" // resten er tall (siste del kan forbedres)
 )

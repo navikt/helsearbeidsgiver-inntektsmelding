cb2920423d5dc163c53258797aae38ffa660c525
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2023-09-26 13:20:34 +0200

    Lagre inntektsmelding som jsonb (#279)
    * Lagre inntektsmelding som jsonb
    
    * Oppdater navn på migrering

--- db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/config/InntektsmeldingEntitet.kt
+++ db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/config/InntektsmeldingEntitet.kt
@@ -4,8 +4,6 @@ import no.nav.helsearbeidsgiver.felles.EksternInntektsmelding
 import no.nav.helsearbeidsgiver.felles.inntektsmelding.felles.models.InntektsmeldingDokument
 import no.nav.helsearbeidsgiver.felles.json.Jackson
 import no.nav.helsearbeidsgiver.utils.json.jsonIgnoreUnknown
-import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.ColumnType
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.datetime
 import org.jetbrains.exposed.sql.json.jsonb
@@ -15,34 +13,13 @@ object InntektsmeldingEntitet : Table("inntektsmelding") {
         idSeqName = "inntektsmelding_id_seq"
     )
     val forespoerselId = varchar(name = "forespoersel_id", length = 40) references ForespoerselEntitet.forespoerselId
-    val dokument = json("dokument", InntektsmeldingDokument::class.java).nullable()
+    val dokument = jsonb<InntektsmeldingDokument>(
+        name = "dokument",
+        serialize = Jackson::toJson,
+        deserialize = Jackson::fromJson
+    ).nullable()
     val eksternInntektsmelding = jsonb<EksternInntektsmelding>("ekstern_inntektsmelding", jsonIgnoreUnknown).nullable()
     val innsendt = datetime("innsendt")
     val journalpostId = varchar("journalpostid", 30).nullable()
     override val primaryKey = PrimaryKey(id, name = "id")
-}
-
-private fun <T : Any> Table.json(
-    name: String,
-    clazz: Class<T>
-): Column<T> =
-    registerColumn(
-        name = name,
-        type = JsonColumnType(clazz)
-    )
-
-class JsonColumnType<T : Any>(private val clazz: Class<T>) : ColumnType() {
-    override fun sqlType(): String =
-        "jsonb"
-
-    override fun valueFromDB(value: Any): T = Jackson.objectMapper.readValue(value as String, clazz)
-
-    override fun notNullValueToDB(value: Any): String =
-        Jackson.toJson(value)
-
-    override fun valueToString(value: Any?): String =
-        when (value) {
-            is Iterable<*> -> notNullValueToDB(value)
-            else -> super.valueToString(value)
-        }
 }

7e845a4be614eb895bcb33be6f5ddd39f894ef3a
Mikael Bjerga <mikael.bjerga@nav.no>
2023-10-02 14:46:36 +0200

    Innsending: Håndter ulike format på begrunnelse

--- api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/innsending/InnsendingRoute.kt
+++ api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/innsending/InnsendingRoute.kt
@@ -8,6 +8,10 @@ import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.BegrunnelseIngenEllerRedusertUtbetalingKode
 import no.nav.helsearbeidsgiver.domene.inntektsmelding.FullLoennIArbeidsgiverPerioden
 import no.nav.helsearbeidsgiver.domene.inntektsmelding.Innsending
 import no.nav.helsearbeidsgiver.inntektsmelding.api.RedisPollerTimeoutException
@@ -26,6 +30,8 @@ import no.nav.helsearbeidsgiver.inntektsmelding.api.utils.respondInternalServerE
 import no.nav.helsearbeidsgiver.inntektsmelding.api.validation.ValidationResponse
 import no.nav.helsearbeidsgiver.inntektsmelding.api.validation.validationResponseMapper
 import no.nav.helsearbeidsgiver.utils.json.fromJson
+import no.nav.helsearbeidsgiver.utils.json.parseJson
+import no.nav.helsearbeidsgiver.utils.json.toJson
 import no.nav.helsearbeidsgiver.utils.json.toPretty
 import org.valiktor.ConstraintViolationException
 import java.util.UUID
@@ -42,20 +48,23 @@ fun RouteExtra.innsendingRoute() {
 
             if (forespoerselId != null) {
                 try {
-                    val request = call.receiveText().fromJson(Innsending.serializer()).let {
-                        // TODO gjør denne sjekken ved opprettelse
-                        if (it.fullLønnIArbeidsgiverPerioden?.utbetalerFullLønn == true) {
-                            it.copy(
-                                fullLønnIArbeidsgiverPerioden = FullLoennIArbeidsgiverPerioden(
-                                    utbetalerFullLønn = true,
-                                    begrunnelse = null,
-                                    utbetalt = null
+                    val request = call.receiveText()
+                        .parseJson()
+                        .erstattBegrunnelseForIkkeFullLoennIAgp()
+                        .fromJson(Innsending.serializer()).let {
+                            // TODO gjør denne sjekken ved opprettelse
+                            if (it.fullLønnIArbeidsgiverPerioden?.utbetalerFullLønn == true) {
+                                it.copy(
+                                    fullLønnIArbeidsgiverPerioden = FullLoennIArbeidsgiverPerioden(
+                                        utbetalerFullLønn = true,
+                                        begrunnelse = null,
+                                        utbetalt = null
+                                    )
                                 )
-                            )
-                        } else {
-                            it
+                            } else {
+                                it
+                            }
                         }
-                    }
 
                     "Mottok innsending med forespørselId: $forespoerselId".let {
                         logger.info(it)
@@ -101,4 +110,34 @@ fun RouteExtra.innsendingRoute() {
             }
         }
     }
+}
+
+// For FullLoennIArbeidsgiverPerioden.begrunnelse: Erstatter strengverdi med enum
+private fun JsonElement.erstattBegrunnelseForIkkeFullLoennIAgp(): JsonElement {
+    val innsendingMap = jsonObject.toMap()
+
+    val fullLoennIAgpMap = innsendingMap[Innsending::fullLønnIArbeidsgiverPerioden.name]
+        ?.jsonObject
+        ?.toMap()
+        ?: throw SerializationException("Noe gikk galt under erstatting av begrunnelse.")
+
+    val begrunnelse = fullLoennIAgpMap[FullLoennIArbeidsgiverPerioden::begrunnelse.name]
+        ?.jsonPrimitive
+        ?.content
+        ?.let { begrunnelseVerdi ->
+            BegrunnelseIngenEllerRedusertUtbetalingKode.entries.firstOrNull {
+                // Tåler wrappet verdi (dagens)
+                it.value == begrunnelseVerdi ||
+                    // ... og enum-navn (fremtidens?)
+                    it.name == begrunnelseVerdi
+            }
+        }
+        ?: throw SerializationException("Noe gikk galt under erstatting av begrunnelse.")
+
+    return innsendingMap.plus(
+        Innsending::fullLønnIArbeidsgiverPerioden.name to fullLoennIAgpMap.plus(
+            FullLoennIArbeidsgiverPerioden::begrunnelse.name to begrunnelse.toJson(BegrunnelseIngenEllerRedusertUtbetalingKode.serializer())
+        ).toJson()
+    )
+        .toJson()
 }

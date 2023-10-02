bdf7a4a2125652e52ced9ca54739498d730ae31c
Gustav Berggren <gustav.berggren@nav.no>
2023-10-02 16:01:17 +0200

    mapp med begrunnelse feltet til xml ved journalføring

--- joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/mappers/InntektDokumentTilSkjemainnholdMapper.kt
+++ joark/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/mappers/InntektDokumentTilSkjemainnholdMapper.kt
@@ -30,6 +30,7 @@ interface InntektDokumentTilSkjemainnholdMapper {
         Mapping(source = "inntekt.endringÅrsak", target = "arbeidsforhold.beregnetInntekt.aarsakVedEndring"),
         Mapping(source = "arbeidsgiverperioder", target = "sykepengerIArbeidsgiverperioden.arbeidsgiverperiodeListe"),
         Mapping(source = "fullLønnIArbeidsgiverPerioden.utbetalt", target = "sykepengerIArbeidsgiverperioden.bruttoUtbetalt"),
+        Mapping(source = "fullLønnIArbeidsgiverPerioden.begrunnelse", target = "sykepengerIArbeidsgiverperioden.begrunnelseForReduksjonEllerIkkeUtbetalt"),
         Mapping(source = "refusjon", target = "refusjon"),
         Mapping(source = "naturalytelser", target = "opphoerAvNaturalytelseListe"),
         Mapping(constant = "NAV_NO", target = "avsendersystem.systemnavn"),

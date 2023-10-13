2013a95780ec325a37a4f19d06a4798bb7ecab44
Gustav Berggren <gustav.berggren@nav.no>
2023-10-13 13:39:19 +0200

    ktlint format

--- bro-spinn/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/brospinn/HttpUtils.kt
+++ bro-spinn/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/brospinn/HttpUtils.kt
@@ -3,14 +3,14 @@ package no.nav.helsearbeidsgiver.inntektsmelding.brospinn
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.apache5.Apache5
+import io.ktor.client.network.sockets.ConnectTimeoutException
+import io.ktor.client.network.sockets.SocketTimeoutException
 import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpRequestTimeoutException
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import no.nav.helsearbeidsgiver.utils.json.jsonConfig
-import io.ktor.client.network.sockets.SocketTimeoutException
-import io.ktor.client.network.sockets.ConnectTimeoutException
-import io.ktor.client.plugins.HttpRequestTimeoutException
-import io.ktor.client.plugins.HttpTimeout
 
 internal fun createHttpClient(): HttpClient =
     HttpClient(Apache5) { configure() }
@@ -27,9 +27,9 @@ internal fun HttpClientConfig<*>.configure() {
         retryOnServerErrors(maxRetries)
         retryOnExceptionIf { _, cause ->
             cause is SocketTimeoutException ||
-            cause is ConnectTimeoutException ||
-            cause is HttpRequestTimeoutException ||
-            cause is java.net.SocketTimeoutException
+                cause is ConnectTimeoutException ||
+                cause is HttpRequestTimeoutException ||
+                cause is java.net.SocketTimeoutException
         }
         exponentialDelay()
     }

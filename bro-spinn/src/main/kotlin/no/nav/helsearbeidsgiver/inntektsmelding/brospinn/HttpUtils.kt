fceb4d304d26608ddaa636914a75d8b007ed67da
Gustav Berggren <gustav.berggren@nav.no>
2023-10-13 13:07:23 +0200

    retry på flere feil

--- bro-spinn/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/brospinn/HttpUtils.kt
+++ bro-spinn/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/brospinn/HttpUtils.kt
@@ -7,6 +7,9 @@ import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import no.nav.helsearbeidsgiver.utils.json.jsonConfig
+import io.ktor.client.network.sockets.SocketTimeoutException
+import io.ktor.client.network.sockets.ConnectTimeoutException
+import io.ktor.client.plugins.HttpRequestTimeoutException
 
 internal fun createHttpClient(): HttpClient =
     HttpClient(Apache5) { configure() }
@@ -22,6 +25,9 @@ internal fun HttpClientConfig<*>.configure() {
         maxRetries = 3
         retryOnServerErrors(maxRetries)
         retryOnExceptionIf { _, cause ->
+            cause is SocketTimeoutException ||
+            cause is ConnectTimeoutException ||
+            cause is HttpRequestTimeoutException ||
             cause is java.net.SocketTimeoutException
         }
         exponentialDelay()

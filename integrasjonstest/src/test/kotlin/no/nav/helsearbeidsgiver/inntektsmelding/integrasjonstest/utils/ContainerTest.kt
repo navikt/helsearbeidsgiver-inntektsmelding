a0db35afc3b31c498b3202630ee153a5eccaa3c9
Gustav Berggren <gustav.berggren@nav.no>
2023-10-23 14:53:02 +0200

    Bump confluent kafka docker image versjon

--- integrasjonstest/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/integrasjonstest/utils/ContainerTest.kt
+++ integrasjonstest/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/integrasjonstest/utils/ContainerTest.kt
@@ -14,7 +14,7 @@ import java.util.Properties
 abstract class ContainerTest {
     val topic = "helsearbeidsgiver.inntektsmelding"
 
-    val kafkaContainer = KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"))
+    val kafkaContainer = KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.1"))
     val redisContainer = RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag(RedisContainer.DEFAULT_TAG))
     val postgreSQLContainer = PostgreSQLContainer<Nothing>("postgres:14")
 

ea79d9270191f95489a4f16627c386b0a1083ecf
mortenbyhring <117746154+mortenbyhring@users.noreply.github.com>
2023-11-16 10:36:01 +0100

    catch opprettOppgave exception (#344)
    * catch exception i opprettOppgave

--- notifikasjon/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/OpprettOppgaveLoeser.kt
+++ notifikasjon/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/OpprettOppgaveLoeser.kt
@@ -4,11 +4,13 @@ import kotlinx.coroutines.runBlocking
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 import no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon.ArbeidsgiverNotifikasjonKlient
+import no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon.OpprettNyOppgaveException
 import no.nav.helsearbeidsgiver.felles.BehovType
 import no.nav.helsearbeidsgiver.felles.DataFelt
 import no.nav.helsearbeidsgiver.felles.Key
 import no.nav.helsearbeidsgiver.felles.rapidsrivers.Loeser
 import no.nav.helsearbeidsgiver.felles.rapidsrivers.model.Behov
+import no.nav.helsearbeidsgiver.utils.log.logger
 
 class OpprettOppgaveLoeser(
     rapidsConnection: RapidsConnection,
@@ -16,6 +18,7 @@ class OpprettOppgaveLoeser(
     private val linkUrl: String
 ) : Loeser(rapidsConnection) {
 
+    private val logger = logger()
     override fun accept(): River.PacketValidation =
         River.PacketValidation {
             it.demandValue(Key.BEHOV.str, BehovType.OPPRETT_OPPGAVE.name)
@@ -34,6 +37,10 @@ class OpprettOppgaveLoeser(
             behov[DataFelt.ORGNRUNDERENHET].asText(),
             behov[DataFelt.VIRKSOMHET].asText()
         )
+        if (oppgaveId.isNullOrBlank()) {
+            publishFail(behov.createFail("OpprettOppgave feilet"))
+            return
+        }
 
         behov.createBehov(
             BehovType.PERSISTER_OPPGAVE_ID,
@@ -50,26 +57,32 @@ class OpprettOppgaveLoeser(
         forespørselId: String,
         orgnr: String,
         virksomhetnavn: String
-    ): String {
+    ): String? {
         val requestTimer = Metrics.requestLatency.labels("opprettOppgave").startTimer()
-        return runBlocking {
-            arbeidsgiverNotifikasjonKlient.opprettNyOppgave(
-                eksternId = forespørselId,
-                lenke = "$linkUrl/im-dialog/$forespørselId",
-                tekst = "Send inn inntektsmelding",
-                virksomhetsnummer = orgnr,
-                merkelapp = "Inntektsmelding",
-                tidspunkt = null,
-                grupperingsid = forespørselId,
-                varslingTittel = "Nav trenger inntektsmelding",
-                varslingInnhold = """$virksomhetnavn - orgnr $orgnr: En av dine ansatte har søkt om sykepenger
+        return try {
+            runBlocking {
+                arbeidsgiverNotifikasjonKlient.opprettNyOppgave(
+                    eksternId = forespørselId,
+                    lenke = "$linkUrl/im-dialog/$forespørselId",
+                    tekst = "Send inn inntektsmelding",
+                    virksomhetsnummer = orgnr,
+                    merkelapp = "Inntektsmelding",
+                    tidspunkt = null,
+                    grupperingsid = forespørselId,
+                    varslingTittel = "Nav trenger inntektsmelding",
+                    varslingInnhold = """$virksomhetnavn - orgnr $orgnr: En av dine ansatte har søkt om sykepenger
                     og vi trenger inntektsmelding for å behandle søknaden.
                     Logg inn på Min side – arbeidsgiver hos NAV.
                     Hvis dere sender inntektsmelding via lønnssystem kan dere fortsatt gjøre dette,
                     og trenger ikke sende inn via Min side – arbeidsgiver."""
-            )
-        }.also {
-            requestTimer.observeDuration()
+                )
+            }.also {
+                requestTimer.observeDuration()
+            }
+        } catch (e: OpprettNyOppgaveException) {
+            sikkerLogger.error("Feil ved kall til opprett oppgave for $forespørselId!", e)
+            logger.error("Feil ved kall til opprett oppgave for $forespørselId!")
+            return null
         }
     }
 }

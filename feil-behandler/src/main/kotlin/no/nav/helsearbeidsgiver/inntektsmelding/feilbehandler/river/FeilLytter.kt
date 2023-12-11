51542a6e16de403148356bfcda417119c51292aa
Morten Byhring <morten.byhring@nav.no>
2023-12-11 15:43:15 +0100

    Legg til håndtering av bare bestemte typer feil, resten skal ignoreres

--- feil-behandler/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/feilbehandler/river/FeilLytter.kt
+++ feil-behandler/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/feilbehandler/river/FeilLytter.kt
@@ -1,16 +1,47 @@
 package no.nav.helsearbeidsgiver.inntektsmelding.feilbehandler.river
 
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.serializer
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import no.nav.helsearbeidsgiver.felles.BehovType
 import no.nav.helsearbeidsgiver.felles.Key
+import no.nav.helsearbeidsgiver.felles.json.toMap
+import no.nav.helsearbeidsgiver.felles.rapidsrivers.model.Fail
 import no.nav.helsearbeidsgiver.felles.rapidsrivers.toPretty
+import no.nav.helsearbeidsgiver.utils.json.fromJson
+import no.nav.helsearbeidsgiver.utils.json.parseJson
 import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 
 class FeilLytter(rapidsConnection: RapidsConnection) : River.PacketListener {
 
     private val sikkerLogger = sikkerLogger()
+
+    companion object FeilHaandterer {
+        val behovSomHaandteres = listOf(
+            BehovType.LAGRE_FORESPOERSEL,
+            BehovType.OPPRETT_OPPGAVE,
+            BehovType.OPPRETT_SAK,
+            BehovType.PERSISTER_OPPGAVE_ID,
+            BehovType.PERSISTER_SAK_ID,
+            BehovType.JOURNALFOER,
+            BehovType.LAGRE_JOURNALPOST_ID,
+            BehovType.NOTIFIKASJON_HENT_ID
+        )
+
+        fun skalHaandteres(fail: Fail): Boolean {
+            if (fail.forespoerselId == null) {
+                return false
+            }
+            val behovFraMelding = fail.utloesendeMelding.toMap()[Key.BEHOV]?.fromJson(BehovType.serializer())
+            if (behovSomHaandteres.contains(behovFraMelding)) {
+                return true
+            }
+            return false
+        }
+    }
 
     init {
         sikkerLogger.info("Starter applikasjon - lytter på innkommende feil!")
@@ -24,5 +55,22 @@ class FeilLytter(rapidsConnection: RapidsConnection) : River.PacketListener {
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         sikkerLogger.info("Mottok feil: ${packet.toPretty()}")
+        val fail = toFailOrNull(packet.toJson().parseJson())
+        if (fail == null) {
+            sikkerLogger.warn("Kunne ikke parse feil-objekt, ignorerer...")
+            return
+        }
+        if (skalHaandteres(fail)) {
+            sikkerLogger.info("Behandler feil")
+        } else {
+            sikkerLogger.info("Ignorerer feil")
+        }
     }
+
+    fun toFailOrNull(json: JsonElement): Fail? = // TODO: duplisert, lage felles metode?
+        json.toMap()[Key.FAIL]
+            ?.runCatching {
+                fromJson(Fail.serializer())
+            }
+            ?.getOrNull()
 }

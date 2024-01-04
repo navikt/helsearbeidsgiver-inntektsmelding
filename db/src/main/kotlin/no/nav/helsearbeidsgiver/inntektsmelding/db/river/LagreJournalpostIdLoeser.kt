6e27d89c45fbea73b16211a01f21b72a7fad6f8b
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2024-01-04 15:32:36 +0100

    Ikke oppdater ekstern IM med journalpost-ID (#391)
    * Ikke oppdater ekstern IM med journalpost-ID
    
    * Gjør test mer reell
    
    * Glemte den viktigste delen av testen -.-

--- db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/river/LagreJournalpostIdLoeser.kt
+++ db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/river/LagreJournalpostIdLoeser.kt
@@ -76,7 +76,7 @@ class LagreJournalpostIdLoeser(
                 .also { publishFail(it) }
         } else {
             try {
-                repository.oppdaterJournalpostId(journalpostId, forespoerselId)
+                repository.oppdaterJournalpostId(forespoerselId, journalpostId)
 
                 logger.info("Lagret journalpost-ID $journalpostId i database.")
                 sikkerLogger.info("Lagret journalpost-ID $journalpostId i database.")

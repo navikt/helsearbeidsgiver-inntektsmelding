e5b30a99f5ff5f7ee202b5daaaaa5586039e0ec9
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2024-02-16 09:56:46 +0100

    Ta i bruk fellesmodul for database (#433)

--- db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/config/DbUtils.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.helsearbeidsgiver.inntektsmelding.db.config
-
-import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.Query
-
-fun <T> Query.firstOrNull(col: Column<T>): T? =
-    firstOrNull()?.getOrNull(col)

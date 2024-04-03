4da9c8909150907aa06c514bf39ade40a3147f0d
Gustav Berggren <gustav.berggren@nav.no>
2024-04-03 13:37:09 +0200

    Gjør indentering mer konsekvent

--- db/src/main/resources/db/migration/V14__datastream_publication.sql
+++ db/src/main/resources/db/migration/V14__datastream_publication.sql
@@ -1,10 +1,10 @@
 DO
 $$
 BEGIN
-        IF NOT EXISTS
-            (SELECT 1 FROM pg_publication WHERE pubname = 'simba_publication')
-        THEN
-            CREATE PUBLICATION simba_publication FOR ALL TABLES;
-END IF;
+    IF NOT EXISTS
+        (SELECT 1 FROM pg_publication WHERE pubname = 'simba_publication')
+    THEN
+        CREATE PUBLICATION simba_publication FOR ALL TABLES;
+    END IF;
 END;
 $$;

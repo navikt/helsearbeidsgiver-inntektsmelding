de1181d71acda8999e5c613b211a82dbb3c9f7ab
Mikael Bjerga <mikael.bjerga@nav.no>
2024-04-11 10:50:23 +0200

    Bruk funksjon fra domenepakke for å finne bestemmende fraværsdag

--- felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/TrengerInntekt.kt
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/TrengerInntekt.kt
@@ -6,9 +6,10 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
 import no.nav.helsearbeidsgiver.utils.json.serializer.LocalDateSerializer
 import no.nav.helsearbeidsgiver.utils.json.serializer.UuidSerializer
-import no.nav.helsearbeidsgiver.utils.pipe.orDefault
 import java.time.LocalDate
 import java.util.UUID
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Periode as PeriodeV1
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.bestemmendeFravaersdag as finnBestemmendeFravaersdag
 
 // TODO nytt navn
 @Serializable
@@ -17,6 +18,7 @@ data class TrengerInntekt(
     val orgnr: String,
     val fnr: String,
     val vedtaksperiodeId: UUID,
+    // TODO bytt til domene.v1.Periode
     val sykmeldingsperioder: List<Periode>,
     val egenmeldingsperioder: List<Periode>,
     val bestemmendeFravaersdager: Map<String, LocalDate>,
@@ -25,23 +27,17 @@ data class TrengerInntekt(
 ) {
     fun bestemmendeFravaersdag(): LocalDate =
         bestemmendeFravaersdager[orgnr]
-            ?: finnBestemmendeFravaersdag()
+            ?: finnBestemmendeFravaersdag(
+                arbeidsgiverperioder = emptyList(),
+                egenmeldingsperioder = egenmeldingsperioder.map { PeriodeV1(it.fom, it.tom) },
+                sykmeldingsperioder = sykmeldingsperioder.map { PeriodeV1(it.fom, it.tom) }
+            )
 
     fun inntektsdato(): LocalDate? =
         bestemmendeFravaersdager.minOfOrNull { it.value }
 
     fun eksternBestemmendeFravaersdag(): LocalDate? =
         bestemmendeFravaersdager.minus(orgnr).minOfOrNull { it.value }
-
-    /** Perioder som ikke har opphold over hverdager anses som sammenhengende. */
-    private fun finnBestemmendeFravaersdag(): LocalDate =
-        (egenmeldingsperioder + sykmeldingsperioder)
-            .sortedBy { it.fom }
-            .reduce { p1, p2 ->
-                p1.slåSammenIgnorerHelgOrNull(p2)
-                    .orDefault(p2)
-            }
-            .fom
 }
 
 enum class ForespoerselType {

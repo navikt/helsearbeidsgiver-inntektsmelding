3d568e72a6f02efe6a748fe5db77770fb76097fd
Mehdi Zare <mehdi.zare@nav.no>
2024-07-10 09:41:54 +0200

    oppgraderer Altinn client til å bruke maskinporten client

--- altinn/build.gradle.kts
+++ altinn/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     val altinnClientVersion: String by project
-
+    val maskinportenClientVersion: String by project
     implementation("no.nav.helsearbeidsgiver:altinn-client:$altinnClientVersion")
+    implementation("no.nav.helsearbeidsgiver:maskinporten-client:$maskinportenClientVersion")
 }
--- altinn/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/altinn/App.kt
+++ altinn/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/altinn/App.kt
@@ -1,18 +1,23 @@
 package no.nav.helsearbeidsgiver.inntektsmelding.altinn
 
+import kotlinx.coroutines.runBlocking
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helsearbeidsgiver.altinn.AltinnClient
 import no.nav.helsearbeidsgiver.altinn.CacheConfig
+import no.nav.helsearbeidsgiver.maskinporten.MaskinportenClient
+import no.nav.helsearbeidsgiver.maskinporten.MaskinportenClientConfig
 import no.nav.helsearbeidsgiver.utils.log.logger
 import kotlin.time.Duration.Companion.minutes
 
 private val logger = "helsearbeidsgiver-im-altinn".logger()
 
 fun main() {
+    val maskinportenClient = createMaskinportenClient()
+    val altinnClient = createAltinnClient(maskinportenClient)
     RapidApplication
         .create(System.getenv())
-        .createAltinn(createAltinnClient())
+        .createAltinn(altinnClient)
         .start()
 }
 
@@ -25,11 +30,27 @@ fun RapidsConnection.createAltinn(altinnClient: AltinnClient): RapidsConnection 
         AltinnRiver(altinnClient).connect(this)
     }
 
-private fun createAltinnClient(): AltinnClient =
+private fun createAltinnClient(maskinportenClient: MaskinportenClient): AltinnClient =
     AltinnClient(
         url = Env.url,
         serviceCode = Env.serviceCode,
-        apiGwApiKey = Env.apiGwApiKey,
+        getToken = maskinportenClient::getToken,
         altinnApiKey = Env.altinnApiKey,
         cacheConfig = CacheConfig(60.minutes, 100),
     )
+
+private fun createMaskinportenClient(): MaskinportenClient =
+    MaskinportenClient(
+        MaskinportenClientConfig(
+            Env.Maskinporten.altinnScope,
+            Env.Maskinporten.endpoint,
+            Env.Maskinporten.clientJwk,
+            Env.Maskinporten.issuer,
+            Env.Maskinporten.clientId,
+        ),
+    )
+
+fun MaskinportenClient.getToken() =
+    runBlocking {
+        fetchNewAccessToken().tokenResponse.accessToken
+    }

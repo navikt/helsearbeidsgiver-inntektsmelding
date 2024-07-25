1d2851f4099f010151223c6177fcad54051a35b4
Gustav Berggren <gustav.berggren@nav.no>
2024-07-25 10:43:36 +0200

    klint fiks

--- api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/Env.kt
+++ api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/Env.kt
@@ -6,8 +6,9 @@ object Env {
     object Auth {
         val discoveryUrl: String = "IDPORTEN_WELL_KNOWN_URL".fromEnv()
         val acceptedAudience: List<String> = "IDPORTEN_AUDIENCE".fromEnv().let(::listOf)
+
         object TokenX {
-            val discoveryUrl : String = "TOKEN_X_WELL_KNOWN_URL".fromEnv()
+            val discoveryUrl: String = "TOKEN_X_WELL_KNOWN_URL".fromEnv()
             val acceptedAudience: List<String> = "TOKEN_X_CLIENT_ID".fromEnv().let(::listOf)
         }
     }

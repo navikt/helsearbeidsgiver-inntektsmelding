ec2f2e66a149ca5af26bd082ee1ac620841baebb
Mikael Bjerga <mikael.bjerga@nav.no>
2024-08-06 11:25:32 +0200

    Erstatt river for å markere forespørsel som besvart

--- forespoersel-marker-besvart/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/forespoerselmarkerbesvart/MarkerForespoerselBesvartRiverTest.kt
+++ forespoersel-marker-besvart/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/forespoerselmarkerbesvart/MarkerForespoerselBesvartRiverTest.kt
@@ -1,6 +1,7 @@
 package no.nav.helsearbeidsgiver.inntektsmelding.forespoerselmarkerbesvart
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.ints.shouldBeExactly
 import io.mockk.clearAllMocks
 import io.mockk.every
@@ -14,20 +15,23 @@ import no.nav.helsearbeidsgiver.felles.BehovType
 import no.nav.helsearbeidsgiver.felles.EventName
 import no.nav.helsearbeidsgiver.felles.Key
 import no.nav.helsearbeidsgiver.felles.json.toJson
+import no.nav.helsearbeidsgiver.felles.rapidsrivers.model.Fail
 import no.nav.helsearbeidsgiver.felles.rapidsrivers.pritopic.Pri
 import no.nav.helsearbeidsgiver.felles.rapidsrivers.pritopic.PriProducer
 import no.nav.helsearbeidsgiver.felles.test.rapidsrivers.sendJson
-import no.nav.helsearbeidsgiver.felles.utils.randomUuid
 import no.nav.helsearbeidsgiver.utils.json.toJson
+import no.nav.helsearbeidsgiver.utils.test.wrapper.genererGyldig
+import no.nav.helsearbeidsgiver.utils.wrapper.Fnr
+import java.util.UUID
 
 class MarkerForespoerselBesvartRiverTest :
     FunSpec({
         val testRapid = TestRapid()
         val mockPriProducer = mockk<PriProducer>()
 
-        MarkerForespoerselBesvartRiver(testRapid, mockPriProducer)
+        MarkerForespoerselBesvartRiver(mockPriProducer).connect(testRapid)
 
-        beforeEach {
+        beforeTest {
             testRapid.reset()
             clearAllMocks()
         }
@@ -36,11 +40,11 @@ class MarkerForespoerselBesvartRiverTest :
             // Må bare returnere en Result med gyldig JSON
             every { mockPriProducer.send(*anyVararg<Pair<Pri.Key, JsonElement>>()) } returns Result.success(JsonNull)
 
-            val expectedForespoerselId = randomUuid()
+            val expectedForespoerselId = UUID.randomUUID()
 
             testRapid.sendJson(
                 Key.EVENT_NAME to EventName.INNTEKTSMELDING_MOTTATT.toJson(),
-                Key.UUID to randomUuid().toJson(),
+                Key.UUID to UUID.randomUUID().toJson(),
                 Key.FORESPOERSEL_ID to expectedForespoerselId.toJson(),
             )
 
@@ -54,33 +58,35 @@ class MarkerForespoerselBesvartRiverTest :
             }
         }
 
-        test("Ignorerer meldinger med behov") {
-            testRapid.sendJson(
-                Key.EVENT_NAME to EventName.INNTEKTSMELDING_MOTTATT.toJson(),
-                Key.UUID to randomUuid().toJson(),
-                Key.FORESPOERSEL_ID to randomUuid().toJson(),
-                Key.BEHOV to BehovType.HENT_PERSONER.toJson(),
-            )
+        context("ignorerer melding") {
+            withData(
+                mapOf(
+                    "melding med behov" to Pair(Key.BEHOV, BehovType.HENT_PERSONER.toJson()),
+                    "melding med data" to Pair(Key.DATA, mapOf(Key.FNR to Fnr.genererGyldig().toJson()).toJson()),
+                    "melding med fail" to Pair(Key.FAIL, mockFail.toJson(Fail.serializer())),
+                ),
+            ) { uoensketKeyMedVerdi ->
+                testRapid.sendJson(
+                    Key.EVENT_NAME to EventName.INNTEKTSMELDING_MOTTATT.toJson(),
+                    Key.UUID to UUID.randomUUID().toJson(),
+                    Key.FORESPOERSEL_ID to UUID.randomUUID().toJson(),
+                    uoensketKeyMedVerdi,
+                )
 
-            testRapid.inspektør.size shouldBeExactly 0
+                testRapid.inspektør.size shouldBeExactly 0
 
-            verify(exactly = 0) {
-                mockPriProducer.send(*anyVararg<Pair<Pri.Key, JsonElement>>())
-            }
-        }
-
-        test("Ignorerer meldinger med data") {
-            testRapid.sendJson(
-                Key.EVENT_NAME to EventName.INNTEKTSMELDING_MOTTATT.toJson(),
-                Key.UUID to randomUuid().toJson(),
-                Key.FORESPOERSEL_ID to randomUuid().toJson(),
-                Key.DATA to "".toJson(),
-            )
-
-            testRapid.inspektør.size shouldBeExactly 0
-
-            verify(exactly = 0) {
-                mockPriProducer.send(*anyVararg<Pair<Pri.Key, JsonElement>>())
+                verify(exactly = 0) {
+                    mockPriProducer.send(*anyVararg<Pair<Pri.Key, JsonElement>>())
+                }
             }
         }
     })
+
+private val mockFail =
+    Fail(
+        feilmelding = "Life, eh, finds a way.",
+        event = EventName.INNTEKTSMELDING_MOTTATT,
+        transaksjonId = UUID.randomUUID(),
+        forespoerselId = UUID.randomUUID(),
+        utloesendeMelding = JsonNull,
+    )

72ec5f8569a60ab0c17cf7ea06c80e64d84c0b58
Mikael Bjerga <mikael.bjerga@nav.no>
2024-08-07 16:24:16 +0200

    Erstatt river for å håndtere at forespørsel har blitt mottatt

--- felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/rapidsrivers/RiverUtils.kt
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/rapidsrivers/RiverUtils.kt
@@ -1,6 +1,5 @@
 package no.nav.helsearbeidsgiver.felles.rapidsrivers
 
-import com.fasterxml.jackson.databind.JsonNode
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonNull
 import no.nav.helse.rapids_rivers.JsonMessage
@@ -30,6 +29,7 @@ fun JsonMessage.requireKeys(vararg keys: IKey) {
     requireKey(*keysAsStr)
 }
 
+// TODO slett
 fun JsonMessage.interestedIn(vararg keys: IKey) {
     val keysAsStr = keys.map(IKey::str).toTypedArray()
     interestedIn(*keysAsStr)
@@ -48,14 +48,3 @@ fun MessageContext.publish(messageFields: Map<Key, JsonElement>): JsonElement =
         }.toJson()
         .also(::publish)
         .parseJson()
-
-private fun JsonMessage.validate(
-    validateFn: (JsonMessage, String, (JsonNode) -> Any) -> Unit,
-    keyAndParserPairs: List<Pair<String, (JsonElement) -> Any>>,
-) {
-    keyAndParserPairs.forEach { (key, block) ->
-        validateFn(this, key) {
-            it.toString().parseJson().let(block)
-        }
-    }
-}

d29817de426cc6af8d470635a129f1a1b9e47a77
Mikael Bjerga <mikael.bjerga@nav.no>
2024-08-14 13:58:26 +0200

    Legg til kommentar for å forklare utleding av bestemmende fraværsdag

--- innsending/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/innsending/UtledBestemmendeFravaersdag.kt
+++ innsending/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/innsending/UtledBestemmendeFravaersdag.kt
@@ -5,6 +5,14 @@ import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.bestemmendeFravaersdag
 import no.nav.helsearbeidsgiver.felles.domene.Forespoersel
 import java.time.LocalDate
 
+/**
+ * Vi bruker Spleis sitt forslag til bestemmende fraværsdag (BF), med to unntak der vi må beregne BF selv:
+ * - Vi ber om AGP. Da kan AGP inneholde info som Spleis ikke vet om, f. eks. egenmeldinger.
+ * - Vi ber kun om refusjon. Da kan Spleis sitt forslag inneholde feil`*` dersom sykmeldt har mer enn én arbeidsgiver.
+ *
+ * `*` Det er ikke feil, men forslagene fra Spleis er egentlig inntektsdatoer, ikke BF-er.
+ * For én arbeidsgiver så er disse datoene like, men det er de nødvendigvis ikke ved mer enn én arbeidsgiver.
+*/
 fun utledBestemmendeFravaersdag(
     forespoersel: Forespoersel,
     inntektsmelding: Inntektsmelding,

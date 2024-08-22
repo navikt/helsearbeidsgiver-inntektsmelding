d7d269e1883e9b4dfc5d69f927f3f1031f622586
Magnus Sælensminde <magnus.salensminde@bekk.no>
2024-08-22 17:29:21 +0200

    Lag en berikinntektsmelding enhetstest

---
+++ berik-inntektsmelding-service/src/test/kotlin/no.nav.helsearbeidsgiver.inntektsmelding.berikinntektsmeldingservice/BerikInntektsmeldingServiceTest.kt
@@ -0,0 +1,297 @@
+package no.nav.helsearbeidsgiver.inntektsmelding.berikinntektsmeldingservice
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.mockk.clearAllMocks
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.deprecated.Inntektsmelding
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.skjema.SkjemaInntektsmelding
+import no.nav.helsearbeidsgiver.felles.BehovType
+import no.nav.helsearbeidsgiver.felles.EventName
+import no.nav.helsearbeidsgiver.felles.Key
+import no.nav.helsearbeidsgiver.felles.domene.EksternInntektsmelding
+import no.nav.helsearbeidsgiver.felles.domene.Forespoersel
+import no.nav.helsearbeidsgiver.felles.domene.Person
+import no.nav.helsearbeidsgiver.felles.json.lesOrNull
+import no.nav.helsearbeidsgiver.felles.json.orgMapSerializer
+import no.nav.helsearbeidsgiver.felles.json.personMapSerializer
+import no.nav.helsearbeidsgiver.felles.json.toJson
+import no.nav.helsearbeidsgiver.felles.json.toMap
+import no.nav.helsearbeidsgiver.felles.rapidsrivers.model.Fail
+import no.nav.helsearbeidsgiver.felles.rapidsrivers.service.ServiceRiverStateless
+import no.nav.helsearbeidsgiver.felles.test.json.lesBehov
+import no.nav.helsearbeidsgiver.felles.test.json.lesData
+import no.nav.helsearbeidsgiver.felles.test.json.lesEventName
+import no.nav.helsearbeidsgiver.felles.test.json.plusData
+import no.nav.helsearbeidsgiver.felles.test.mock.mockEksternInntektsmelding
+import no.nav.helsearbeidsgiver.felles.test.mock.mockForespoersel
+import no.nav.helsearbeidsgiver.felles.test.mock.mockInntektsmeldingGammeltFormat
+import no.nav.helsearbeidsgiver.felles.test.mock.mockSkjemaInntektsmelding
+import no.nav.helsearbeidsgiver.felles.test.rapidsrivers.message
+import no.nav.helsearbeidsgiver.felles.test.rapidsrivers.sendJson
+import no.nav.helsearbeidsgiver.utils.json.serializer.UuidSerializer
+import no.nav.helsearbeidsgiver.utils.json.serializer.set
+import no.nav.helsearbeidsgiver.utils.json.toJson
+import no.nav.helsearbeidsgiver.utils.test.wrapper.genererGyldig
+import no.nav.helsearbeidsgiver.utils.wrapper.Fnr
+import no.nav.helsearbeidsgiver.utils.wrapper.Orgnr
+import java.util.UUID
+
+class BerikInntektsmeldingServiceTest :
+    FunSpec({
+
+        val testRapid = TestRapid()
+
+        ServiceRiverStateless(
+            BerikInntektsmeldingService(testRapid),
+        ).connect(testRapid)
+
+        beforeEach {
+            testRapid.reset()
+            clearAllMocks()
+        }
+
+        test("nytt inntektsmeldingskjema berikes, lagres og sendes videre til journalføring") {
+            testRapid.sendJson(Mock.steg0(Mock.transaksjonId))
+
+            // Melding med forventet behov og data sendt for å hente forespørsel
+            testRapid.inspektør.size shouldBeExactly 1
+            testRapid.message(0).also {
+                it.lesBehov() shouldBe BehovType.HENT_TRENGER_IM
+
+                Key.FORESPOERSEL_ID.lesOrNull(
+                    serializer = UuidSerializer,
+                    melding = it.lesData(),
+                ) shouldBe Mock.skjema.forespoerselId
+            }
+
+            testRapid.sendJson(Mock.steg1(Mock.transaksjonId))
+
+            // Melding med forventet behov og data sendt for å hente lagrede inntektsmeldinger
+            testRapid.inspektør.size shouldBeExactly 2
+            testRapid.message(1).also {
+                it.lesBehov() shouldBe BehovType.HENT_LAGRET_IM
+
+                Key.FORESPOERSEL_ID.lesOrNull(
+                    serializer = UuidSerializer,
+                    melding = it.lesData(),
+                ) shouldBe Mock.skjema.forespoerselId
+            }
+
+            testRapid.sendJson(Mock.steg2(Mock.transaksjonId))
+
+            // Melding med forventet behov og data sendt for å hente virksomhetsnavn
+            testRapid.inspektør.size shouldBeExactly 3
+            testRapid.message(2).also {
+                it.lesBehov() shouldBe BehovType.HENT_VIRKSOMHET_NAVN
+
+                Key.ORGNR_UNDERENHETER.lesOrNull(
+                    serializer = Orgnr.serializer().set(),
+                    melding = it.lesData(),
+                ) shouldNotBe null
+            }
+
+            testRapid.sendJson(Mock.steg3(Mock.transaksjonId))
+
+            // Melding med forventet behov og data sendt for å hente personnavn
+            testRapid.inspektør.size shouldBeExactly 4
+            testRapid.message(3).also {
+                it.lesBehov() shouldBe BehovType.HENT_PERSONER
+
+                Key.FNR_LISTE.lesOrNull(
+                    serializer = Fnr.serializer().set(),
+                    melding = it.lesData(),
+                ) shouldNotBe null
+            }
+
+            testRapid.sendJson(Mock.steg4(Mock.transaksjonId))
+
+            // Melding med forventet behov og data sendt for å lagre inntektsmelding
+            testRapid.inspektør.size shouldBeExactly 5
+            testRapid.message(4).also {
+                it.lesBehov() shouldBe BehovType.LAGRE_IM
+
+                val data = it.lesData()
+                Key.INNTEKTSMELDING.lesOrNull(
+                    Inntektsmelding
+                        .serializer(),
+                    data,
+                ) shouldNotBe null
+                Key.FORESPOERSEL_ID.lesOrNull(UuidSerializer, data) shouldBe Mock.skjema.forespoerselId
+                Key.INNSENDING_ID.lesOrNull(Long.serializer(), data) shouldBe Mock.innsendingId
+            }
+
+            testRapid.sendJson(Mock.steg5(Mock.transaksjonId))
+
+            // Inntektsmelding sendt videre til journalføring med forventet data
+            testRapid.inspektør.size shouldBeExactly 6
+            testRapid.message(5).also {
+                it.lesEventName() shouldBe EventName.INNTEKTSMELDING_MOTTATT
+                Key.FORESPOERSEL_ID.lesOrNull(UuidSerializer, it.toMap()) shouldBe Mock.skjema.forespoerselId
+                Key.INNTEKTSMELDING_DOKUMENT.lesOrNull(Inntektsmelding.serializer(), it.toMap()) shouldNotBe null
+                Key.INNSENDING_ID.lesOrNull(Long.serializer(), it.toMap()) shouldBe Mock.innsendingId
+            }
+        }
+
+        test("duplikat IM sendes _ikke_ videre til journalføring") {
+            testRapid.sendJson(
+                Mock
+                    .steg5(Mock.transaksjonId)
+                    .plusData(Key.ER_DUPLIKAT_IM to true.toJson(Boolean.serializer())),
+            )
+
+            testRapid.inspektør.size shouldBeExactly 0
+        }
+
+        test("skal ved feil ikke foreta seg noe (FeilLytter skal plukke opp og rekjøre meldingen som utløste feilen)") {
+
+            val transaksjonId = UUID.randomUUID()
+            testRapid.sendJson(
+                Fail(
+                    feilmelding = "Detta gikk jo ikke så bra.",
+                    event = EventName.INNTEKTSMELDING_SKJEMA_LAGRET,
+                    transaksjonId = transaksjonId,
+                    forespoerselId = Mock.skjema.forespoerselId,
+                    utloesendeMelding =
+                        JsonObject(
+                            mapOf(
+                                Key.BEHOV.toString() to BehovType.HENT_TRENGER_IM.toJson(),
+                            ),
+                        ),
+                ).tilMelding(),
+            )
+
+            testRapid.inspektør.size shouldBeExactly 0
+        }
+    })
+
+private object Mock {
+    val transaksjonId = UUID.randomUUID()
+    val forespoersel = mockForespoersel()
+    val skjema = mockSkjemaInntektsmelding()
+    val inntektsmelding = mockInntektsmeldingGammeltFormat()
+    val eksternInntektsmelding = mockEksternInntektsmelding()
+
+    val avsender =
+        Fnr.genererGyldig().let {
+            Person(
+                fnr = it,
+                navn = "Skrue McDuck",
+                foedselsdato = Person.foedselsdato(it),
+            )
+        }
+
+    val sykmeldt =
+        Fnr(forespoersel.fnr).let {
+            Person(
+                fnr = it,
+                navn = "Dolly Duck",
+                foedselsdato = Person.foedselsdato(it),
+            )
+        }
+
+    val orgnrMedNavn = mapOf(Orgnr(forespoersel.orgnr) to "Lasses kasserollesjappe")
+
+    val personer =
+        mapOf(
+            avsender.fnr to avsender,
+            sykmeldt.fnr to sykmeldt,
+        )
+
+    val innsendingId = 1L
+
+    val steg0data =
+        mapOf(
+            Key.ARBEIDSGIVER_FNR to avsender.fnr.toJson(),
+            Key.SKJEMA_INNTEKTSMELDING to skjema.toJson(SkjemaInntektsmelding.serializer()),
+            Key.INNSENDING_ID to innsendingId.toJson(Long.serializer()),
+        )
+
+    val steg1data =
+        mapOf(
+            Key.FORESPOERSEL_SVAR to forespoersel.toJson(Forespoersel.serializer()),
+        )
+
+    val steg2data =
+        mapOf(
+            Key.LAGRET_INNTEKTSMELDING to
+                inntektsmelding.toJson(
+                    Inntektsmelding
+                        .serializer(),
+                ),
+            Key.EKSTERN_INNTEKTSMELDING to eksternInntektsmelding.toJson(EksternInntektsmelding.serializer()),
+        )
+
+    val steg3data =
+        mapOf(
+            Key.VIRKSOMHETER to orgnrMedNavn.toJson(orgMapSerializer),
+        )
+
+    val steg4data =
+        mapOf(
+            Key.PERSONER to personer.toJson(personMapSerializer),
+        )
+
+    val steg5data =
+        mapOf(
+            Key.ER_DUPLIKAT_IM to false.toJson(Boolean.serializer()),
+            Key.INNTEKTSMELDING to
+                inntektsmelding.toJson(
+                    Inntektsmelding
+                        .serializer(),
+                ),
+        )
+
+    fun steg0(transaksjonId: UUID): Map<Key, JsonElement> =
+        mapOf(
+            Key.EVENT_NAME to EventName.INNTEKTSMELDING_SKJEMA_LAGRET.toJson(),
+            Key.UUID to transaksjonId.toJson(),
+            Key.DATA to
+                steg0data.toJson(),
+        )
+
+    fun steg1(transaksjonId: UUID): Map<Key, JsonElement> =
+        mapOf(
+            Key.EVENT_NAME to EventName.INNTEKTSMELDING_SKJEMA_LAGRET.toJson(),
+            Key.UUID to transaksjonId.toJson(),
+            Key.DATA to
+                (steg0data + steg1data).toJson(),
+        )
+
+    fun steg2(transaksjonId: UUID): Map<Key, JsonElement> =
+        mapOf(
+            Key.EVENT_NAME to EventName.INNTEKTSMELDING_SKJEMA_LAGRET.toJson(),
+            Key.UUID to transaksjonId.toJson(),
+            Key.DATA to
+                (steg0data + steg1data + steg2data).toJson(),
+        )
+
+    fun steg3(transaksjonId: UUID): Map<Key, JsonElement> =
+        mapOf(
+            Key.EVENT_NAME to EventName.INNTEKTSMELDING_SKJEMA_LAGRET.toJson(),
+            Key.UUID to transaksjonId.toJson(),
+            Key.DATA to
+                (steg0data + steg1data + steg2data + steg3data).toJson(),
+        )
+
+    fun steg4(transaksjonId: UUID): Map<Key, JsonElement> =
+        mapOf(
+            Key.EVENT_NAME to EventName.INNTEKTSMELDING_SKJEMA_LAGRET.toJson(),
+            Key.UUID to transaksjonId.toJson(),
+            Key.DATA to
+                (steg0data + steg1data + steg2data + steg3data + steg4data).toJson(),
+        )
+
+    fun steg5(transaksjonId: UUID): Map<Key, JsonElement> =
+        mapOf(
+            Key.EVENT_NAME to EventName.INNTEKTSMELDING_SKJEMA_LAGRET.toJson(),
+            Key.UUID to transaksjonId.toJson(),
+            Key.DATA to
+                (steg0data + steg1data + steg2data + steg3data + steg4data + steg5data).toJson(),
+        )
+}
--- felles/src/testFixtures/kotlin/no/nav/helsearbeidsgiver/felles/test/json/KotlinxUtils.kt
+++ felles/src/testFixtures/kotlin/no/nav/helsearbeidsgiver/felles/test/json/KotlinxUtils.kt
@@ -2,12 +2,17 @@ package no.nav.helsearbeidsgiver.felles.test.json
 
 import kotlinx.serialization.json.JsonElement
 import no.nav.helsearbeidsgiver.felles.BehovType
+import no.nav.helsearbeidsgiver.felles.EventName
 import no.nav.helsearbeidsgiver.felles.Key
 import no.nav.helsearbeidsgiver.felles.json.lesOrNull
 import no.nav.helsearbeidsgiver.felles.json.toJson
 import no.nav.helsearbeidsgiver.felles.json.toMap
 
 fun JsonElement.lesBehov(): BehovType? = Key.BEHOV.lesOrNull(BehovType.serializer(), this.toMap())
+
+fun JsonElement.lesEventName(): EventName? = Key.EVENT_NAME.lesOrNull(EventName.serializer(), this.toMap())
+
+fun JsonElement.lesData(): Map<Key, JsonElement> = this.toMap()[Key.DATA]?.toMap().orEmpty()
 
 fun Map<Key, JsonElement>.plusData(dataField: Pair<Key, JsonElement>): Map<Key, JsonElement> {
     val data = this[Key.DATA]?.toMap().orEmpty()

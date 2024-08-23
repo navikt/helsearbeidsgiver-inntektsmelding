cb84d8faa54406c614b2248ab8d852b16515de4c
Magnus Sælensminde <41949947+magnusae@users.noreply.github.com>
2024-08-23 11:03:34 +0200

    Legg til endepunkt for å sjekke tilgang til orgnummer (#666)
    * nytt endepunkt for å sjekke tilgang til orgnr for arbeidsgiverinitiert IM-flyt
    ---------
    
    Co-authored-by: Morten Byhring <morten.byhring@nav.no>

---
+++ api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/tilgangorgnr/TilgangOrgnrRoute.kt
@@ -0,0 +1,42 @@
+package no.nav.helsearbeidsgiver.inntektsmelding.api.tilgangorgnr
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.call
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import no.nav.helsearbeidsgiver.inntektsmelding.api.RedisPollerTimeoutException
+import no.nav.helsearbeidsgiver.inntektsmelding.api.Routes
+import no.nav.helsearbeidsgiver.inntektsmelding.api.auth.ManglerAltinnRettigheterException
+import no.nav.helsearbeidsgiver.inntektsmelding.api.auth.Tilgangskontroll
+import no.nav.helsearbeidsgiver.inntektsmelding.api.logger
+import no.nav.helsearbeidsgiver.inntektsmelding.api.sikkerLogger
+import no.nav.helsearbeidsgiver.utils.wrapper.Orgnr
+
+fun Route.tilgangOrgnrRoute(tilgangskontroll: Tilgangskontroll) {
+    get(Routes.TILGANG_ORGNR) {
+        val orgnr = call.parameters["orgnr"]
+        try {
+            if (orgnr.isNullOrEmpty() || !Orgnr.erGyldig(orgnr)) {
+                call.respond(HttpStatusCode.BadRequest, "Ugyldig orgnr")
+            } else {
+                tilgangskontroll.validerTilgangTilOrg(call.request, orgnr)
+                call.respond(HttpStatusCode.OK)
+            }
+        } catch (_: ManglerAltinnRettigheterException) {
+            call.respond(HttpStatusCode.Forbidden, "Du har ikke rettigheter for organisasjon.")
+        } catch (_: RedisPollerTimeoutException) {
+            "Fikk timeout for tilgangsjekk orgnr.".also {
+                logger.error(it)
+                sikkerLogger.error(it)
+            }
+            call.respond(HttpStatusCode.InternalServerError, "Teknisk feil")
+        } catch (e: Exception) {
+            "Ukjent feil".also {
+                logger.error(it)
+                sikkerLogger.error(it, e)
+            }
+            call.respond(HttpStatusCode.InternalServerError, "Teknisk feil")
+        }
+    }
+}

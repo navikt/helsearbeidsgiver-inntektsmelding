3964600fb11068dd6eab7a8d659fed824a7c3178
Magnus Sælensminde <magnus.salensminde@bekk.no>
2024-08-23 10:32:04 +0200

    Legg til kolonne for å lagre im skjema

--- db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/tabell/InntektsmeldingEntitet.kt
+++ db/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/db/tabell/InntektsmeldingEntitet.kt
@@ -1,6 +1,7 @@
 package no.nav.helsearbeidsgiver.inntektsmelding.db.tabell
 
 import no.nav.helsearbeidsgiver.domene.inntektsmelding.deprecated.Inntektsmelding
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.skjema.SkjemaInntektsmelding
 import no.nav.helsearbeidsgiver.felles.domene.EksternInntektsmelding
 import no.nav.helsearbeidsgiver.utils.json.jsonConfig
 import org.jetbrains.exposed.sql.Table
@@ -24,6 +25,12 @@ object InntektsmeldingEntitet : Table("inntektsmelding") {
             name = "ekstern_inntektsmelding",
             jsonConfig = jsonConfig,
             kSerializer = EksternInntektsmelding.serializer(),
+        ).nullable()
+    val skjema =
+        jsonb<SkjemaInntektsmelding>(
+            name = "skjema",
+            jsonConfig = jsonConfig,
+            kSerializer = SkjemaInntektsmelding.serializer(),
         ).nullable()
     val innsendt = datetime("innsendt")
     val journalpostId = varchar("journalpostid", 30).nullable()

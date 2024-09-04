49df9ab69fbc135b92733c3c3f07c8eb04b6df2f
Magnus Sælensminde <magnus.salensminde@bekk.no>
2024-09-04 14:55:40 +0200

    Lint me baby, one more time

--- api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/kvittering/KvitteringRoute.kt
+++ api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/kvittering/KvitteringRoute.kt
@@ -131,7 +131,9 @@ private fun Inntektsmelding.tilKvitteringSimba(): KvitteringSimba =
         behandlingsdager = behandlingsdager,
         egenmeldingsperioder = egenmeldingsperioder,
         arbeidsgiverperioder = arbeidsgiverperioder,
-        bestemmendeFraværsdag = inntektsdato ?: bestemmendeFraværsdag, // Frontend tolker feltet bestemmendeFraværsdag som om det var inntektsdato. Vil bli orden på dette ved overgang til v1.Inntektsmelding, som kun har inntektsdato (ikke bestemmende fraværsdag).
+        // Frontend tolker feltet bestemmendeFraværsdag som om det var inntektsdato.
+        // Vi vil slippe denne hacken ved overgang til v1.Inntektsmelding, som kun inneholder inntektsdato (ikke bestemmende fraværsdag).
+        bestemmendeFraværsdag = inntektsdato ?: bestemmendeFraværsdag,
         fraværsperioder = fraværsperioder,
         inntekt =
             Inntekt(

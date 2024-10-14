7ca09281e5195577ec97e76ae7dfdee9d7fd2718
Magnus Sælensminde <magnus.salensminde@bekk.no>
2024-10-14 14:30:26 +0200

    Sett sak til ferdig når vi setter oppgave til utgått

--- notifikasjon/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/river/OppgaveUtgaattLoeser.kt
+++ notifikasjon/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/river/OppgaveUtgaattLoeser.kt
@@ -6,6 +6,7 @@ import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import kotlinx.serialization.json.JsonElement
 import no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon.ArbeidsgiverNotifikasjonKlient
+import no.nav.helsearbeidsgiver.arbeidsgivernotifkasjon.graphql.generated.enums.SaksStatus
 import no.nav.helsearbeidsgiver.felles.EventName
 import no.nav.helsearbeidsgiver.felles.Key
 import no.nav.helsearbeidsgiver.felles.json.les
@@ -100,6 +101,15 @@ class OppgaveUtgaattLoeser(
                 merkelapp = "Inntektsmelding sykepenger",
                 eksternId = forespoerselId.toString(),
                 nyLenke = "https://arbeidsgiver.intern.dev.nav.no/im-dialog/utgatt",
+            )
+        }
+
+        Metrics.agNotifikasjonRequest.recordTime(agNotifikasjonKlient::nyStatusSakByGrupperingsid) {
+            agNotifikasjonKlient.nyStatusSakByGrupperingsid(
+                grupperingsid = forespoerselId.toString(),
+                merkelapp = "Inntektsmelding sykepenger",
+                nyStatus = SaksStatus.FERDIG,
+                tidspunkt = null,
             )
         }
 

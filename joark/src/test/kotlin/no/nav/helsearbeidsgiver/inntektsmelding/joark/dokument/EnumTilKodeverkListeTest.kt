98cf8dbb1343c674a92505c808d4a44817974a0b
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2024-10-21 10:51:05 +0200

    Map ny IM-klasse til XML (#714)
    * Map ny IM-klasse til XML
    
    * Oppdater test for enumkonvertering
    
    * Oppdater avhengigheter

--- joark/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/EnumTilKodeverkListeTest.kt
+++ joark/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/joark/dokument/EnumTilKodeverkListeTest.kt
@@ -3,21 +3,21 @@ package no.nav.helsearbeidsgiver.inntektsmelding.joark.dokument
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withData
 import io.kotest.matchers.collections.shouldBeIn
-import no.nav.helsearbeidsgiver.domene.inntektsmelding.deprecated.BegrunnelseIngenEllerRedusertUtbetalingKode
-import no.nav.helsearbeidsgiver.domene.inntektsmelding.deprecated.NaturalytelseKode
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Naturalytelse
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.RedusertLoennIAgp
 import no.nav.inntektsmelding.xml.kodeliste._20210216.BegrunnelseIngenEllerRedusertUtbetalingKodeliste
 import no.nav.inntektsmelding.xml.kodeliste._20210216.NaturalytelseKodeliste
 
 class EnumTilKodeverkListeTest :
     FunSpec({
-        context("Sjekk at NaturalytelseKodeliste i kodeverk tilsvarer NaturalYtelseKode i domenemodellen") {
-            withData(NaturalytelseKode.entries) { kode ->
+        context("Sjekk at NaturalytelseKodeliste i kodeverk tilsvarer 'Naturalytelse.Kode' i domenemodellen") {
+            withData(Naturalytelse.Kode.entries) { kode ->
                 kode.name shouldBeIn NaturalytelseKodeliste.entries.map { it.value().uppercase() }
             }
         }
 
-        context("Sjekk at BegrunnelseIngenEllerRedusertUtbetalingKodeliste i kodeverk tilsvarer BegrunnelseIngenEllerRedusertUtbetalingKode i domenemodellen") {
-            withData(BegrunnelseIngenEllerRedusertUtbetalingKode.entries) { kode ->
+        context("Sjekk at BegrunnelseIngenEllerRedusertUtbetalingKodeliste i kodeverk tilsvarer 'RedusertLoennIAgp.Begrunnelse' i domenemodellen") {
+            withData(RedusertLoennIAgp.Begrunnelse.entries) { kode ->
                 kode.name shouldBeIn BegrunnelseIngenEllerRedusertUtbetalingKodeliste.entries.map { it.value() }
             }
         }

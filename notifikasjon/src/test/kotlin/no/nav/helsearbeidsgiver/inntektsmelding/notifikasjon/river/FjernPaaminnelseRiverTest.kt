dd43d316f2e4959351aa5eae36181085c1e50a3d
Magnus Sælensminde <magnus.salensminde@bekk.no>
2024-11-04 13:22:00 +0100

    Fjern påminnelse på oppgaver som er sendt til Infotrygd

--- notifikasjon/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/river/FjernPaaminnelseRiverTest.kt
+++ notifikasjon/src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/river/FjernPaaminnelseRiverTest.kt
@@ -3,13 +3,24 @@ package no.nav.helsearbeidsgiver.inntektsmelding.notifikasjon.river
 import com.github.navikt.tbd_libs.rapids_and_rivers.test_support.TestRapid
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.maps.shouldContainExactly
+import io.mockk.Runs
 import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.coVerifySequence
+import io.mockk.just
 import io.mockk.mockk
 import no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon.ArbeidsgiverNotifikasjonKlient
+import no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon.OppgaveEndrePaaminnelseByEksternIdException
+import no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon.SakEllerOppgaveFinnesIkkeException
 import no.nav.helsearbeidsgiver.felles.EventName
 import no.nav.helsearbeidsgiver.felles.Key
 import no.nav.helsearbeidsgiver.felles.json.toJson
+import no.nav.helsearbeidsgiver.felles.json.toMap
+import no.nav.helsearbeidsgiver.felles.rapidsrivers.model.Fail
+import no.nav.helsearbeidsgiver.felles.test.rapidsrivers.firstMessage
 import no.nav.helsearbeidsgiver.felles.test.rapidsrivers.sendJson
+import no.nav.helsearbeidsgiver.inntektsmelding.notifikasjon.NotifikasjonTekst
 import no.nav.helsearbeidsgiver.utils.json.toJson
 import java.util.UUID
 
@@ -19,7 +30,7 @@ class FjernPaaminnelseRiverTest :
         val testRapid = TestRapid()
         val mockagNotifikasjonKlient = mockk<ArbeidsgiverNotifikasjonKlient>()
 
-        FjernPaaminnelseRiver(mockagNotifikasjonKlient)
+        FjernPaaminnelseRiver(mockagNotifikasjonKlient).connect(testRapid)
 
         beforeTest {
             testRapid.reset()
@@ -27,15 +38,104 @@ class FjernPaaminnelseRiverTest :
         }
 
         test("fjern påminnelse for forespørsel") {
-            val innkommendeMelding =
-                mapOf(
-                    Key.EVENT_NAME to EventName.FORESPOERSEL_KASTET_TIL_INFOTRYGD.toJson(),
-                    Key.UUID to UUID.randomUUID().toJson(),
-                    Key.FORESPOERSEL_ID to UUID.randomUUID().toJson(),
+            val innkommendeMelding = innkommendeFjernPaaminnelseMelding()
+
+            coEvery { mockagNotifikasjonKlient.slettOppgavePaaminnelserByEksternId(any(), any()) } just Runs
+
+            testRapid.sendJson(innkommendeMelding.toMap())
+
+            coVerifySequence {
+                mockagNotifikasjonKlient.slettOppgavePaaminnelserByEksternId(
+                    NotifikasjonTekst.MERKELAPP,
+                    innkommendeMelding.forespoerselId.toString(),
+                )
+            }
+
+            testRapid.inspektør.size shouldBeExactly 0
+        }
+
+        test("ukjent feil ved sletting av påminnelser håndteres") {
+            val innkommendeMelding = innkommendeFjernPaaminnelseMelding()
+
+            coEvery { mockagNotifikasjonKlient.slettOppgavePaaminnelserByEksternId(any(), any()) } throws
+                OppgaveEndrePaaminnelseByEksternIdException(
+                    innkommendeMelding.forespoerselId.toString(),
+                    "mock feil",
                 )
 
-            testRapid.sendJson(innkommendeMelding)
+            testRapid.sendJson(innkommendeMelding.toMap())
+
+            coVerifySequence {
+                mockagNotifikasjonKlient.slettOppgavePaaminnelserByEksternId(
+                    NotifikasjonTekst.MERKELAPP,
+                    innkommendeMelding.forespoerselId.toString(),
+                )
+            }
+
+            testRapid.inspektør.size shouldBeExactly 1
+
+            testRapid.firstMessage().toMap() shouldContainExactly forventetFail(innkommendeMelding).tilMelding()
+        }
+
+        test("kaster ingen exception dersom oppgaven ikke finnes") {
+            val innkommendeMelding = innkommendeFjernPaaminnelseMelding()
+
+            coEvery {
+                mockagNotifikasjonKlient.slettOppgavePaaminnelserByEksternId(
+                    any(),
+                    any(),
+                )
+            } throws SakEllerOppgaveFinnesIkkeException("Sak eller oppgave finnes ikke.")
+
+            testRapid.sendJson(innkommendeMelding.toMap())
+
+            coVerifySequence {
+                mockagNotifikasjonKlient.slettOppgavePaaminnelserByEksternId(
+                    NotifikasjonTekst.MERKELAPP,
+                    innkommendeMelding.forespoerselId.toString(),
+                )
+            }
+
+            testRapid.inspektør.size shouldBeExactly 0
+        }
+
+        test("kaster ikke exception dersom oppgaven allerede er utført") {
+            val innkommendeMelding = innkommendeFjernPaaminnelseMelding()
+
+            coEvery { mockagNotifikasjonKlient.slettOppgavePaaminnelserByEksternId(any(), any()) } just Runs
+
+            testRapid.sendJson(innkommendeMelding.toMap())
+
+            coVerifySequence {
+                mockagNotifikasjonKlient.slettOppgavePaaminnelserByEksternId(
+                    NotifikasjonTekst.MERKELAPP,
+                    innkommendeMelding.forespoerselId.toString(),
+                )
+            }
 
             testRapid.inspektør.size shouldBeExactly 0
         }
     })
+
+private fun forventetFail(innkommendeMelding: FjernPaaminnelseMelding): Fail =
+    Fail(
+        feilmelding = "Klarte ikke fjerne påminnelse fra oppgave.",
+        event = innkommendeMelding.eventName,
+        transaksjonId = innkommendeMelding.transaksjonId,
+        forespoerselId = innkommendeMelding.forespoerselId,
+        utloesendeMelding = innkommendeMelding.toMap().toJson(),
+    )
+
+private fun FjernPaaminnelseMelding.toMap() =
+    mapOf(
+        Key.EVENT_NAME to eventName.toJson(),
+        Key.UUID to transaksjonId.toJson(),
+        Key.FORESPOERSEL_ID to forespoerselId.toJson(),
+    )
+
+fun innkommendeFjernPaaminnelseMelding(): FjernPaaminnelseMelding =
+    FjernPaaminnelseMelding(
+        eventName = EventName.FORESPOERSEL_KASTET_TIL_INFOTRYGD,
+        transaksjonId = UUID.randomUUID(),
+        forespoerselId = UUID.randomUUID(),
+    )

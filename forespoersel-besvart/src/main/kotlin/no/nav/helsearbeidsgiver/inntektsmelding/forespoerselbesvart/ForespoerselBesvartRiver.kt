a4996fc235f313ebc77cb6cab63bf3bccef9b0ce
Jesper Hustad <Jesper.Forrest.Hustad@nav.no>
2024-11-26 15:03:29 +0100

    Slettet midlertidlig spinn im id key

--- forespoersel-besvart/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/forespoerselbesvart/ForespoerselBesvartRiver.kt
+++ forespoersel-besvart/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/forespoerselbesvart/ForespoerselBesvartRiver.kt
@@ -53,7 +53,6 @@ class ForespoerselBesvartRiver : PriObjectRiver<BesvartMelding>() {
                     mapOf(
                         Key.FORESPOERSEL_ID to forespoerselId.toJson(),
                         Key.SPINN_INNTEKTSMELDING_ID to spinnInntektsmeldingId?.toJson(),
-                        Key.SPINN_INNTEKTSMELDING_ID_V2 to spinnInntektsmeldingId?.toJson(),
                     ).mapValuesNotNull { it }
                         .toJson(),
             )

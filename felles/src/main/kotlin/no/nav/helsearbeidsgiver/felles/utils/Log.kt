d337fb1cc82ebdff5a71ba3598d0c6a4b73356a9
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2024-12-16 15:45:19 +0100

    Bruk prefiks for loggfelt (#806)
    * Bruk prefiks for loggfelt
    
    * Bytt loggfelt fra transaksjon-ID til kontekst-ID

--- felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/utils/Log.kt
+++ felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/utils/Log.kt
@@ -6,19 +6,19 @@ import no.nav.helsearbeidsgiver.felles.rapidsrivers.pritopic.Pri
 import java.util.UUID
 
 object Log {
-    fun <T : Any> klasse(value: T) = "class" to value::class.simpleName.orEmpty()
+    fun <T : Any> klasse(value: T) = "hag_class" to value::class.simpleName.orEmpty()
 
-    fun event(value: EventName) = "event" to value.name
+    fun event(value: EventName) = "hag_event_name" to value.name
 
-    fun priNotis(value: Pri.NotisType) = "pri_notis" to value.name
+    fun priNotis(value: Pri.NotisType) = "hag_pri_notis" to value.name
 
-    fun behov(value: BehovType) = "behov" to value.name
+    fun behov(value: BehovType) = "hag_behov" to value.name
 
-    fun transaksjonId(value: UUID) = "transaksjon_id" to value.toString()
+    fun transaksjonId(value: UUID) = "hag_kontekst_id" to value.toString()
 
-    fun forespoerselId(value: UUID) = "forespoersel_id" to value.toString()
+    fun forespoerselId(value: UUID) = "hag_forespoersel_id" to value.toString()
 
-    fun selvbestemtId(value: UUID) = "selvbestemt_id" to value.toString()
+    fun selvbestemtId(value: UUID) = "hag_selvbestemt_id" to value.toString()
 
-    fun apiRoute(value: String) = "api_route" to value
+    fun apiRoute(value: String) = "hag_api_route" to value
 }

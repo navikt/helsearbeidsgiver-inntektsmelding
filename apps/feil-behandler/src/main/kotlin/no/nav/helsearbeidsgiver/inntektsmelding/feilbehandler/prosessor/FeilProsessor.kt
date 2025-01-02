3aae2b0282c29356b36aa1f7e174964b128910f8
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2025-01-02 14:28:56 +0100

    Bruk ObjectRiver i FeilLytter (#816)
    * Bruk ObjectRiver i FeilLytter
    
    * Logg penere json

--- apps/feil-behandler/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/feilbehandler/prosessor/FeilProsessor.kt
+++ apps/feil-behandler/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/feilbehandler/prosessor/FeilProsessor.kt
@@ -3,11 +3,17 @@ package no.nav.helsearbeidsgiver.inntektsmelding.feilbehandler.prosessor
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import no.nav.hag.utils.bakgrunnsjobb.Bakgrunnsjobb
 import no.nav.hag.utils.bakgrunnsjobb.BakgrunnsjobbProsesserer
+import no.nav.helsearbeidsgiver.utils.json.parseJson
+import no.nav.helsearbeidsgiver.utils.json.toPretty
+import no.nav.helsearbeidsgiver.utils.log.logger
 import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 
 class FeilProsessor(
     private val rapid: RapidsConnection,
 ) : BakgrunnsjobbProsesserer {
+    private val logger = logger()
+    private val sikkerLogger = sikkerLogger()
+
     override val type: String
         get() = JOB_TYPE
 
@@ -15,11 +21,12 @@ class FeilProsessor(
         const val JOB_TYPE = "kafka-retry-message"
     }
 
-    private val sikkerLogger = sikkerLogger()
-
     override fun prosesser(jobb: Bakgrunnsjobb) {
-        sikkerLogger.info("Prosesserer jobb - rekjører melding med id ${jobb.uuid}")
-        sikkerLogger.debug("Sender melding: ${jobb.data}")
+        "Prosesserer jobb - rekjører melding med ID '${jobb.uuid}'.".also {
+            logger.info(it)
+            sikkerLogger.info(it)
+        }
+        sikkerLogger.debug("Sender melding.\n${jobb.data.parseJson().toPretty()}")
         rapid.publish(jobb.data)
     }
 }

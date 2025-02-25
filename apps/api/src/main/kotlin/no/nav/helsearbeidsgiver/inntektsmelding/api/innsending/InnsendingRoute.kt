2a42e426d84bae3fd2d42fe71db4a5694c442dc7
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-02-25 13:44:48 +0100

    Fjernet kommentert ut kode

--- apps/api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/innsending/InnsendingRoute.kt
+++ apps/api/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/api/innsending/InnsendingRoute.kt
@@ -95,19 +95,6 @@ fun Route.innsending(
     }
 }
 
-// // midlertidlig duplisering for å støtte flere endringsÅrsaker
-// fun SkjemaInntektsmelding.fyllUtMangledeEndringsAarsaker(): SkjemaInntektsmelding {
-//    val medEndringsAarsakerfyllt = this.copy(inntekt = this.inntekt?.copy(endringAarsaker = listOfNotNull(this.inntekt?.endringAarsak)))
-//    val verdiMangler = this.inntekt?.endringAarsaker.isNullOrEmpty()
-//    return if (verdiMangler) medEndringsAarsakerfyllt else this
-// }
-//
-// fun Inntektsmelding.fyllUtMangledeEndringsAarsaker(): Inntektsmelding {
-//    val medEndringsAarsakerfyllt = this.copy(inntekt = this.inntekt?.copy(endringAarsaker = listOfNotNull(this.inntekt?.endringAarsak)))
-//    val verdiMangler = this.inntekt?.endringAarsaker.isNullOrEmpty()
-//    return if (verdiMangler) medEndringsAarsakerfyllt else this
-// }
-
 private suspend fun PipelineContext<Unit, ApplicationCall>.lesRequestOrNull(): SkjemaInntektsmelding? =
     call
         .receiveText()

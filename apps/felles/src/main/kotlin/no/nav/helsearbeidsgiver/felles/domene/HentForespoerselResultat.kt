437c40942e20be79ba004440f941741d913fd2f6
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2025-05-21 10:18:37 +0200

    Åpne for nullable navn under henting av preutfyllingsinfo (#879)

--- apps/felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/domene/HentForespoerselResultat.kt
+++ apps/felles/src/main/kotlin/no/nav/helsearbeidsgiver/felles/domene/HentForespoerselResultat.kt
@@ -10,9 +10,9 @@ import no.nav.helsearbeidsgiver.utils.json.serializer.UuidSerializer
 
 @Serializable
 data class HentForespoerselResultat(
-    val sykmeldtNavn: String,
-    val avsenderNavn: String,
-    val orgNavn: String,
+    val sykmeldtNavn: String?,
+    val avsenderNavn: String?,
+    val orgNavn: String?,
     val inntekt: Inntekt?,
     val forespoersel: Forespoersel,
     val feil: Map<Key, String>,

7bf1704af4dc6bf0556af53116e2f811627d32ed
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-09-15 15:12:55 +0200

    Gjør valideringen mer presis

--- apps/innsending/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/innsending/ekstern/ValideringsUtils.kt
+++ apps/innsending/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/innsending/ekstern/ValideringsUtils.kt
@@ -8,14 +8,14 @@ import kotlin.math.abs
 const val FEILMARGIN_INNTEKT_A_ORDNING_KRONER: Double = 10.0
 
 /**
- * Validerer om inntekten i inntektsmeldingen avviker fra inntekt i A-ordningen uten at det er oppgitt endringsårsak
+ * Validerer om inntekten i inntektsmeldingen avviker fra inntekten i a-ordningen
  */
 fun Inntekt.validerInntektMotAordningen(aordningInntekt: Map<YearMonth, Double?>): Set<Feilkode> {
     val aordningSnittInntekt = aordningInntekt.gjennomsnitt()
 
-    val inntektErUtenforFeilmargin = abs(beloep - aordningSnittInntekt) >= FEILMARGIN_INNTEKT_A_ORDNING_KRONER
+    val inntektErUtenforFeilmargin = abs(beloep - aordningSnittInntekt) > FEILMARGIN_INNTEKT_A_ORDNING_KRONER
 
-    return if (inntektErUtenforFeilmargin || aordningInntekt.isEmpty()) {
+    return if (inntektErUtenforFeilmargin || aordningInntekt.all { it.value == null }) {
         setOf(Feilkode.INNTEKT_AVVIKER_FRA_A_ORDNINGEN)
     } else {
         emptySet()

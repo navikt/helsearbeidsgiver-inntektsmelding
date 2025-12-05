b7fae786678232e210828f5382f90ba5b17f4e0e
Gustav Berggren <gustav.berggren@nav.no>
2025-12-05 10:19:29 +0100

    Legger til flere fleter på avvist inntektsmelding klassen
    Co-authored-by: Mehdi Zare <mehdi.zare@nav.no>

--- apps/innsending/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/innsending/ekstern/AvvistInntektsmelding.kt
+++ apps/innsending/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/innsending/ekstern/AvvistInntektsmelding.kt
@@ -5,11 +5,15 @@ package no.nav.helsearbeidsgiver.inntektsmelding.innsending.ekstern
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
 import no.nav.helsearbeidsgiver.utils.json.serializer.UuidSerializer
+import no.nav.helsearbeidsgiver.utils.wrapper.Orgnr
 import java.util.UUID
 
 @Serializable
 data class AvvistInntektsmelding(
     val inntektsmeldingId: UUID,
+    val forespoerselId: UUID,
+    val vedtaksperiodeId: UUID,
+    val orgnr: Orgnr,
     val feilkode: Feilkode,
 )
 

b6fe8f66ea5a3fed03e6c68ef48f424d255266a5
Mikael Bjerga <mikael.bjerga@nav.no>
2026-01-05 13:30:03 +0100

    Fiks feil etter merge

--- utils/felles/src/testFixtures/kotlin/no/nav/hag/simba/utils/felles/test/mock/MockInntektsmelding.kt
+++ utils/felles/src/testFixtures/kotlin/no/nav/hag/simba/utils/felles/test/mock/MockInntektsmelding.kt
@@ -58,7 +58,6 @@ fun mockInnsending(): Innsending {
                         versjon = "3.0.0",
                     ),
             ),
-        kontaktinfo = "Arbeidsgiveren",
         innsendtTid = OffsetDateTime.now(),
         kontaktinfo = "Ella LPS",
         versjon = 1,

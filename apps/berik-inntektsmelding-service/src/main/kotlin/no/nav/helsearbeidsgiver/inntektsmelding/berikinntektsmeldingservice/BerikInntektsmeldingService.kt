0fd6eaeffaa6e5e2ca1ecc2dfea9685df1867945
Morten Byhring <morten.byhring@nav.no>
2026-01-05 11:38:52 +0100

    fix kommentar

--- apps/berik-inntektsmelding-service/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/berikinntektsmeldingservice/BerikInntektsmeldingService.kt
+++ apps/berik-inntektsmelding-service/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/berikinntektsmeldingservice/BerikInntektsmeldingService.kt
@@ -149,8 +149,8 @@ class BerikInntektsmeldingService(
     ) {
         val orgNavn = steg1.orgnrMedNavn[steg0.forespoersel.orgnr] ?: UKJENT_VIRKSOMHET
         val sykmeldtNavn = steg2.personer[steg0.forespoersel.fnr]?.navn ?: UKJENT_NAVN
-        val avsenderNavn = steg2.personer[steg0.avsenderFnr]?.navn ?: steg0.innsending?.kontaktinfo ?: UKJENT_NAVN // API sender ikke avsenderFnr,
-        // sender kontaktinformasjon direkte i Innsending
+        // LPS-API har ikke avsenderFnr, sender i stedet kontaktinfo direkte i Innsending.kontaktinfo
+        val avsenderNavn = steg2.personer[steg0.avsenderFnr]?.navn ?: steg0.innsending?.kontaktinfo ?: UKJENT_NAVN
         val aarsakInnsending = if (steg0.forespoersel.erBesvart) AarsakInnsending.Endring else AarsakInnsending.Ny // !!! hmm..
 
         val inntektsmelding =

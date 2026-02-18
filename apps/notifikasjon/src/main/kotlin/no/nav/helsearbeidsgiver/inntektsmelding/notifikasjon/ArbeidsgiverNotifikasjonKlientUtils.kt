b46cf2837b80ec8af35ee02dd5a98607d23f587e
Gustav Berggren <gustav.berggren@nav.no>
2026-02-18 16:54:51 +0100

    Fjern orgnr

--- apps/notifikasjon/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/ArbeidsgiverNotifikasjonKlientUtils.kt
+++ apps/notifikasjon/src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/notifikasjon/ArbeidsgiverNotifikasjonKlientUtils.kt
@@ -120,7 +120,7 @@ fun ArbeidsgiverNotifikasjonKlient.opprettSak(
         }
 
     logger.info(
-        "Oppretter sak med ${mottaker.tilTekst()} for inntektsmelding med typeId ${inntektsmeldingType.id} for virksomhet ${orgnr.verdi}.",
+        "Oppretter sak med ${mottaker.tilTekst()} for inntektsmelding med typeId ${inntektsmeldingType.id}",
     )
 
     return try {

8728b16ac58732b04c34817cbc6d0008be95ba78
Jesper Hustad <jesper.forrest.hustad@nav.no>
2026-07-03 12:57:59 +0200

    Deploy til dev

--- utils/felles/src/main/kotlin/no/nav/hag/simba/utils/felles/Tekst.kt
+++ utils/felles/src/main/kotlin/no/nav/hag/simba/utils/felles/Tekst.kt
@@ -4,7 +4,7 @@ object Tekst {
     const val UKJENT_NAVN = "Ukjent navn"
     const val UKJENT_VIRKSOMHET = "Ukjent virksomhet"
 
-    // Feilmeldinger
+    //Feilmeldinger
     const val TEKNISK_FEIL_FORBIGAAENDE = "Teknisk feil, prøv igjen senere."
     const val REDIS_TIMEOUT_FEILMELDING = "Brukte for lang tid mot Redis."
 }

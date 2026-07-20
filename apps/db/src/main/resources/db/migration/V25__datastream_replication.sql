6615093a6b0b9354a2493fd0a4e09fa55801d27f
Gustav Berggren <gustav.berggren@nav.no>
2026-07-20 15:08:40 +0200

    Legg til replication slot etter oppdatering

---
+++ apps/db/src/main/resources/db/migration/V25__datastream_replication.sql
@@ -0,0 +1,10 @@
+DO
+$$
+BEGIN
+    IF NOT EXISTS
+        (SELECT 1 FROM pg_replication_slots WHERE slot_name = 'simba_replication')
+    THEN
+        PERFORM PG_CREATE_LOGICAL_REPLICATION_SLOT ('simba_replication', 'pgoutput');
+    END IF;
+END;
+$$;
